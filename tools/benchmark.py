7894e6e052f30edfbee2778ebeaa934750345eb9
xiaohang <xiaohang@sensetime.com>
2020-07-28 20:31:51 +0800

    add benchmark and update getting_started.md

---
+++ tools/benchmark.py
@@ -0,0 +1,73 @@
+import argparse
+import time
+
+import torch
+from mmcv import Config
+from mmcv.parallel import MMDataParallel
+
+from mmpose.core import wrap_fp16_model
+from mmpose.datasets import build_dataloader, build_dataset
+from mmpose.models import build_posenet
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='MMPose benchmark a recognizer')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument(
+        '--log-interval', default=10, help='interval of logging')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    cfg = Config.fromfile(args.config)
+    # set cudnn_benchmark
+    if cfg.get('cudnn_benchmark', False):
+        torch.backends.cudnn.benchmark = True
+
+    # build the dataloader
+    dataset = build_dataset(cfg.data.train)
+    data_loader = build_dataloader(
+        dataset,
+        samples_per_gpu=cfg.data.samples_per_gpu,
+        workers_per_gpu=cfg.data.workers_per_gpu,
+        dist=False,
+        shuffle=False)
+
+    # build the model and load checkpoint
+    model = build_posenet(cfg.model)
+    fp16_cfg = cfg.get('fp16', None)
+    if fp16_cfg is not None:
+        wrap_fp16_model(model)
+    model = MMDataParallel(model, device_ids=[0])
+
+    # the first several iterations may be very slow so skip them
+    num_warmup = 5
+    pure_inf_time = 0
+
+    # benchmark with total batch and take the average
+    for i, data in enumerate(data_loader):
+
+        torch.cuda.synchronize()
+        start_time = time.perf_counter()
+
+        model(return_loss=True, **data)
+
+        torch.cuda.synchronize()
+        elapsed = time.perf_counter() - start_time
+
+        if i >= num_warmup:
+            pure_inf_time += elapsed
+            if (i + 1) % args.log_interval == 0:
+                fps = (i + 1 - num_warmup) / pure_inf_time
+                print(
+                    f'Done batch [{i + 1:<3}], fps: {fps:.1f} batch / s', flush=True)
+    print(f'Overall fps: {fps:.1f} batch / s', flush=True)
+    print(f'Total time: {pure_inf_time:.1f} s', flush=True)
+
+
+if __name__ == '__main__':
+    main()
