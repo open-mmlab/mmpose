beddeb7b8bfe86e12c2b5f3ec476b01cbd8040e7
Joanna <34728304+JoannaLXY@users.noreply.github.com>
2020-07-29 18:05:07 +0800

    Merge docs (#39)
    Co-authored-by: Xuanyi Li <xuanyil@blueshark.ics.cs.cmu.edu>
    Co-authored-by: lizz <innerlee@users.noreply.github.com>

---
+++ docs/merge_docs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cat  ../configs/bottom_up/*/*.md > bottom_up_models.md
+cat  ../configs/top_down/*/*.md > top_down_models.md
+
+sed -i "s/#/##&/" bottom_up_models.md
+sed -i "s/#/##&/" top_down_models.md
+sed -i "s/md###t/html#t/g" bottom_up_models.md
+sed -i "s/md###t/html#t/g" top_down_models.md
+
+sed -i '1i\## Bottom Up Models' bottom_up_models.md
+sed -i '1i\## Top Down Models' top_down_models.md
+
+sed -i 's/](\/docs\//](/g' bottom_up_models.md # remove /docs/ for link used in doc site
+sed -i 's/](\/docs\//](/g' top_down_models.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' bottom_up_models.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' top_down_models.md
+
+cat bottom_up_models.md top_down_models.md >> model_zoo.md
