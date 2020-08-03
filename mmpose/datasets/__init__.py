80b6c6acc3b601720eced0c56028e641ad9c4175
jinsheng <jinsheng@sensetime.com>
2020-08-03 21:34:30 +0800

    fix bugs

--- mmpose/datasets/__init__.py
+++ mmpose/datasets/__init__.py
@@ -1,12 +1,12 @@
 from .builder import build_dataloader, build_dataset
 from .datasets import (BottomUpCocoDataset, TopDownCocoDataset,
-                       TopDownTRBMPIDataset)
+                       TopDownMpiiTrbDataset)
 from .pipelines import Compose
 from .registry import DATASETS, PIPELINES
 from .samplers import DistributedSampler
 
 __all__ = [
-    'TopDownCocoDataset', 'BottomUpCocoDataset', 'TopDownTRBMPIDataset',
+    'TopDownCocoDataset', 'BottomUpCocoDataset', 'TopDownMpiiTrbDataset',
     'build_dataloader', 'build_dataset', 'Compose', 'DistributedSampler',
     'DATASETS', 'PIPELINES'
 ]
