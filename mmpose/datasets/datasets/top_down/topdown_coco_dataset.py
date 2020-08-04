5ac5799cbcb14ba57c3f7d860cc12a7264416545
jinsheng <jinsheng@sensetime.com>
2020-08-04 12:20:43 +0800

    add assert print

--- mmpose/datasets/datasets/top_down/topdown_coco_dataset.py
+++ mmpose/datasets/datasets/top_down/topdown_coco_dataset.py
@@ -383,6 +383,7 @@ class TopDownCocoDataset(TopDownBaseDataset):
 
         return cat_results
 
+
     def _do_python_keypoint_eval(self, res_file):
         """Keypoint evaluation using COCOAPI."""
         coco_dt = self.coco.loadRes(res_file)
