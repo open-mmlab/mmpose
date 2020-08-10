ec779499e61e8df27fc1792a03ff8bd625cc5e72
xiaohang <xiaohang@sensetime.com>
2020-08-10 15:55:28 +0800

    add crowdpose pipelines & update

--- mmpose/datasets/pipelines/__init__.py
+++ mmpose/datasets/pipelines/__init__.py
@@ -1,5 +1,5 @@
 from .bottom_up_transform import *  # noqa
 from .loading import LoadImageFromFile  # noqa
 from .shared_transform import *  # noqa
-from .top_down_transform import *  # noqa
 from .top_down_crowd_transform import *  # noqa
+from .top_down_transform import *  # noqa
