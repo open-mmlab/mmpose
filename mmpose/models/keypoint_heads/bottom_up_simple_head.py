a746532584b576ff823a48e794256f1b913118f7
jin-s13 <jinsheng13@foxmail.com>
2020-08-12 20:17:03 +0800

    update bu cfg

--- mmpose/models/keypoint_heads/bottom_up_simple_head.py
+++ mmpose/models/keypoint_heads/bottom_up_simple_head.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
-from mmcv.cnn import build_conv_layer, normal_init
+from mmcv.cnn import (build_conv_layer, build_upsample_layer, constant_init,
+                      normal_init)
 
 from ..registry import HEADS
 
@@ -11,6 +12,12 @@ class BottomUpSimpleHead(nn.Module):
     Args:
         in_channels (int): Number of input channels.
         num_joints (int): Number of joints.
+        num_deconv_layers (int): Number of deconv layers.
+            num_deconv_layers should >= 0. Note that 0 means
+            no deconv layers.
+        num_deconv_filters (list|tuple): Number of filters.
+            If num_deconv_layers > 0, the length of
+        num_deconv_kernels (list|tuple): Kernel sizes.
         tag_per_joint (bool): If tag_per_joint is True,
             the dimension of tags equals to num_joints,
             else the dimension of tags is 1. Default: True
@@ -20,6 +27,9 @@ class BottomUpSimpleHead(nn.Module):
     def __init__(self,
                  in_channels,
                  num_joints,
+                 num_deconv_layers=3,
+                 num_deconv_filters=(256, 256, 256),
+                 num_deconv_kernels=(4, 4, 4),
                  tag_per_joint=True,
                  with_ae_loss=None,
                  extra=None):
@@ -35,6 +45,18 @@ class BottomUpSimpleHead(nn.Module):
         if extra is not None and not isinstance(extra, dict):
             raise TypeError('extra should be dict or None.')
 
+        if num_deconv_layers > 0:
+            self.deconv_layers = self._make_deconv_layer(
+                num_deconv_layers,
+                num_deconv_filters,
+                num_deconv_kernels,
+            )
+        elif num_deconv_layers == 0:
+            self.deconv_layers = nn.Identity()
+        else:
+            raise ValueError(
+                f'num_deconv_layers ({num_deconv_layers}) should >= 0.')
+
         if extra is not None and 'final_conv_kernel' in extra:
             assert extra['final_conv_kernel'] in [1, 3]
             if extra['final_conv_kernel'] == 3:
@@ -46,9 +68,10 @@ class BottomUpSimpleHead(nn.Module):
             kernel_size = 1
             padding = 0
 
-        self.final_layers = build_conv_layer(
+        self.final_layer = build_conv_layer(
             cfg=dict(type='Conv2d'),
-            in_channels=in_channels,
+            in_channels=num_deconv_filters[-1]
+            if num_deconv_layers > 0 else in_channels,
             out_channels=out_channels,
             kernel_size=kernel_size,
             stride=1,
@@ -59,12 +82,67 @@ class BottomUpSimpleHead(nn.Module):
         if isinstance(x, list):
             x = x[0]
         final_outputs = []
+        x = self.deconv_layers(x)
         y = self.final_layers(x)
         final_outputs.append(y)
         return final_outputs
 
+    def _make_deconv_layer(self, num_layers, num_filters, num_kernels):
+        """Make deconv layers."""
+        if num_layers != len(num_filters):
+            error_msg = f'num_layers({num_layers}) ' \
+                        f'!= length of num_filters({len(num_filters)})'
+            raise ValueError(error_msg)
+        if num_layers != len(num_kernels):
+            error_msg = f'num_layers({num_layers}) ' \
+                        f'!= length of num_kernels({len(num_kernels)})'
+            raise ValueError(error_msg)
+
+        layers = []
+        for i in range(num_layers):
+            kernel, padding, output_padding = \
+                self._get_deconv_cfg(num_kernels[i])
+
+            planes = num_filters[i]
+            layers.append(
+                build_upsample_layer(
+                    dict(type='deconv'),
+                    in_channels=self.in_channels,
+                    out_channels=planes,
+                    kernel_size=kernel,
+                    stride=2,
+                    padding=padding,
+                    output_padding=output_padding,
+                    bias=False))
+            layers.append(nn.BatchNorm2d(planes))
+            layers.append(nn.ReLU(inplace=True))
+            self.in_channels = planes
+
+        return nn.Sequential(*layers)
+
+    def _get_deconv_cfg(self, deconv_kernel):
+        """Get configurations for deconv layers."""
+        if deconv_kernel == 4:
+            padding = 1
+            output_padding = 0
+        elif deconv_kernel == 3:
+            padding = 1
+            output_padding = 1
+        elif deconv_kernel == 2:
+            padding = 0
+            output_padding = 0
+        else:
+            raise ValueError(f'Not supported num_kernels ({deconv_kernel}).')
+
+        return deconv_kernel, padding, output_padding
+
     def init_weights(self):
         """Initialize model weights."""
-        for m in self.final_layers.modules():
+        for name, m in self.deconv_layers.named_modules():
+            if isinstance(m, nn.ConvTranspose2d):
+                normal_init(m, std=0.001)
+            elif isinstance(m, nn.BatchNorm2d):
+                constant_init(m, 1)
+        for m in self.final_layer.modules():
             if isinstance(m, nn.Conv2d):
                 normal_init(m, std=0.001, bias=0)
