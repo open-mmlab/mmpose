b596bdd99c64ed2b8cd2b508163dca24575247e1
liuxin1 <liuxin1@sensetime.com>
2020-08-14 23:43:57 +0800

    fix lint

--- docs/conf.py
+++ docs/conf.py
@@ -11,8 +11,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
 import subprocess
+import sys
 
 sys.path.insert(0, os.path.abspath('..'))
 
