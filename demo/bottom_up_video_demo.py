04c2eea8418de844e32e9fe079c3637b21c27361
jinsheng <jinsheng@sensetime.com>
2020-08-16 16:58:38 +0800

    add video demo for bottom-up models

---
+++ demo/bottom_up_video_demo.py
@@ -0,0 +1,93 @@
+import os
+from argparse import ArgumentParser
+
+import cv2
+
+from mmpose.apis import (inference_bottom_up_pose_model, init_pose_model,
+                         vis_pose_result)
+
+
+def main():
+    """Visualize the demo images."""
+    parser = ArgumentParser()
+    parser.add_argument('pose_config', help='Config file for pose')
+    parser.add_argument('pose_checkpoint', help='Checkpoint file for pose')
+    parser.add_argument('--video-path', type=str, help='Video path')
+    parser.add_argument(
+        '--show',
+        action='store_true',
+        default=False,
+        help='whether to show visualizations.')
+    parser.add_argument(
+        '--out-video-root',
+        default='',
+        help='Root of the output video file. '
+        'Default not saving the visualization video.')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument(
+        '--kpt-thr', type=float, default=0.3, help='Keypoint score threshold')
+
+    args = parser.parse_args()
+
+    skeleton = [[16, 14], [14, 12], [17, 15], [15, 13], [12, 13], [6, 12],
+                [7, 13], [6, 7], [6, 8], [7, 9], [8, 10], [9, 11], [2, 3],
+                [1, 2], [1, 3], [2, 4], [3, 5], [4, 6], [5, 7]]
+
+    assert args.show or (args.out_video_root != '')
+    assert 'cuda' in args.device
+    # build the pose model from a config file and a checkpoint file
+    pose_model = init_pose_model(
+        args.pose_config, args.pose_checkpoint, device=args.device)
+
+    cap = cv2.VideoCapture(args.video_path)
+
+    if args.out_video_root == '':
+        save_out_video = False
+    else:
+        os.makedirs(args.out_video_root, exist_ok=True)
+        save_out_video = True
+
+    if save_out_video:
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        videoWriter = cv2.VideoWriter(
+            os.path.join(args.out_video_root,
+                         f'vis_{os.path.basename(args.video_path)}'), fourcc,
+            fps, size)
+
+    while (cap.isOpened()):
+        flag, img = cap.read()
+        if not flag:
+            break
+
+        pose_results = inference_bottom_up_pose_model(pose_model, img)
+
+        # show the results
+        vis_img = vis_pose_result(
+            pose_model,
+            img,
+            pose_results,
+            skeleton=skeleton,
+            kpt_score_thr=args.kpt_thr,
+            show=False)
+
+        if args.show:
+            cv2.imshow('Image', vis_img)
+
+        if save_out_video:
+            videoWriter.write(vis_img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    if save_out_video:
+        videoWriter.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
