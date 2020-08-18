c416e8dc3c7f056907d4a96c8cc7e421a0e6e90d
lizz <363664470@qq.com>
2020-08-18 11:32:56 +0800

    tiny
    Signed-off-by: lizz <363664470@qq.com>

--- mmpose/datasets/datasets/top_down/topdown_crowdpose_dataset.py
+++ mmpose/datasets/datasets/top_down/topdown_crowdpose_dataset.py
@@ -92,9 +92,9 @@ class TopDownCrowdPoseDataset(TopDownBaseDataset):
         self.num_classes = len(self.classes)
         self._class_to_ind = dict(zip(self.classes, range(self.num_classes)))
         self._class_to_coco_ind = dict(zip(cats, self.coco.getCatIds()))
-        self._coco_ind_to_class_ind = dict((self._class_to_coco_ind[cls],
-                                             self._class_to_ind[cls])
-                                            for cls in self.classes[1:])
+        self._coco_ind_to_class_ind = dict(
+            (self._class_to_coco_ind[cls], self._class_to_ind[cls])
+            for cls in self.classes[1:])
         self.image_set_index = self.coco.getImgIds()
         self.num_images = len(self.image_set_index)
         self.db = self._get_db()
