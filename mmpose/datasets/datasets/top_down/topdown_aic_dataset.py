ebf2efd01ff9777831240e82804a1c7df402fe07
jinsheng <jinsheng@sensetime.com>
2020-08-19 10:25:54 +0800

    add sigma

--- mmpose/datasets/datasets/top_down/topdown_aic_dataset.py
+++ mmpose/datasets/datasets/top_down/topdown_aic_dataset.py
@@ -109,6 +109,12 @@ class TopDownAicDataset(TopDownCocoDataset):
             ],
             dtype=np.float32).reshape((self.ann_info['num_joints'], 1))
 
+        self.sigmas = np.array([
+            0.01388152, 0.01515228, 0.01057665, 0.01417709, 0.01497891,
+            0.01402144, 0.03909642, 0.03686941, 0.01981803, 0.03843971,
+            0.03412318, 0.02415081, 0.01291456, 0.01236173
+        ])
+
         self.coco = COCO(test_num_keypoints=14, annotation_file=ann_file)
 
         cats = [
@@ -258,10 +264,13 @@ class TopDownAicDataset(TopDownCocoDataset):
 
             if self.soft_nms:
                 keep = soft_oks_nms(
-                    [img_kpts[i] for i in range(len(img_kpts))], oks_thr)
+                    [img_kpts[i] for i in range(len(img_kpts))],
+                    oks_thr,
+                    sigmas=self.sigmas)
             else:
                 keep = oks_nms([img_kpts[i] for i in range(len(img_kpts))],
-                               oks_thr)
+                               oks_thr,
+                               sigmas=self.sigmas)
 
             if len(keep) == 0:
                 oks_nmsed_kpts.append(img_kpts)
@@ -278,13 +287,8 @@ class TopDownAicDataset(TopDownCocoDataset):
     def _do_python_keypoint_eval(self, res_file):
         """Keypoint evaluation using COCOAPI."""
 
-        sigmas = np.array([
-            0.01388152, 0.01515228, 0.01057665, 0.01417709, 0.01497891,
-            0.01402144, 0.03909642, 0.03686941, 0.01981803, 0.03843971,
-            0.03412318, 0.02415081, 0.01291456, 0.01236173
-        ])
         coco_dt = self.coco.loadRes(res_file)
-        coco_eval = COCOeval(sigmas, self.coco, coco_dt, 'keypoints')
+        coco_eval = COCOeval(self.sigmas, self.coco, coco_dt, 'keypoints')
         coco_eval.params.useSegm = None
         coco_eval.evaluate()
         coco_eval.accumulate()
