ca7d3690eabcca6f3163138ac405187b4ae96ffe
jinsheng <jinsheng@sensetime.com>
2020-09-02 20:36:52 +0800

    use smaller interval

--- configs/top_down/resnet/onehand10k/res50_onehand10k_256x256.py
+++ configs/top_down/resnet/onehand10k/res50_onehand10k_256x256.py
@@ -3,7 +3,7 @@ load_from = None
 resume_from = None
 dist_params = dict(backend='nccl')
 workflow = [('train', 1)]
-checkpoint_config = dict(interval=10)
+checkpoint_config = dict(interval=2)
 evaluation = dict(interval=1, metric=['PCK', 'AUC', 'EPE'])
 
 optimizer = dict(
