42cfbcdaca8f658dfbe6232a6e278bc3cdcb3c5b
Ycr <34575412+yaochaorui@users.noreply.github.com>
2020-09-19 18:41:31 +0800

    Modified version files. (#135)

---
+++ mmpose/version.py
@@ -0,0 +1,7 @@
+# Copyright (c) Open-MMLab. All rights reserved.
+from mmcv import parse_version_info
+
+__version__ = '0.6.0'
+short_version = __version__
+
+version_info = parse_version_info(__version__)
