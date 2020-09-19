b41d86d2e879d1e227d84acf02aa3bc39434dc93
Ycr <yaochaorui@sensetime.com>
2020-09-19 18:14:29 +0800

    Fixed.

--- mmpose/version.py
+++ mmpose/version.py
@@ -1,19 +1,7 @@
 # Copyright (c) Open-MMLab. All rights reserved.
+from mmcv import parse_version_info
 
 __version__ = '0.6.0'
 short_version = __version__
 
-
-def parse_version_info(version_str):
-    version_info = []
-    for x in version_str.split('.'):
-        if x.isdigit():
-            version_info.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
-            version_info.append(int(patch_version[0]))
-            version_info.append(f'rc{patch_version[1]}')
-    return tuple(version_info)
-
-
-__version__ = parse_version_info(__version__)
+version_info = parse_version_info(__version__)
