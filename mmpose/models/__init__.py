e068e7b698c6cb15923a078f8c9863f75aaf70bc
zengwang <zengwang430521@gmail.com>
2020-09-28 20:10:56 +0800

    Add the hmr head and discriminator for SMPL parameters. Add test codes and test data.

--- mmpose/models/__init__.py
+++ mmpose/models/__init__.py
@@ -3,6 +3,7 @@ from .builder import build_backbone, build_head, build_loss, build_posenet
 from .detectors import *  # noqa
 from .keypoint_heads import *  # noqa
 from .losses import *  # noqa
+from .mesh_heads import *  # noqa
 from .registry import BACKBONES, HEADS, LOSSES, POSENETS
 
 __all__ = [
