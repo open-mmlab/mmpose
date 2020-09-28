70500838d6f2759ea331b752743a45f9c174fe29
zengwang430521 <zengwang430521@gmail.com>
2020-09-28 21:55:32 +0800

    Add the function of Procrutes Alignment (#157)
    * Add the function of Procrutes Alignment used in 3D joints error evaluation and corresponding test codes.
    
    * Fix the input shape of function compute_similarity_transform.

--- mmpose/core/evaluation/__init__.py
+++ mmpose/core/evaluation/__init__.py
@@ -1,11 +1,13 @@
 from .bottom_up_eval import (aggregate_results, get_group_preds,
                              get_multi_stage_outputs)
 from .eval_hooks import DistEvalHook, EvalHook
+from .mesh_eval import compute_similarity_transform
 from .top_down_eval import (keypoint_auc, keypoint_epe, keypoint_pck_accuracy,
                             keypoints_from_heatmaps, pose_pck_accuracy)
 
 __all__ = [
     'EvalHook', 'DistEvalHook', 'pose_pck_accuracy', 'keypoints_from_heatmaps',
     'keypoint_pck_accuracy', 'keypoint_auc', 'keypoint_epe', 'get_group_preds',
-    'get_multi_stage_outputs', 'aggregate_results'
+    'get_multi_stage_outputs', 'aggregate_results',
+    'compute_similarity_transform'
 ]
