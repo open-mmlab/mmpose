a056e3c51d37db4bb8087fb62e3465045bf1a7e0
Ycr <yaochaorui@sensetime.com>
2020-10-04 21:45:52 +0800

    Whitesapce.

--- mmpose/__init__.py
+++ mmpose/__init__.py
@@ -13,7 +13,7 @@ assert digit_version(mmcv_minimum_version) <= mmcv_version, \
         f'Please install mmcv>={mmcv_minimum_version}.'
 
 assert digit_version(mmcv_maximum_version) > mmcv_version, \
-         f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-         f'Please install mmcv<{mmcv_maximum_version}.'
+        f'MMCV=={mmcv.__version__} is used but incompatible. ' \
+        f'Please install mmcv<{mmcv_maximum_version}.'
 
 __all__ = ['__version__', 'short_version', 'version_info']
