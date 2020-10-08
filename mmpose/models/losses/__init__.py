586d4e64563ad2347820f4080aff7ee24c227fc5
zengwang430521 <zengwang430521@gmail.com>
2020-10-08 17:56:02 +0800

    Add mesh loss and gan loss for human mesh estimation. (#167)
    * Add mesh loss and gan loss for human mesh estimation.
    
    * Fix a bug in test code.
    
    * Remove the comment related to mmedit.
    
    * Modify codes according to reviews.
    
    * Add some comments.
    
    * Add some comments.
    
    * Modify joints_2d normalization code.

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -1,7 +1,8 @@
+from .mesh_loss import GANLoss, MeshLoss
 from .mse_loss import JointsMSELoss, JointsOHKMMSELoss
 from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
 
 __all__ = [
     'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss',
-    'MultiLossFactory'
+    'MultiLossFactory', 'MeshLoss', 'GANLoss'
 ]
