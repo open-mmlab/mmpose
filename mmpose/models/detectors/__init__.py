89c243e0f91d7a60cb4e3e82291c6b169cf02920
zengwang430521 <zengwang430521@gmail.com>
2020-10-12 00:47:15 +0800

    Add ParametricMesh detector. (#184)
    Co-authored-by: lizz <innerlee@users.noreply.github.com>

--- mmpose/models/detectors/__init__.py
+++ mmpose/models/detectors/__init__.py
@@ -1,4 +1,5 @@
 from .bottom_up import BottomUp
+from .mesh import ParametricMesh
 from .top_down import TopDown
 
-__all__ = ['TopDown', 'BottomUp']
+__all__ = ['TopDown', 'BottomUp', 'ParametricMesh']
