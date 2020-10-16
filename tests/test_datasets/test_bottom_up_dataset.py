f60ac5b86a629b920695a98b8039068445d7cf11
jinsheng <jinsheng@sensetime.com>
2020-10-16 13:35:38 +0800

    add bu crowdpose test

--- tests/test_datasets/test_bottom_up_dataset.py
+++ tests/test_datasets/test_bottom_up_dataset.py
@@ -1,5 +1,3 @@
-import copy
-
 from mmpose.datasets import DATASETS
 
 
@@ -28,13 +26,63 @@ def test_bottom_up_COCO_dataset():
         num_scales=2,
         scale_aware_sigma=False)
 
-    # Test det bbox
-    data_cfg_copy = copy.deepcopy(data_cfg)
+    _ = dataset_class(
+        ann_file='tests/data/coco/test_coco.json',
+        img_prefix='tests/data/coco/',
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=False)
+
     custom_dataset = dataset_class(
         ann_file='tests/data/coco/test_coco.json',
         img_prefix='tests/data/coco/',
-        data_cfg=data_cfg_copy,
-        pipeline=[])
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=True)
 
     assert custom_dataset.num_images == 4
+    _ = custom_dataset[0]
+
+
+def test_bottom_up_CrowdPose_dataset():
+    dataset = 'BottomUpCrowdPoseDataset'
+    # test COCO datasets
+    dataset_class = DATASETS.get(dataset)
+
+    channel_cfg = dict(
+        num_output_channels=14,
+        dataset_joints=14,
+        dataset_channel=[
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+        ],
+        inference_channel=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+
+    data_cfg = dict(
+        image_size=512,
+        base_size=256,
+        base_sigma=2,
+        heatmap_size=[128, 256],
+        num_joints=channel_cfg['dataset_joints'],
+        dataset_channel=channel_cfg['dataset_channel'],
+        inference_channel=channel_cfg['inference_channel'],
+        num_scales=2,
+        scale_aware_sigma=False)
+
+    _ = dataset_class(
+        ann_file='tests/data/crowdpose/test_crowdpose.json',
+        img_prefix='tests/data/crowdpose/',
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=False)
+
+    custom_dataset = dataset_class(
+        ann_file='tests/data/crowdpose/test_crowdpose.json',
+        img_prefix='tests/data/crowdpose/',
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=True)
+
+    image_id = 103319
+    assert image_id in custom_dataset.image_set_index
+    assert len(custom_dataset.image_set_index) == 2
     _ = custom_dataset[0]
