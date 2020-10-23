b6061344b6f07209b918c54d8c25be5043d27ac1
liuxin1 <liuxin1@sensetime.com>
2020-10-23 13:19:07 +0800

    fix inference

--- tests/test_api/test_inference.py
+++ tests/test_api/test_inference.py
@@ -36,7 +36,7 @@ def test_top_down_demo():
     # OneHand10K demo
     # build the pose model from a config file and a checkpoint file
     pose_model = init_pose_model(
-        'configs/top_down/resnet/onehand10k/res50_onehand10k_256x256.py',
+        'configs/hand/resnet/onehand10k/res50_onehand10k_256x256.py',
         None,
         device='cpu')
     image_name = 'tests/data/onehand10k/9.jpg'
