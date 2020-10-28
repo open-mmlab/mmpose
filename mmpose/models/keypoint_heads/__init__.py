d5582ff95c996ad0fdfd1ae9a28a3fa06b6cea48
wusize <wusize@sensetime.com>
2020-10-28 22:14:25 +0800

    Add rsn

--- mmpose/models/keypoint_heads/__init__.py
+++ mmpose/models/keypoint_heads/__init__.py
@@ -1,9 +1,10 @@
 from .bottom_up_higher_resolution_head import BottomUpHigherResolutionHead
 from .bottom_up_simple_head import BottomUpSimpleHead
+from .top_down_mspn_head import TopDownMSPNHead
 from .top_down_multi_stage_head import TopDownMultiStageHead
 from .top_down_simple_head import TopDownSimpleHead
 
 __all__ = [
-    'TopDownSimpleHead', 'TopDownMultiStageHead',
+    'TopDownSimpleHead', 'TopDownMultiStageHead', 'TopDownMSPNHead',
     'BottomUpHigherResolutionHead', 'BottomUpSimpleHead'
 ]
