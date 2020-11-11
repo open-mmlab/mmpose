b4f487365c4b513270c6a7d9cda1c480b155a9c7
jinsheng <jinsheng@sensetime.com>
2020-11-11 15:15:09 +0800

    remove mspn

--- mmpose/models/backbones/__init__.py
+++ mmpose/models/backbones/__init__.py
@@ -4,7 +4,6 @@ from .hourglass import HourglassNet
 from .hrnet import HRNet
 from .mobilenet_v2 import MobileNetV2
 from .mobilenet_v3 import MobileNetV3
-from .mspn import MSPN
 from .regnet import RegNet
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
@@ -18,5 +17,5 @@ from .shufflenet_v2 import ShuffleNetV2
 __all__ = [
     'AlexNet', 'HourglassNet', 'HRNet', 'MobileNetV2', 'MobileNetV3', 'RegNet',
     'ResNet', 'ResNetV1d', 'ResNeXt', 'SCNet', 'SEResNet', 'SEResNeXt',
-    'ShuffleNetV1', 'ShuffleNetV2', 'CPM', 'RSN', 'MSPN'
+    'ShuffleNetV1', 'ShuffleNetV2', 'CPM', 'RSN'
 ]
