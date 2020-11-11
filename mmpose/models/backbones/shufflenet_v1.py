b2286184a2920e1580e06fbc790f98be0bc77aa9
Ycr <yaochaorui@sensetime.com>
2020-11-11 13:03:53 +0800

    Simplified following mentor's convention.

--- mmpose/models/backbones/shufflenet_v1.py
+++ mmpose/models/backbones/shufflenet_v1.py
@@ -235,7 +235,7 @@ class ShuffleNetV1(BaseBackbone):
 
         self.layers = nn.ModuleList()
         for i, num_blocks in enumerate(self.stage_blocks):
-            first_block = bool(i == 0)
+            first_block = (i == 0)
             layer = self.make_layer(channels[i], num_blocks, first_block)
             self.layers.append(layer)
 
