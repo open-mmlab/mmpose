b0fe89a1318301c75370c3c8478536c5f6d6fa9a
wusize <72066971+wusize@users.noreply.github.com>
2020-11-12 13:50:47 +0800

    Add rsn backbone, head and pre-processing (#221)
    * Add rsn
    
    * Add unit test
    
    * Modification
    
    * Some corrections
    
    * Corrections
    
    * Correction in top_down_transform.py
    
    * Correction in top_down_transform.py
    
    * Add mspn
    
    * unit test for mspn
    
    * remove mspn
    
    * rename mspn_head to msmu_head
    
    * Add unit test for backbone and head
    
    * fix a bug in rsn.py
    
    * Add rsn
    
    * Add unit test
    
    * Modification
    
    * Some corrections
    
    * Corrections
    
    * Correction in top_down_transform.py
    
    * Correction in top_down_transform.py
    
    * Add mspn
    
    * unit test for mspn
    
    * remove mspn
    
    * rename mspn_head to msmu_head
    
    * Add unit test for backbone and head
    
    * fix a bug in rsn.py
    
    * rm unnecessary codes
    
    * add comment for 3-sigma rule
    
    * fix some bugs
    
    Co-authored-by: jinsheng <jinsheng@sensetime.com>

--- mmpose/models/keypoint_heads/__init__.py
+++ mmpose/models/keypoint_heads/__init__.py
@@ -1,9 +1,9 @@
 from .bottom_up_higher_resolution_head import BottomUpHigherResolutionHead
 from .bottom_up_simple_head import BottomUpSimpleHead
-from .top_down_multi_stage_head import TopDownMultiStageHead
+from .top_down_multi_stage_head import TopDownMSMUHead, TopDownMultiStageHead
 from .top_down_simple_head import TopDownSimpleHead
 
 __all__ = [
-    'TopDownSimpleHead', 'TopDownMultiStageHead',
+    'TopDownSimpleHead', 'TopDownMultiStageHead', 'TopDownMSMUHead',
     'BottomUpHigherResolutionHead', 'BottomUpSimpleHead'
 ]
