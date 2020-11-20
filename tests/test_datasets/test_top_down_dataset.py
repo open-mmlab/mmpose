139742fbe1edcf5a9d68e26f87399fe1dde6847e
jinsheng <jinsheng@sensetime.com>
2020-11-20 15:37:58 +0800

    add jhmdb dataset

--- tests/test_datasets/test_top_down_dataset.py
+++ tests/test_datasets/test_top_down_dataset.py
@@ -771,3 +771,77 @@ def test_top_down_AIC_dataset():
 
         with pytest.raises(KeyError):
             _ = custom_dataset.evaluate(outputs, tmpdir, 'PCK')
+
+
+def test_top_down_JHMDB_dataset():
+    dataset = 'TopDownJhmdbDataset'
+    # test JHMDB datasets
+    dataset_class = DATASETS.get(dataset)
+    dataset_class.load_annotations = MagicMock()
+    dataset_class.coco = MagicMock()
+
+    channel_cfg = dict(
+        num_output_channels=15,
+        dataset_joints=15,
+        dataset_channel=[
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        ],
+        inference_channel=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+
+    data_cfg = dict(
+        image_size=[192, 256],
+        heatmap_size=[48, 64],
+        num_output_channels=channel_cfg['num_output_channels'],
+        num_joints=channel_cfg['dataset_joints'],
+        dataset_channel=channel_cfg['dataset_channel'],
+        inference_channel=channel_cfg['inference_channel'],
+        soft_nms=False,
+        nms_thr=1.0,
+        oks_thr=0.9,
+        vis_thr=0.2,
+        bbox_thr=1.0,
+        use_gt_bbox=True,
+        image_thr=0.0,
+        bbox_file='')
+
+    with pytest.raises(AssertionError):
+        # Test det bbox
+        data_cfg_copy = copy.deepcopy(data_cfg)
+        data_cfg_copy['use_gt_bbox'] = False
+        _ = dataset_class(
+            ann_file='tests/data/jhmdb/test_jhmdb_sub1.json',
+            img_prefix='tests/data/jhmdb/',
+            data_cfg=data_cfg_copy,
+            pipeline=[],
+            test_mode=True)
+
+        _ = dataset_class(
+            ann_file='tests/data/jhmdb/test_jhmdb_sub1.json',
+            img_prefix='tests/data/jhmdb/',
+            data_cfg=data_cfg_copy,
+            pipeline=[],
+            test_mode=False)
+
+    # Test gt bbox
+    custom_dataset = dataset_class(
+        ann_file='tests/data/jhmdb/test_jhmdb_sub1.json',
+        img_prefix='tests/data/jhmdb/',
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=True)
+
+    assert custom_dataset.test_mode is True
+
+    image_id = 2290001
+    assert image_id in custom_dataset.img_ids
+    assert len(custom_dataset.img_ids) == 3
+    _ = custom_dataset[0]
+
+    outputs = load_json_to_output('tests/data/jhmdb/test_jhmdb_sub1.json',
+                                  'tests/data/jhmdb/')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        infos = custom_dataset.evaluate(outputs, tmpdir, ['PCK'])
+        assert_almost_equal(infos['Mean PCK'], 1.0)
+
+        with pytest.raises(KeyError):
+            infos = custom_dataset.evaluate(outputs, tmpdir, 'mAP')
