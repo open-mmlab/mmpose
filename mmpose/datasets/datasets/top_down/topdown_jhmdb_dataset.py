bee18b5ab9bd3b676bb969d4c2ce1dee2f93cc7f
jinsheng <jinsheng@sensetime.com>
2020-11-21 23:47:19 +0800

    shift 1 pixel

--- mmpose/datasets/datasets/top_down/topdown_jhmdb_dataset.py
+++ mmpose/datasets/datasets/top_down/topdown_jhmdb_dataset.py
@@ -114,6 +114,78 @@ class TopDownJhmdbDataset(TopDownCocoDataset):
         gt_db = self._load_coco_keypoint_annotations()
         return gt_db
 
+    def _load_coco_keypoint_annotation_kernel(self, img_id):
+        """load annotation from COCOAPI.
+
+        Note:
+            bbox:[x1, y1, w, h]
+        Args:
+            img_id: coco image id
+        Returns:
+            dict: db entry
+        """
+        img_ann = self.coco.loadImgs(img_id)[0]
+        width = img_ann['width']
+        height = img_ann['height']
+        num_joints = self.ann_info['num_joints']
+
+        ann_ids = self.coco.getAnnIds(imgIds=img_id, iscrowd=False)
+        objs = self.coco.loadAnns(ann_ids)
+
+        # sanitize bboxes
+        valid_objs = []
+        for obj in objs:
+            if 'bbox' not in obj:
+                continue
+            x, y, w, h = obj['bbox']
+            # JHMDB uses matlab format, index is 1-based,
+            # we should first convert to 0-based index
+            x -= 1
+            y -= 1
+            x1 = max(0, x)
+            y1 = max(0, y)
+            x2 = min(width - 1, x1 + max(0, w - 1))
+            y2 = min(height - 1, y1 + max(0, h - 1))
+            if ('area' not in obj or obj['area'] > 0) and x2 > x1 and y2 > y1:
+                obj['clean_bbox'] = [x1, y1, x2 - x1, y2 - y1]
+                valid_objs.append(obj)
+        objs = valid_objs
+
+        rec = []
+        for obj in objs:
+            if 'keypoints' not in obj:
+                continue
+            if max(obj['keypoints']) == 0:
+                continue
+            if 'num_keypoints' in obj and obj['num_keypoints'] == 0:
+                continue
+            joints_3d = np.zeros((num_joints, 3), dtype=np.float32)
+            joints_3d_visible = np.zeros((num_joints, 3), dtype=np.float32)
+
+            keypoints = np.array(obj['keypoints']).reshape(-1, 3)
+
+            # JHMDB uses matlab format, index is 1-based,
+            # we should first convert to 0-based index
+            joints_3d[:, :2] = keypoints[:, :2] - 1
+            joints_3d_visible[:, :2] = np.minimum(1, keypoints[:, 2:3])
+
+            center, scale = self._xywh2cs(*obj['clean_bbox'][:4])
+
+            image_file = os.path.join(self.img_prefix, self.id2name[img_id])
+            rec.append({
+                'image_file': image_file,
+                'center': center,
+                'scale': scale,
+                'bbox': obj['clean_bbox'][:4],
+                'rotation': 0,
+                'joints_3d': joints_3d,
+                'joints_3d_visible': joints_3d_visible,
+                'dataset': self.dataset_name,
+                'bbox_score': 1
+            })
+
+        return rec
+
     def _write_keypoint_results(self, keypoints, res_file):
         """Write results into a json file."""
 
@@ -246,6 +318,10 @@ class TopDownJhmdbDataset(TopDownCocoDataset):
         kpts = []
 
         for preds, boxes, image_path, _ in outputs:
+            # convert 0-based index to 1-based index,
+            # and get the first two dimensions.
+            preds[..., :2] += 1.0
+
             str_image_path = ''.join(image_path)
             image_id = self.name2id[str_image_path[len(self.img_prefix):]]
 
