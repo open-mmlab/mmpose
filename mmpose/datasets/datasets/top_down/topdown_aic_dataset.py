df7aae8df95ff0354d78ae795dee8ba12d1ce1f9
jinsheng <jinsheng@sensetime.com>
2020-11-29 21:49:57 +0800

    use default value

--- mmpose/datasets/datasets/top_down/topdown_aic_dataset.py
+++ mmpose/datasets/datasets/top_down/topdown_aic_dataset.py
@@ -54,11 +54,7 @@ class TopDownAicDataset(TopDownCocoDataset):
         self.use_gt_bbox = data_cfg['use_gt_bbox']
         self.bbox_file = data_cfg['bbox_file']
         self.image_thr = data_cfg['image_thr']
-
-        if 'use_nms' in data_cfg:
-            self.use_nms = data_cfg['use_nms']
-        else:
-            self.use_nms = True
+        self.use_nms = data_cfg.get('use_nms', True)
         self.soft_nms = data_cfg['soft_nms']
         self.nms_thr = data_cfg['nms_thr']
         self.oks_thr = data_cfg['oks_thr']
