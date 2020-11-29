c2518358752ffe50611aa3e4654ea4c2221dcbd6
lizz <lizz@sensetime.com>
2020-11-29 21:01:16 +0800

    One more
    Signed-off-by: lizz <lizz@sensetime.com>

--- configs/top_down/rsn/coco/single_ctf_rsn18_coco_256x192.py
+++ configs/top_down/rsn/coco/single_ctf_rsn18_coco_256x192.py
@@ -58,9 +58,8 @@ model = dict(
     train_cfg=dict(num_units=4, loss_weights=[0.25] * 3 + [1]),
     test_cfg=dict(
         flip_test=True,
-        post_process=True,
+        post_process='default',
         shift_heatmap=False,
-        unbiased_decoding=False,
         modulate_kernel=5),
     loss_pose=[dict(type='JointsMSELoss', use_target_weight=True)] * 3 +
     [dict(type='JointsOHKMMSELoss', use_target_weight=True)])
