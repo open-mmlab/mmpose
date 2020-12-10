0161155a5917f6ccb708f01195bf025585e11458
junjie.huang <junjie.huang@xforwardai.com>
2020-12-10 11:14:53 +0800

    add information dropping augmentation (AID)

--- mmpose/datasets/pipelines/shared_transform.py
+++ mmpose/datasets/pipelines/shared_transform.py
@@ -1,5 +1,6 @@
 from collections.abc import Sequence
 
+import numpy as np
 from mmcv.parallel import DataContainer as DC
 from mmcv.utils import build_from_cfg
 from torchvision.transforms import functional as F
@@ -140,3 +141,81 @@ class Collect:
         """Compute the string representation."""
         return (f'{self.__class__.__name__}('
                 f'keys={self.keys}, meta_keys={self.meta_keys})')
+
+
+@PIPELINES.register_module()
+class AID:
+    """AID: Augmentation by Informantion Dropping.
+    Paper ref: Huang et al. AID: Pushing the Performance Boundary of Human Pose
+    Estimation with Information Dropping Augmentation ( arXiv:2008.07139 2020).
+
+    Args:
+        transforms (list): ToTensor & Normalize
+        prob_cutout (float): Probability of performing cutout.
+        radius_factor (float): Size factor of cutout area.
+        num_patch (float): Number of patches to be cutout.
+        prob_has (float): Probability of performing hide-and-seek.
+        prob_has_hide (float): Probability of hiding patches.
+    """
+
+    def __init__(self,
+                 prob_cutout=0.0,
+                 radius_factor=0.2,
+                 num_patch=1,
+                 prob_has=0.0,
+                 prob_has_hide=0.5):
+
+        self.prob_cutout = prob_cutout
+        self.radius_factor = radius_factor
+        self.num_patch = num_patch
+        self.prob_has = prob_has
+        self.prob_has_hide = prob_has_hide
+        assert (self.prob_cutout + self.prob_has) > 0
+
+    def _hide_and_seek(self, img):
+        # get width and height of the image
+        ht, wd, _ = img.shape
+
+        # possible grid size, 0 means no hiding
+        grid_sizes = [0, 16, 32, 44, 56]
+
+        # randomly choose one grid size
+        grid_size = grid_sizes[np.random.randint(0, len(grid_sizes) - 1)]
+
+        # hide the patches
+        if grid_size != 0:
+            for x in range(0, wd, grid_size):
+                for y in range(0, ht, grid_size):
+                    x_end = min(wd, x + grid_size)
+                    y_end = min(ht, y + grid_size)
+                    if np.random.rand() <= self.prob_has_hide:
+                        img[x:x_end, y:y_end, :] = 0
+        return img
+
+    def _cutout(self, img):
+        height, width, _ = img.shape
+        img = img.reshape((height * width, -1))
+        feat_x_int = np.arange(0, width)
+        feat_y_int = np.arange(0, height)
+        feat_x_int, feat_y_int = np.meshgrid(feat_x_int, feat_y_int)
+        feat_x_int = feat_x_int.reshape((-1, ))
+        feat_y_int = feat_y_int.reshape((-1, ))
+        for _ in range(self.num_patch):
+            center = [np.random.rand() * width, np.random.rand() * height]
+            radius = self.radius_factor * (1 + np.random.rand(2)) * width
+            x_offset = (center[0] - feat_x_int) / radius[0]
+            y_offset = (center[1] - feat_y_int) / radius[1]
+            dis = x_offset**2 + y_offset**2
+            keep_pos = np.where((dis <= 1) & (dis >= 0))[0]
+            img[keep_pos, :] = 0
+        img = img.reshape((height, width, -1))
+        return img
+
+    def __call__(self, results):
+        img = results['img']
+        if np.random.rand() < self.prob_cutout:
+            img = self.cutout(img)
+        if np.random.rand() < self.prob_has:
+            img = self.hide_patch(img)
+        results['img'] = img
+        return results
