bcb4a5235d1e94735c43263a05db86460df860c4
jin-s13 <jinsheng13@foxmail.com>
2020-12-16 15:49:18 +0800

    add warnings

--- mmpose/datasets/datasets/top_down/topdown_jhmdb_dataset.py
+++ mmpose/datasets/datasets/top_down/topdown_jhmdb_dataset.py
@@ -60,7 +60,12 @@ class TopDownJhmdbDataset(TopDownCocoDataset):
 
         self.use_gt_bbox = data_cfg['use_gt_bbox']
         self.bbox_file = data_cfg['bbox_file']
-        self.det_bbox_thr = data_cfg['det_bbox_thr']
+        self.det_bbox_thr = data_cfg.get('det_bbox_thr', 0.0)
+        if 'image_thr' in data_cfg:
+            warnings.warn(
+                'image_thr is deprecated, '
+                'please use det_bbox_thr instead', DeprecationWarning)
+            self.det_bbox_thr = data_cfg['image_thr']
         self.soft_nms = data_cfg['soft_nms']
         self.nms_thr = data_cfg['nms_thr']
         self.oks_thr = data_cfg['oks_thr']
