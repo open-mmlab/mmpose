472173f0d7017179bd744b1344dc07b83b946e52
wusize <72066971+wusize@users.noreply.github.com>
2020-12-19 19:56:31 +0800

    rm bbox_id in mspn configs (#373)

--- configs/top_down/mspn/coco/mspn50_coco_256x192.py
+++ configs/top_down/mspn/coco/mspn50_coco_256x192.py
@@ -106,7 +106,7 @@ train_pipeline = [
         keys=['img', 'target', 'target_weight'],
         meta_keys=[
             'image_file', 'joints_3d', 'joints_3d_visible', 'center', 'scale',
-            'rotation', 'bbox_score', 'flip_pairs', 'bbox_id'
+            'rotation', 'bbox_score', 'flip_pairs'
         ]),
 ]
 
@@ -125,7 +125,7 @@ val_pipeline = [
         ],
         meta_keys=[
             'image_file', 'center', 'scale', 'rotation', 'bbox_score',
-            'flip_pairs', 'bbox_id'
+            'flip_pairs'
         ]),
 ]
 
