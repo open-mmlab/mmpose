c2ef1a72887a78952267a3a50c15839d924699ef
luminxu <luminxu@link.cuhk.edu.hk>
2020-12-29 17:24:34 +0800

    add pose tracking demo

--- mmpose/apis/inference_tracking.py
+++ mmpose/apis/inference_tracking.py
@@ -17,12 +17,12 @@ def iou(boxA, boxB):
     xB = min(boxA[2], boxB[2])
     yB = min(boxA[3], boxB[3])
 
-    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    inter_area = max(0, xB - xA + 1) * max(0, yB - yA + 1)
 
-    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    boxA_area = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxB_area = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
 
-    iou = interArea / float(boxAArea + boxBArea - interArea)
+    iou = inter_area / float(boxA_area + boxB_area - inter_area)
 
     return iou
 
