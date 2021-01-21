7b4a5869c294caf9250f4901b53f91466c64ec8e
jin-s13 <jinsheng13@foxmail.com>
2021-01-21 17:05:27 +0800

    add tests

--- tests/test_datasets/test_bottom_up_dataset.py
+++ tests/test_datasets/test_bottom_up_dataset.py
@@ -197,3 +197,56 @@ def test_bottom_up_MHP_dataset():
 
         with pytest.raises(KeyError):
             _ = custom_dataset.evaluate(outputs, tmpdir, 'PCK')
+
+
+def test_bottom_up_AIC_dataset():
+    dataset = 'BottomUpAicDataset'
+    # test MHP datasets
+    dataset_class = DATASETS.get(dataset)
+
+    channel_cfg = dict(
+        num_output_channels=14,
+        dataset_joints=14,
+        dataset_channel=[
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+        ],
+        inference_channel=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+
+    data_cfg = dict(
+        image_size=512,
+        base_size=256,
+        base_sigma=2,
+        heatmap_size=[128],
+        num_joints=channel_cfg['dataset_joints'],
+        dataset_channel=channel_cfg['dataset_channel'],
+        inference_channel=channel_cfg['inference_channel'],
+        num_scales=1,
+        scale_aware_sigma=False,
+    )
+
+    _ = dataset_class(
+        ann_file='tests/data/aic/test_aic.json',
+        img_prefix='tests/data/aic/',
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=False)
+
+    custom_dataset = dataset_class(
+        ann_file='tests/data/aic/test_aic.json',
+        img_prefix='tests/data/aic/',
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=True)
+
+    image_id = 1
+    assert image_id in custom_dataset.img_ids
+    assert len(custom_dataset.img_ids) == 3
+    _ = custom_dataset[0]
+
+    outputs = convert_coco_to_output(custom_dataset.coco)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        infos = custom_dataset.evaluate(outputs, tmpdir, 'mAP')
+        assert_almost_equal(infos['AP'], 1.0)
+
+        with pytest.raises(KeyError):
+            _ = custom_dataset.evaluate(outputs, tmpdir, 'PCK')
