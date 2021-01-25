237c32dd1f97cca9b7a3e1e53053bd07a20ea746
lizz <innerlee@users.noreply.github.com>
2021-01-25 12:40:06 +0800

    Fix fashion doc (#435)
    * Fix fashion doc
    
    Signed-off-by: lizz <lizz@sensetime.com>
    
    * Fix fashion dataset doc
    
    Signed-off-by: lizz <lizz@sensetime.com>

--- docs/merge_docs.sh
+++ docs/merge_docs.sh
@@ -5,34 +5,40 @@ sed -i '$a\\n' ../configs/top_down/*/*.md
 sed -i '$a\\n' ../demo/*_demo.md
 sed -i '$a\\n' ../configs/hand/*/*.md
 sed -i '$a\\n' ../configs/mesh/*/*.md
+sed -i '$a\\n' ../configs/fashion/*/*.md
 
 cat ../configs/bottom_up/*/*.md >bottom_up_models.md
 cat ../configs/top_down/*/*.md >top_down_models.md
 cat ../demo/*_demo.md >demo.md
 cat ../configs/hand/*/*.md >hand_models.md
 cat ../configs/mesh/*/*.md >mesh_models.md
+cat ../configs/fashion/*/*.md >fashion_models.md
 
 sed -i "s/#/#&/" bottom_up_models.md
 sed -i "s/#/#&/" top_down_models.md
 sed -i "s/#/#&/" demo.md
 sed -i "s/#/#&/" hand_models.md
 sed -i "s/#/#&/" mesh_models.md
+sed -i "s/#/#&/" fashion_models.md
 sed -i "s/md###t/html#t/g" bottom_up_models.md
 sed -i "s/md###t/html#t/g" top_down_models.md
 sed -i "s/md###t/html#t/g" demo.md
 sed -i "s/md###t/html#t/g" hand_models.md
 sed -i "s/md###t/html#t/g" mesh_models.md
+sed -i "s/md###t/html#t/g" fashion_models.md
 
 sed -i '1i\# Bottom Up Models' bottom_up_models.md
 sed -i '1i\# Top Down Models' top_down_models.md
 sed -i '1i\# Demo' demo.md
 sed -i '1i\# Hand Models' hand_models.md
 sed -i '1i\# Mesh Models' mesh_models.md
+sed -i '1i\# Fashion Models' fashion_models.md
 
 sed -i 's/](\/docs\//](/g' bottom_up_models.md # remove /docs/ for link used in doc site
 sed -i 's/](\/docs\//](/g' top_down_models.md
 sed -i 's/](\/docs\//](/g' hand_models.md
 sed -i 's/](\/docs\//](/g' mesh_models.md
+sed -i 's/](\/docs\//](/g' fashion_models.md
 sed -i 's/](\/docs\//](/g' ./tutorials/*.md
 sed -i 's/](\/docs\//](/g' data_preparation.md
 sed -i 's/](\/docs\//](/g' ./tasks/*.md
@@ -41,6 +47,7 @@ sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' bottom_up_m
 sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' top_down_models.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' hand_models.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' mesh_models.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' fashion_models.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' getting_started.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' install.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' benchmark.md
