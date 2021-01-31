da88b6a9fba98368875f7eb74bf8ae9500dcc4f9
Jas <jinsheng@sensetime.com>
2021-01-31 23:59:57 +0800

    Bottom-up AIC (#438)
    * add bu aic
    
    * add aic
    
    * add tests
    
    * add hrnet aic
    
    * fix aic sigmas
    
    * update aic config and checkpoints
    
    * update udp config
    
    * update config

--- mmpose/datasets/datasets/bottom_up/__init__.py
+++ mmpose/datasets/datasets/bottom_up/__init__.py
@@ -1,7 +1,9 @@
+from .bottom_up_aic import BottomUpAicDataset
 from .bottom_up_coco import BottomUpCocoDataset
 from .bottom_up_crowdpose import BottomUpCrowdPoseDataset
 from .bottom_up_mhp import BottomUpMhpDataset
 
 __all__ = [
-    'BottomUpCocoDataset', 'BottomUpCrowdPoseDataset', 'BottomUpMhpDataset'
+    'BottomUpCocoDataset', 'BottomUpCrowdPoseDataset', 'BottomUpMhpDataset',
+    'BottomUpAicDataset'
 ]
