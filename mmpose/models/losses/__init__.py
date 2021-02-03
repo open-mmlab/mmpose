5541c68b44b4f5cdecbf5d057a81659be43717bd
jinsheng <jinsheng@sensetime.com>
2021-02-03 22:23:04 +0800

    add regression

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -1,9 +1,9 @@
 from .mesh_loss import GANLoss, MeshLoss
 from .mse_loss import JointsMSELoss, JointsOHKMMSELoss
 from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
-from .regression_loss import SmoothL1Loss
+from .regression_loss import SmoothL1Loss, WingLoss
 
 __all__ = [
     'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss',
-    'MultiLossFactory', 'MeshLoss', 'GANLoss', 'SmoothL1Loss'
+    'MultiLossFactory', 'MeshLoss', 'GANLoss', 'SmoothL1Loss', 'WingLoss'
 ]
--- mmpose/models/losses/regression_loss.py
+++ mmpose/models/losses/regression_loss.py
@@ -1,3 +1,6 @@
+import math
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -31,6 +34,56 @@ class SmoothL1Loss(nn.Module):
         if self.use_target_weight:
             loss = self.criterion(output * target_weight,
                                   target * target_weight)
+        else:
+            loss = self.criterion(output, target)
+
+        return loss / num_joints * self.loss_weight
+
+
+@LOSSES.register_module()
+class WingLoss(nn.Module):
+    """Wing Loss 'Wing Loss for Robust Facial Landmark Localisation with
+    Convolutional Neural Networks' Feng et al.
+
+    CVPR'2018
+    """
+
+    def __init__(self,
+                 omega=10,
+                 epsilon=2,
+                 use_target_weight=False,
+                 loss_weight=1.):
+        super().__init__()
+        self.omega = omega
+        self.epsilon = epsilon
+        self.use_target_weight = use_target_weight
+        self.loss_weight = loss_weight
+
+    def criterion(self, pred, target):
+        delta = (target - pred).abs()
+        c = self.omega * (1.0 - math.log(1.0 + self.omega / self.epsilon))
+        losses = torch.where(
+            torch.lt(delta, self.omega),
+            self.omega * torch.log(1.0 + delta / self.epsilon), delta - c)
+        return torch.mean(torch.sum(losses, dim=[1, 2]), dim=0)
+
+    def forward(self, output, target, target_weight):
+        """Forward function.
+
+        Note:
+            batch_size: N
+            num_keypoints: K
+
+        Args:
+            output (torch.Tensor[N, K, 2]): Output regression.
+            target (torch.Tensor[N, K, 2]): Target regression.
+            target_weight (torch.Tensor[N, K, 2]):
+                Weights across different joint types.
+        """
+        num_joints = output.size(1)
+        if self.use_target_weight:
+            loss = self.criterion(
+                output.mul(target_weight), target.mul(target_weight))
         else:
             loss = self.criterion(output, target)
 
