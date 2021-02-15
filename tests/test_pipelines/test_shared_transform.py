da1c74c0eb925d5932693256430aef4d0ee15301
Jas <jinsheng@sensetime.com>
2021-02-16 00:02:41 +0800

    add albu (#469)
    * add albu
    
    * remove Normalize
    
    * resolve comments
    
    * albu to albumentation
    
    * add an example for keymap

---
+++ tests/test_pipelines/test_shared_transform.py
@@ -0,0 +1,30 @@
+import os.path as osp
+
+import numpy as np
+from mmcv.utils import build_from_cfg
+
+from mmpose.datasets import PIPELINES
+
+
+def test_albu_transform():
+    data_prefix = 'tests/data/coco/'
+    results = dict(image_file=osp.join(data_prefix, '000000000785.jpg'))
+
+    # Define simple pipeline
+    load = dict(type='LoadImageFromFile')
+    load = build_from_cfg(load, PIPELINES)
+
+    albu_transform = dict(
+        type='Albumentation',
+        transforms=[
+            dict(type='RandomBrightnessContrast', p=0.2),
+            dict(type='ToFloat')
+        ])
+    albu_transform = build_from_cfg(albu_transform, PIPELINES)
+
+    # Execute transforms
+    results = load(results)
+
+    results = albu_transform(results)
+
+    assert results['img'].dtype == np.float32
