f238daa2768bc9af89dc8af6596fb7a90a306cb4
jinsheng <jinsheng@sensetime.com>
2021-02-20 21:44:39 +0800

    fix code format

--- mmpose/models/losses/regression_loss.py
+++ mmpose/models/losses/regression_loss.py
@@ -94,8 +94,8 @@ class WingLoss(nn.Module):
         """
         num_joints = output.size(1)
         if self.use_target_weight:
-            loss = self.criterion(
-                output.mul(target_weight), target.mul(target_weight))
+            loss = self.criterion(output * target_weight,
+                                  target * target_weight)
         else:
             loss = self.criterion(output, target)
 
