13d44d09260d95880b3aa504f93441eaab1ee347
Jas <jinsheng@sensetime.com>
2021-02-24 11:50:43 +0800

    add seed option (#493)

--- mmpose/datasets/builder.py
+++ mmpose/datasets/builder.py
@@ -77,7 +77,7 @@ def build_dataloader(dataset,
     rank, world_size = get_dist_info()
     if dist:
         sampler = DistributedSampler(
-            dataset, world_size, rank, shuffle=shuffle)
+            dataset, world_size, rank, shuffle=shuffle, seed=seed)
         shuffle = False
         batch_size = samples_per_gpu
         num_workers = workers_per_gpu
