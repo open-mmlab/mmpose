39531926c3bddd4217d40070c19d40fe8f26e700
lizz <innerlee@users.noreply.github.com>
2021-02-24 15:13:43 +0800

    fix (#498)

--- docs/merge_docs.sh
+++ docs/merge_docs.sh
@@ -9,14 +9,14 @@ sed -i '$a\\n' ../configs/mesh/*/*.md
 sed -i '$a\\n' ../configs/fashion/*/*.md
 sed -i '$a\\n' ../demo/*_demo.md
 
-cat ../configs/bottom_up/*/*.md | sed -i "s/#/#&/" | sed -i "s/md###t/html#t/g" | sed -i '1i\# Bottom Up Models' | sed -i 's/](\/docs\//](/g' | sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >bottom_up_models.md
-cat ../configs/top_down/*/*.md | sed -i "s/#/#&/" | sed -i "s/md###t/html#t/g" | sed -i '1i\# Top Down Models' | sed -i 's/](\/docs\//](/g' | sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >top_down_models.md
-cat ../configs/wholebody/*/*.md | sed -i "s/#/#&/" | sed -i "s/md###t/html#t/g" | sed -i '1i\# Whole-Body Models' | sed -i 's/](\/docs\//](/g' | sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >wholebody_models.md
-cat ../configs/hand/*/*.md | sed -i "s/#/#&/" | sed -i "s/md###t/html#t/g" | sed -i '1i\# Hand Models' | sed -i 's/](\/docs\//](/g' | sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >hand_models.md
-cat ../configs/face/*/*.md | sed -i "s/#/#&/" | sed -i "s/md###t/html#t/g" | sed -i '1i\# Face Models' | sed -i 's/](\/docs\//](/g' | sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >face_models.md
-cat ../configs/mesh/*/*.md | sed -i "s/#/#&/" | sed -i "s/md###t/html#t/g" | sed -i '1i\# Mesh Models' | sed -i 's/](\/docs\//](/g' | sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >mesh_models.md
-cat ../configs/fashion/*/*.md | sed -i "s/#/#&/" | sed -i "s/md###t/html#t/g" | sed -i '1i\# Fashion Models' | sed -i 's/](\/docs\//](/g' | sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >fashion_models.md
-cat ../demo/*_demo.md | sed -i "s/#/#&/" | sed -i "s/md###t/html#t/g" | sed -i '1i\# Demo' | sed -i 's/](\/docs\//](/g' | sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >demo.md
+cat ../configs/bottom_up/*/*.md | sed "s/#/#&/" | sed "s/md###t/html#t/g" | sed '1i\# Bottom Up Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >bottom_up_models.md
+cat ../configs/top_down/*/*.md | sed "s/#/#&/" | sed "s/md###t/html#t/g" | sed '1i\# Top Down Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >top_down_models.md
+cat ../configs/wholebody/*/*.md | sed "s/#/#&/" | sed "s/md###t/html#t/g" | sed '1i\# Whole-Body Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >wholebody_models.md
+cat ../configs/hand/*/*.md | sed "s/#/#&/" | sed "s/md###t/html#t/g" | sed '1i\# Hand Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >hand_models.md
+cat ../configs/face/*/*.md | sed "s/#/#&/" | sed "s/md###t/html#t/g" | sed '1i\# Face Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >face_models.md
+cat ../configs/mesh/*/*.md | sed "s/#/#&/" | sed "s/md###t/html#t/g" | sed '1i\# Mesh Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >mesh_models.md
+cat ../configs/fashion/*/*.md | sed "s/#/#&/" | sed "s/md###t/html#t/g" | sed '1i\# Fashion Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >fashion_models.md
+cat ../demo/*_demo.md | sed "s/#/#&/" | sed "s/md###t/html#t/g" | sed '1i\# Demo' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/master/=g' >demo.md
 
  # remove /docs/ for link used in doc site
 sed -i 's/](\/docs\//](/g' ./tutorials/*.md
