ffadb141b60432dd2241c4706da7ad052cbea182
ly015 <liyining0712@gmail.com>
2021-03-18 13:54:41 +0800

    Add 3D pose pipeline
    * Add transforms on joint coordinates in pipelines
    * Add camera projection in pipelines
    * Add camera interface in mmpose.core

---
+++ mmpose/datasets/pipelines/pose3d_transform.py
@@ -0,0 +1,214 @@
+import mmcv
+import numpy as np
+from mmcv.utils import build_from_cfg
+
+from mmpose.core.camera import CAMERAS
+from mmpose.datasets.registry import PIPELINES
+
+
+@PIPELINES.register_module()
+class JointRelativization:
+    """Zero-center the pose around a given root joint. The root joint will be
+    removed from the original pose and optionally stored as a separate item.
+
+    Note that the relativized joints no longer align with some annotation
+    information (e.g. flip_pairs, num_joints, inference_channel, etal) due to
+    the removal of the root joint.
+
+    Args:
+        item (str): The name of the pose to relativeze.
+        root_index (int): Root joint index in the pose.
+        root_name (str): Optional. If not none, it will be used as the key to
+            store the root position separated from the original pose.
+
+    Required keys:
+        item (depend on args): Should be an array in shape [...,Kj,C], where
+            the joint number Kj should be greater than root_index.
+    Modified keys:
+        item, root_name (depend on args), ann_info (add relative flip pairs)
+    """
+
+    def __init__(self, item, root_index, root_name=None):
+        self.item = item
+        self.root_index = root_index
+        self.root_name = root_name
+
+    def __call__(self, results):
+        assert self.item in results
+        joints = results[self.item]
+        root_idx = self.root_index
+
+        assert joints.ndim >= 2 and joints.shape[-1] > root_idx,\
+            f'Got invalid joint shape {joints.shape}'
+        root = joints[..., root_idx:root_idx + 1, :]
+        joints = np.delete(joints - root, root_idx, axis=-2)
+
+        results[self.item] = joints
+        if self.root_name is not None:
+            results[self.root_name] = root
+        return results
+
+
+@PIPELINES.register_module()
+class JointNormalization:
+    """Normalize the joint coordinate with given mean and std.
+
+    Args:
+        item (str): The name of the pose to normalize.
+        mean (array): Mean values of joint coordiantes in shape [Kj, C].
+        std (array): Std values of joint coordinates in shape [Kj, C].
+
+    Required keys:
+        item (depend on args): Should be an array in shape [...,Kj,C], where
+            the joint number Kj should be greater than root_index.
+    Modified keys:
+        item
+    """
+
+    def __init__(self, item, mean=None, std=None, norm_param_file=None):
+        self.item = item
+        self.norm_param_file = norm_param_file
+        if norm_param_file is not None:
+            norm_param = mmcv.load(norm_param_file)
+            assert 'mean' in norm_param and 'std' in norm_param
+            self.mean = norm_param['mean']
+            self.std = norm_param['std']
+        else:
+            self.mean = mean
+            self.std = std
+
+    def __call__(self, results):
+        assert self.item in results
+        results[self.item] = (results[self.item] - self.mean) / self.std
+        return results
+
+
+@PIPELINES.register_module()
+class CameraProjection:
+    """Apply camera projection to joint coordinates.
+
+    Args:
+        item (str): The name of the pose to apply camera projection.
+        mode (str): The type of camera projection, supported options are
+            - world_to_camera
+            - world_to_pixel
+            - camera_to_world
+            - camera_to_pixel
+        output_name (str|None): The name of the projected pose. If None
+            (default) is given, the projected pose will be stored inplace.
+        camera_type (str): The camera class name (should be registered in
+            CAMERA).
+        camera_param (dict|None): The camera parameter dict. See the camera
+            class definition for more details. If None is given, the camera
+            parameter will be obtained during processing of each data sample
+            with the key "camara_param".
+
+    Required keys:
+        item (depend on args): Should be an array in shape [...,Kj,C]
+        camera_param (if camera parameters are not given in initialization)
+    Modified keys:
+        output_name (depend on args)
+    """
+
+    def __init__(self,
+                 item,
+                 mode,
+                 output_name=None,
+                 camera_type='SimpleCamera',
+                 camera_param=None):
+        self.item = item
+        self.mode = mode
+        self.output_name = output_name
+        self.camera_type = camera_type
+        allowed_mode = {
+            'world_to_camera',
+            'world_to_pixel',
+            'camera_to_world',
+            'camera_to_pixel',
+        }
+        if mode not in allowed_mode:
+            raise ValueError(
+                f'Got invalid mode: {mode}, allowed modes are {allowed_mode}')
+
+        if camera_param is None:
+            self.static_camera = False
+        else:
+            self.static_camera = True
+            self.camera = self._build_camera(camera_param)
+
+    def _build_camera(self, param):
+        cfgs = dict(type=self.camera_type, param=param)
+        return build_from_cfg(cfgs, CAMERAS)
+
+    def __call__(self, results):
+        assert self.item in results
+        joints = results[self.item]
+
+        if self.static_camera:
+            camera = self.camera
+        else:
+            assert 'camera_param' in results, 'Camera parameters are missing.'
+            camera = self._build_camera(results['camera_param'])
+
+        if self.mode == 'world_to_camera':
+            output = camera.world_to_camera(joints)
+        elif self.mode == 'world_to_pixel':
+            output = camera.world_to_pixel(joints)
+        elif self.mode == 'camera_to_world':
+            output = camera.camera_to_world(joints)
+        elif self.mode == 'camera_to_pixel':
+            output = camera.camera_to_pixel(joints)
+        else:
+            raise NotImplementedError
+
+        output_name = self.output_name
+        if output_name is None:
+            output_name = self.item
+
+        results[output_name] = output
+        return results
+
+
+@PIPELINES.register_module()
+class RelativeJointRandomFlip:
+    """Data augmentation with random horizontal joint flip around a root
+    joint."""
+
+    def __init__(self, item, root_index, visible_item=None, flip_prob=0.5):
+        self.item = item
+        self.root_index = root_index
+        self.vis_item = visible_item
+        self.flip_prob = flip_prob
+
+    def __call__(self, results):
+
+        if np.random.rand() <= self.flip_prob:
+
+            flip_pairs = results['ann_info']['flip_pairs']
+            # flip joint coordinates
+            assert self.item in results
+            joints = results[self.item]
+
+            root_idx = self.root_index
+            assert joints.ndim >= 2 and joints.shape[-1] > root_idx,\
+                f'Got invalid joint shape {joints.shape}'
+
+            joints_flipped = joints.copy()
+            for left, right in flip_pairs:
+                assert root_idx not in {left, right}
+                joints_flipped[..., left, :] = joints[..., right, :]
+                joints_flipped[..., right, :] = joints[..., left, :]
+            joints_flipped[..., 0] = -joints_flipped[..., 0] + joints[
+                ..., root_idx:root_idx + 1, 0] * 2
+            results[self.item] = joints_flipped
+            # flip joint visibility
+            if self.vis_item is not None:
+                assert self.vis_item in results
+                visible = results[self.vis_item]
+                visible_flipped = visible.copy()
+                for left, right in flip_pairs:
+                    visible_flipped[..., left, :] = visible[..., right, :]
+                    visible_flipped[..., right, :] = visible[..., left, :]
+                results[self.vis_item] = visible_flipped
+
+        return results
---
+++ tests/test_pipelines/test_pose3d_transform.py
@@ -0,0 +1,149 @@
+import copy
+import os.path as osp
+
+import mmcv
+import numpy as np
+
+from mmpose.datasets.pipelines import Compose
+
+H36M_JOINT_IDX = [14, 2, 1, 0, 3, 4, 5, 16, 12, 17, 18, 9, 10, 11, 8, 7, 6]
+
+
+def get_data_sample():
+
+    def _parse_h36m_imgname(imgname):
+        """Parse imgname to get information of subject, action and camera.
+
+        A typical h36m image filename is like:
+        S1_Directions_1.54138969_000001.jpg
+        """
+        subj, rest = osp.basename(imgname).split('_', 1)
+        action, rest = rest.split('.', 1)
+        camera, rest = rest.split('_', 1)
+        return subj, action, camera
+
+    ann_flle = 'tests/data/h36m/test_h36m.npz'
+    camera_param_file = 'tests/data/h36m/cameras.pkl'
+
+    data = np.load(ann_flle)
+    cameras = mmcv.load(camera_param_file)
+
+    _imgnames = data['imgname']
+    _joints_2d = data['part'][:, H36M_JOINT_IDX].astype(np.float32)
+    _joints_3d = data['S'][:, H36M_JOINT_IDX].astype(np.float32)
+    _joints_3d[..., :3] *= 1000.
+    _centers = data['center'].astype(np.float32)
+    _scales = data['scale'].astype(np.float32)
+
+    frame_ids = [0]
+    target_frame_id = 0
+
+    results = {
+        'frame_ids': frame_ids,
+        'target_frame_id': target_frame_id,
+        'input_2d': _joints_2d[frame_ids, :, :2],
+        'input_2d_visible': _joints_2d[frame_ids, :, -1:],
+        'input_3d': _joints_3d[frame_ids, :, :3],
+        'input_3d_visible': _joints_3d[frame_ids, :, -1:],
+        'target': _joints_3d[target_frame_id, :, :3],
+        'target_visible': _joints_3d[target_frame_id, :, -1:],
+        'imgnames': _imgnames[frame_ids],
+        'scales': _scales[frame_ids],
+        'centers': _centers[frame_ids],
+    }
+
+    # add camera parameters
+    subj, _, camera = _parse_h36m_imgname(_imgnames[frame_ids[0]])
+    results['camera_param'] = cameras[(subj, camera)]
+
+    # add ann_info
+    ann_info = {}
+    ann_info['num_joints'] = 17
+    ann_info['joint_weights'] = np.full(17, 1.0, dtype=np.float32)
+    ann_info['flip_pairs'] = [[1, 4], [2, 5], [3, 6], [11, 14], [12, 15],
+                              [13, 16]]
+    ann_info['upper_body_ids'] = (0, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+    ann_info['lower_body_ids'] = (1, 2, 3, 4, 5, 6)
+    ann_info['use_different_joint_weights'] = False
+
+    results['ann_info'] = ann_info
+
+    return results
+
+
+def test_joint_transforms():
+    results = get_data_sample()
+
+    mean = np.random.rand(16, 3).astype(np.float32)
+    std = np.random.rand(16, 3).astype(np.float32) + 1e-6
+
+    pipeline = [
+        dict(
+            type='RelativeJointRandomFlip',
+            item='target',
+            root_index=0,
+            visible_item='target_visible',
+            flip_prob=1.),
+        dict(
+            type='JointRelativization',
+            item='target',
+            root_index=0,
+            root_name='global_position'),
+        dict(type='JointNormalization', item='target', mean=mean, std=std),
+        dict(
+            type='Collect',
+            keys=[('target', 'output'), 'flip_pairs'],
+            meta_keys=[])
+    ]
+
+    pipeline = Compose(pipeline)
+    output = pipeline(copy.deepcopy(results))
+
+    joints_0 = results['target']
+    joints_1 = output['output']
+
+    # manually do transformations
+    flip_pairs = output['flip_pairs']
+    _joints_0_flipped = joints_0.copy()
+    for _l, _r in flip_pairs:
+        _joints_0_flipped[..., _l, :] = joints_0[..., _r, :]
+        _joints_0_flipped[..., _r, :] = joints_0[..., _l, :]
+
+    _joints_0_flipped[...,
+                      0] = 2 * joints_0[..., 0:1, 0] - _joints_0_flipped[...,
+                                                                         0]
+
+    joints_0 = _joints_0_flipped
+    joints_0 = (joints_0[..., 1:, :] - joints_0[..., 0:1, :] - mean) / std
+
+    np.testing.assert_array_almost_equal(joints_0, joints_1)
+
+
+def test_camera_projection():
+    results = get_data_sample()
+    pipeline = [
+        dict(
+            type='CameraProjection',
+            item='input_3d',
+            output_name='input_3d_w',
+            camera_type='SimpleCamera',
+            mode='camera_to_world'),
+        dict(
+            type='CameraProjection',
+            item='input_3d_w',
+            output_name='input_3d_wp',
+            camera_type='SimpleCamera',
+            mode='world_to_pixel'),
+        dict(
+            type='CameraProjection',
+            item='input_3d',
+            output_name='input_3d_p',
+            camera_type='SimpleCamera',
+            mode='camera_to_pixel'),
+        dict(type='Collect', keys=['input_3d_wp', 'input_3d_p'], meta_keys=[])
+    ]
+
+    pipeline = Compose(pipeline)
+    output = pipeline(results)
+    np.testing.assert_allclose(
+        output['input_3d_wp'], output['input_3d_p'], rtol=1e-6)
