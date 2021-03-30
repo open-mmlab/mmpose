52260050a059fb57612e1b5235ecb26a44092d07
ly015 <liyining0712@gmail.com>
2021-03-30 23:57:15 +0800

    Update mmcv version dependence (#544)
    * Update mmcv version dependence
    
    * update ci config
    
    * update ci config
    
    * update ci mmcv install command
    
    * fix mmcv version in ci config
    
    * pytorch version 1.5.1->1.5.0 in ci config
    
    * fix mmcv version range
    
    * Update build.yml
    
    * Update build.yml
    
    * fix torch1.3.0+cu101 install command
    
    Co-authored-by: lizz <innerlee@users.noreply.github.com>

--- mmpose/__init__.py
+++ mmpose/__init__.py
@@ -4,7 +4,7 @@ from mmcv import digit_version, parse_version_info
 from .version import __version__, short_version
 
 mmcv_minimum_version = '1.1.3'
-mmcv_maximum_version = '1.3'
+mmcv_maximum_version = '1.4.0'
 mmcv_version = digit_version(mmcv.__version__)
 version_info = parse_version_info(__version__)
 
