339f31ff433bd242025836d157ee9c504d8eeeb1
ly015 <liyining0712@gmail.com>
2021-03-31 00:20:53 +0800

    fix importing

--- mmpose/core/post_processing/__init__.py
+++ mmpose/core/post_processing/__init__.py
@@ -1,13 +1,11 @@
 from .nms import oks_nms, soft_oks_nms
 from .post_transforms import (affine_transform, flip_back, fliplr_joints,
-                              fliplr_regression, fliplr_regression_3d,
-                              get_affine_transform, get_warp_matrix,
-                              rotate_point, transform_preds,
+                              fliplr_regression, get_affine_transform,
+                              get_warp_matrix, rotate_point, transform_preds,
                               warp_affine_joints)
 
 __all__ = [
     'oks_nms', 'soft_oks_nms', 'affine_transform', 'rotate_point', 'flip_back',
-    'fliplr_joints', 'fliplr_regression', 'fliplr_regression_3d',
-    'transform_preds', 'get_affine_transform', 'get_warp_matrix',
-    'warp_affine_joints'
+    'fliplr_joints', 'fliplr_regression', 'transform_preds',
+    'get_affine_transform', 'get_warp_matrix', 'warp_affine_joints'
 ]
