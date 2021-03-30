9cb1b87b4c12c23eb2e012310d1466307608c40a
ly015 <liyining0712@gmail.com>
2021-03-30 19:28:30 +0800

    fix mmcv version range

--- mmpose/__init__.py
+++ mmpose/__init__.py
@@ -3,8 +3,8 @@ from mmcv import digit_version, parse_version_info
 
 from .version import __version__, short_version
 
-mmcv_minimum_version = '1.2.6'
-mmcv_maximum_version = '1.4'
+mmcv_minimum_version = '1.1.3'
+mmcv_maximum_version = '1.4.0'
 mmcv_version = digit_version(mmcv.__version__)
 version_info = parse_version_info(__version__)
 
