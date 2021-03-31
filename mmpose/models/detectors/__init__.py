a84d2cae7236d2dd3744b2dc4b77617c78c25885
liuxin9608 <69199111+liuxin9608@users.noreply.github.com>
2021-03-31 15:55:32 +0800

    add multitask detector (#480)
    * add multitask detector
    
    * Add multitask detector needed for interhand 3d task.
    
    * update the test codes of multitask detector.
    
    * fix some bugs.
    
    Co-authored-by: zengwang430521 <zengwang430521@gmail.com>

--- mmpose/models/detectors/__init__.py
+++ mmpose/models/detectors/__init__.py
@@ -1,5 +1,6 @@
 from .bottom_up import BottomUp
 from .mesh import ParametricMesh
+from .multi_task import MultiTask
 from .top_down import TopDown
 
-__all__ = ['TopDown', 'BottomUp', 'ParametricMesh']
+__all__ = ['TopDown', 'BottomUp', 'ParametricMesh', 'MultiTask']
