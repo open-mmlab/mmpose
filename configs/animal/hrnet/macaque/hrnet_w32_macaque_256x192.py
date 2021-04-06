652e55f388afbfa148f378faf13288324a9397e5
jinsheng <jinsheng@sensetime.com>
2021-04-06 10:46:10 +0800

    update config

--- configs/animal/hrnet/macaque/hrnet_w32_macaque_256x192.py
+++ configs/animal/hrnet/macaque/hrnet_w32_macaque_256x192.py
@@ -104,6 +104,10 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='TopDownRandomFlip', flip_prob=0.5),
     dict(
+        type='TopDownHalfBodyTransform',
+        num_joints_half_body=8,
+        prob_half_body=0.3),
+    dict(
         type='TopDownGetRandomScaleRotation', rot_factor=40, scale_factor=0.5),
     dict(type='TopDownAffine'),
     dict(type='ToTensor'),
