d595da69c4382e07bb202cc96afc624e1401874d
Jas <jinsheng@sensetime.com>
2021-04-14 19:28:53 +0800

    ATRW tiger dataset (#571)
    * add atrw dataset
    
    * add atrw configs
    
    * add animal readme
    
    * add atrw
    
    * update log interval
    
    * update readme
    
    * update readme
    
    * update init

--- mmpose/datasets/__init__.py
+++ mmpose/datasets/__init__.py
@@ -1,27 +1,26 @@
 from .builder import build_dataloader, build_dataset
-from .datasets import (AnimalFlyDataset, AnimalHorse10Dataset,
-                       AnimalLocustDataset, AnimalMacaqueDataset,
-                       AnimalZebraDataset, BottomUpCocoDataset,
-                       BottomUpCrowdPoseDataset, BottomUpMhpDataset,
-                       DeepFashionDataset, Face300WDataset, FreiHandDataset,
-                       InterHand2DDataset, MeshAdversarialDataset,
-                       MeshH36MDataset, MeshMixDataset, MoshDataset,
-                       OneHand10KDataset, PanopticDataset, TopDownAicDataset,
-                       TopDownCocoDataset, TopDownCocoWholeBodyDataset,
-                       TopDownCrowdPoseDataset, TopDownFreiHandDataset,
-                       TopDownJhmdbDataset, TopDownMhpDataset,
-                       TopDownMpiiDataset, TopDownMpiiTrbDataset,
-                       TopDownOCHumanDataset, TopDownOneHand10KDataset,
-                       TopDownPanopticDataset, TopDownPoseTrack18Dataset)
 from .pipelines import Compose
 from .registry import DATASETS, PIPELINES
 from .samplers import DistributedSampler
 
+from .datasets import (  # isort:skip
+    AnimalFlyDataset, AnimalATRWDataset, AnimalHorse10Dataset,
+    AnimalLocustDataset, AnimalMacaqueDataset, AnimalZebraDataset,
+    BottomUpCocoDataset, BottomUpCrowdPoseDataset, BottomUpMhpDataset,
+    DeepFashionDataset, Face300WDataset, FreiHandDataset, InterHand2DDataset,
+    MeshAdversarialDataset, MeshH36MDataset, MeshMixDataset, MoshDataset,
+    OneHand10KDataset, PanopticDataset, TopDownAicDataset, TopDownCocoDataset,
+    TopDownCocoWholeBodyDataset, TopDownCrowdPoseDataset,
+    TopDownFreiHandDataset, TopDownJhmdbDataset, TopDownMhpDataset,
+    TopDownMpiiDataset, TopDownMpiiTrbDataset, TopDownOCHumanDataset,
+    TopDownOneHand10KDataset, TopDownPanopticDataset,
+    TopDownPoseTrack18Dataset)
+
 __all__ = [
-    'TopDownCocoDataset', 'BottomUpCocoDataset', 'BottomUpMhpDataset',
-    'TopDownMpiiDataset', 'TopDownMpiiTrbDataset', 'OneHand10KDataset',
-    'PanopticDataset', 'FreiHandDataset', 'InterHand2DDataset',
-    'TopDownOCHumanDataset', 'TopDownAicDataset',
+    'AnimalATRWDataset', 'TopDownCocoDataset', 'BottomUpCocoDataset',
+    'BottomUpMhpDataset', 'TopDownMpiiDataset', 'TopDownMpiiTrbDataset',
+    'OneHand10KDataset', 'PanopticDataset', 'FreiHandDataset',
+    'InterHand2DDataset', 'TopDownOCHumanDataset', 'TopDownAicDataset',
     'TopDownCocoWholeBodyDataset', 'DeepFashionDataset', 'MeshH36MDataset',
     'MeshMixDataset', 'MoshDataset', 'MeshAdversarialDataset',
     'TopDownCrowdPoseDataset', 'BottomUpCrowdPoseDataset',
--- tests/test_datasets/test_animal_dataset.py
+++ tests/test_datasets/test_animal_dataset.py
@@ -268,3 +268,119 @@ def test_animal_zebra_dataset():
 
         with pytest.raises(KeyError):
             infos = custom_dataset.evaluate(outputs, tmpdir, 'mAP')
+
+
+def test_animal_ATRW_dataset():
+    dataset = 'AnimalATRWDataset'
+    dataset_class = DATASETS.get(dataset)
+
+    channel_cfg = dict(
+        num_output_channels=15,
+        dataset_joints=15,
+        dataset_channel=[
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        ],
+        inference_channel=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+
+    data_cfg = dict(
+        image_size=[256, 256],
+        heatmap_size=[64, 64],
+        num_output_channels=channel_cfg['num_output_channels'],
+        num_joints=channel_cfg['dataset_joints'],
+        dataset_channel=channel_cfg['dataset_channel'],
+        inference_channel=channel_cfg['inference_channel'],
+        soft_nms=False,
+        nms_thr=1.0,
+        oks_thr=0.9,
+        vis_thr=0.2,
+        use_gt_bbox=True,
+        det_bbox_thr=0.0,
+        bbox_file='',
+    )
+
+    # Test
+    data_cfg_copy = copy.deepcopy(data_cfg)
+    _ = dataset_class(
+        ann_file='tests/data/atrw/test_atrw.json',
+        img_prefix='tests/data/atrw/',
+        data_cfg=data_cfg_copy,
+        pipeline=[],
+        test_mode=True)
+
+    custom_dataset = dataset_class(
+        ann_file='tests/data/atrw/test_atrw.json',
+        img_prefix='tests/data/atrw/',
+        data_cfg=data_cfg_copy,
+        pipeline=[],
+        test_mode=False)
+
+    assert custom_dataset.test_mode is False
+    assert custom_dataset.num_images == 2
+    _ = custom_dataset[0]
+
+    outputs = convert_db_to_output(custom_dataset.db)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        infos = custom_dataset.evaluate(outputs, tmpdir, 'mAP')
+        assert_almost_equal(infos['AP'], 1.0)
+
+        with pytest.raises(KeyError):
+            infos = custom_dataset.evaluate(outputs, tmpdir, ['PCK'])
+
+
+def test_animal_Macaque_dataset():
+    dataset = 'AnimalMacaqueDataset'
+    dataset_class = DATASETS.get(dataset)
+
+    channel_cfg = dict(
+        num_output_channels=17,
+        dataset_joints=17,
+        dataset_channel=[
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        ],
+        inference_channel=[
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+        ])
+
+    data_cfg = dict(
+        image_size=[192, 256],
+        heatmap_size=[48, 64],
+        num_output_channels=channel_cfg['num_output_channels'],
+        num_joints=channel_cfg['dataset_joints'],
+        dataset_channel=channel_cfg['dataset_channel'],
+        inference_channel=channel_cfg['inference_channel'],
+        soft_nms=False,
+        nms_thr=1.0,
+        oks_thr=0.9,
+        vis_thr=0.2,
+        use_gt_bbox=True,
+        det_bbox_thr=0.0,
+        bbox_file='',
+    )
+
+    # Test
+    data_cfg_copy = copy.deepcopy(data_cfg)
+    _ = dataset_class(
+        ann_file='tests/data/macaque/test_macaque.json',
+        img_prefix='tests/data/macaque/',
+        data_cfg=data_cfg_copy,
+        pipeline=[],
+        test_mode=True)
+
+    custom_dataset = dataset_class(
+        ann_file='tests/data/macaque/test_macaque.json',
+        img_prefix='tests/data/macaque/',
+        data_cfg=data_cfg_copy,
+        pipeline=[],
+        test_mode=False)
+
+    assert custom_dataset.test_mode is False
+    assert custom_dataset.num_images == 2
+    _ = custom_dataset[0]
+
+    outputs = convert_db_to_output(custom_dataset.db)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        infos = custom_dataset.evaluate(outputs, tmpdir, 'mAP')
+        assert_almost_equal(infos['AP'], 1.0)
+
+        with pytest.raises(KeyError):
+            infos = custom_dataset.evaluate(outputs, tmpdir, ['PCK'])
