55f3ea57590a0aa7673548047c560d6b88e2e6de
jinsheng <jinsheng@sensetime.com>
2021-04-14 19:25:36 +0800

    update init

--- mmpose/datasets/__init__.py
+++ mmpose/datasets/__init__.py
@@ -1,10 +1,33 @@
 from .builder import build_dataloader, build_dataset
-from .datasets import *  # noqa
 from .pipelines import Compose
 from .registry import DATASETS, PIPELINES
 from .samplers import DistributedSampler
 
+from .datasets import (  # isort:skip
+    AnimalFlyDataset, AnimalATRWDataset, AnimalHorse10Dataset,
+    AnimalLocustDataset, AnimalMacaqueDataset, AnimalZebraDataset,
+    BottomUpCocoDataset, BottomUpCrowdPoseDataset, BottomUpMhpDataset,
+    DeepFashionDataset, Face300WDataset, FreiHandDataset, InterHand2DDataset,
+    MeshAdversarialDataset, MeshH36MDataset, MeshMixDataset, MoshDataset,
+    OneHand10KDataset, PanopticDataset, TopDownAicDataset, TopDownCocoDataset,
+    TopDownCocoWholeBodyDataset, TopDownCrowdPoseDataset,
+    TopDownFreiHandDataset, TopDownJhmdbDataset, TopDownMhpDataset,
+    TopDownMpiiDataset, TopDownMpiiTrbDataset, TopDownOCHumanDataset,
+    TopDownOneHand10KDataset, TopDownPanopticDataset,
+    TopDownPoseTrack18Dataset)
+
 __all__ = [
-    'build_dataloader', 'build_dataset', 'Compose', 'DistributedSampler',
-    'DATASETS', 'PIPELINES'
+    'AnimalATRWDataset', 'TopDownCocoDataset', 'BottomUpCocoDataset',
+    'BottomUpMhpDataset', 'TopDownMpiiDataset', 'TopDownMpiiTrbDataset',
+    'OneHand10KDataset', 'PanopticDataset', 'FreiHandDataset',
+    'InterHand2DDataset', 'TopDownOCHumanDataset', 'TopDownAicDataset',
+    'TopDownCocoWholeBodyDataset', 'DeepFashionDataset', 'MeshH36MDataset',
+    'MeshMixDataset', 'MoshDataset', 'MeshAdversarialDataset',
+    'TopDownCrowdPoseDataset', 'BottomUpCrowdPoseDataset',
+    'TopDownFreiHandDataset', 'TopDownOneHand10KDataset',
+    'TopDownPanopticDataset', 'TopDownPoseTrack18Dataset',
+    'TopDownJhmdbDataset', 'TopDownMhpDataset', 'Face300WDataset',
+    'AnimalHorse10Dataset', 'AnimalMacaqueDataset', 'AnimalFlyDataset',
+    'AnimalLocustDataset', 'AnimalZebraDataset', 'build_dataloader',
+    'build_dataset', 'Compose', 'DistributedSampler', 'DATASETS', 'PIPELINES'
 ]
