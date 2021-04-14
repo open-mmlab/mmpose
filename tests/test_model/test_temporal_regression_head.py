741d559f3dfb37b6e891c3d94ccd50355daf5ce1
ly015 <liyining0712@gmail.com>
2021-04-14 14:46:09 +0800

    resolve codecov

--- tests/test_model/test_temporal_regression_head.py
+++ tests/test_model/test_temporal_regression_head.py
@@ -41,7 +41,7 @@ def test_temporal_regression_head():
     _ = head.inference_model(inputs, [(0, 1), (2, 3)])
     metas = [{}]
 
-    acc = head.get_accuracy(out, out, metas=metas)
+    acc = head.get_accuracy(out, out, None, metas=metas)
     assert acc['mpjpe'] == 0.
     np.testing.assert_almost_equal(acc['p_mpjpe'], 0.)
 
