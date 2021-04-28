077c3dd701f1a28a76a354454efb4ce6a5379e00
yingjm <yingjm@126.com>
2021-04-28 11:43:40 +0800

    codecov

--- tests/test_datasets/test_body3d_dataset.py
+++ tests/test_datasets/test_body3d_dataset.py
@@ -53,14 +53,15 @@ def test_body3d_h36m_dataset():
         np.testing.assert_almost_equal(infos['P-MPJPE'], 0.0)
         np.testing.assert_almost_equal(infos['N-MPJPE'], 0.0)
 
-    # test multi-frame input
+    # test multi-frame input with joint_2d_src = 'detection'
     data_cfg = dict(
         num_joints=17,
         seq_len=27,
         seq_frame_interval=1,
         causal=True,
         pad=True,
-        joint_2d_src='gt',
+        joint_2d_src='detection',
+        joint_2d_det_file='tests/data/h36m/test_h36m_2d_detection.npy',
         need_camera_param=True,
         camera_param_file='tests/data/h36m/cameras.pkl')
 
