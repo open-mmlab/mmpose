82e1e564447b55587b51e9675bbc1a8bc141d165
jinsheng <jinsheng@sensetime.com>
2021-05-11 20:02:56 +0800

    add resnest

--- configs/top_down/resnest/coco/resnest269_coco_256x192.py
+++ configs/top_down/resnest/coco/resnest269_coco_256x192.py
@@ -116,7 +116,7 @@ test_pipeline = val_pipeline
 
 data_root = 'data/coco'
 data = dict(
-    samples_per_gpu=64,
+    samples_per_gpu=32,
     workers_per_gpu=2,
     train=dict(
         type='TopDownCocoDataset',
--- configs/top_down/resnest/coco/resnest269_coco_384x288.py
+++ configs/top_down/resnest/coco/resnest269_coco_384x288.py
@@ -116,7 +116,7 @@ test_pipeline = val_pipeline
 
 data_root = 'data/coco'
 data = dict(
-    samples_per_gpu=32,
+    samples_per_gpu=16,
     workers_per_gpu=2,
     train=dict(
         type='TopDownCocoDataset',
