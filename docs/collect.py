8c6f2ab1f380e682e41ada31f9ea69c42398db18
jinsheng <jinsheng@sensetime.com>
2021-05-25 20:27:01 +0800

    update

--- docs/collect.py
+++ docs/collect.py
@@ -14,22 +14,19 @@ def anchor(name):
 # Count algorithms
 root_dir = '../configs'
 
-for root, dirs, files in os.walk(root_dir, topdown=True):
-    if '_result' in dirs and '_result_collection' in dirs:
-        collect_files = sorted(
-            glob.glob(os.path.join(root, '_result_collection/*.md')))
+collect_files = sorted(glob.glob('configs/README/*.md'))
+for cf in collect_files:
+    with open(cf, 'r') as collect_content_file:
+        collect_content = collect_content_file.read()
+        collect_papers = set(
+            (papertype, titlecase.titlecase(paper.lower().strip()))
+            for (papertype, paper) in re.findall(
+                r'<!--\s*\[([A-Z]*?)\]\s*-->\s*\n.*?\btitle\s*=\s*{(.*?)}',
+                collect_content, re.DOTALL))
 
-        for cf in collect_files:
-            with open(cf, 'r') as collect_content_file:
-                collect_content = collect_content_file.read()
-                collect_papers = set((
-                    papertype, titlecase.titlecase(paper.lower().strip())
-                ) for (papertype, paper) in re.findall(
-                    r'<!--\s*\[([A-Z]*?)\]\s*-->\s*\n.*?\btitle\s*=\s*{(.*?)}',
-                    collect_content, re.DOTALL))
-
+    for root, dirs, files in os.walk(root_dir, topdown=True):
+        if '_result' in dirs:
             res_files = sorted(glob.glob(os.path.join(root, '_result/*.md')))
-
             for rf in res_files:
                 with open(rf, 'r') as res_content_file:
                     res_content = res_content_file.read()
