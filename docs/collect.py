74d08bcbd2b40526010ae509c7abd7c4e0ea1ea2
lizz <lizz@sensetime.com>
2021-06-01 22:50:10 +0800

    script
    Signed-off-by: lizz <lizz@sensetime.com>

--- docs/collect.py
+++ docs/collect.py
@@ -1,78 +1,51 @@
 #!/usr/bin/env python
 import glob
-import os
-import re
 
-import titlecase
+from titlecase import titlecase
 
+# Step 1: get subtopics: a mix of topic and task
+sections = [x.split('/')[-2:] for x in glob.glob('../configs/*/*')]
+alltopics = sorted(list(set(x[0] for x in sections)))
+subtopics = []
+for t in alltopics:
+    data = [x[1].split('_') for x in sections if x[0] == t]
+    valid_ids = []
+    for i in range(len(data[0])):
+        if len(set(x[i] for x in data)) > 1:
+            valid_ids.append(i)
+    if len(valid_ids) > 0:
+        subtopics.extend(
+            [f"{t}({','.join([d[i] for i in valid_ids])})", t, '_'.join(d)]
+            for d in data)
+    else:
+        subtopics.append([t, t, '_'.join(data[0])])
 
-def anchor(name):
-    return re.sub(r'-+', '-', re.sub(r'[^a-zA-Z0-9]', '-',
-                                     name.strip().lower()))
+contents = {}
+for subtopic, topic, task in sorted(subtopics):
+    # Step 2: get all datasets
+    datasets = sorted(
+        list(
+            set(
+                x.split('/')[-2]
+                for x in glob.glob(f'../configs/{topic}/{task}/*/*/'))))
+    contents[subtopic] = {d: {} for d in datasets}
+    for dataset in datasets:
+        # Step 3: get all settings: algorithm + backbone + trick
+        for file in glob.glob(f'../configs/{topic}/{task}/*/{dataset}/*.md'):
+            keywords = (file.split('/')[-3],
+                        *file.split('/')[-1].split('_')[:-1])
+            with open(file, 'r') as f:
+                contents[subtopic][dataset][keywords] = f.read()
 
+# Step 4: write files
+for subtopic, datasets in contents.items():
+    lines = [f'# {titlecase(subtopic)}', '']
+    for dataset, keywords in datasets.items():
+        lines += [f'## {titlecase(dataset)}', '']
+        for keyword, info in keywords.items():
+            lines += [
+                f'### {" + ".join(map(titlecase, keyword))}', '', info, ''
+            ]
 
-# Count algorithms
-root_dir = '../configs'
-
-collect_files = sorted(glob.glob('../configs/README/*/*.md'))
-for cf in collect_files:
-    res_dict = {}
-    with open(cf, 'r') as collect_content_file:
-        collect_content = collect_content_file.read()
-        collect_papers = set(
-            (papertype, titlecase.titlecase(paper.lower().strip()))
-            for (papertype, paper) in re.findall(
-                r'<!--\s*\[([A-Z]*?)\]\s*-->\s*\n.*?\btitle\s*=\s*{(.*?)}',
-                collect_content, re.DOTALL))
-
-    for root, dirs, files in os.walk(root_dir, topdown=True):
-        SPL = root.split('/')
-        if '_result' in dirs:
-            res_files = sorted(glob.glob(os.path.join(root, '_result/*.md')))
-            for rf in res_files:
-                with open(rf, 'r') as res_content_file:
-                    res_content = res_content_file.read()
-                    res_papers = set(
-                        (papertype, titlecase.titlecase(paper.lower().strip()))
-                        for (papertype, paper) in re.findall(
-                            r'<!--\s*\[([A-Z]*?)\]\s*-->'
-                            r'\s*\n.*?\btitle\s*=\s*{(.*?)}', res_content,
-                            re.DOTALL))
-
-                    Flag = True
-                    for paper in collect_papers:
-                        if paper not in res_papers:
-                            Flag = False
-                            break
-                    if Flag:
-                        topic = SPL[-3]
-                        task = SPL[-2]
-                        algorithm = SPL[-1]
-
-                        if task == '2d_kpt_sview_rgb_img':
-                            task_str = 'Single-view RGB image based 2D ' \
-                                f'{topic} keypoint estimation'
-                        elif task == '3d_kpt_sview_rgb_img':
-                            task_str = 'Single-view RGB image based 3D ' \
-                                f'{topic} keypoint estimation'
-                        elif task == '3d_kpt_sview_rgb_vid':
-                            task_str = 'Single-view RGB video based 3D ' \
-                                f'{topic} keypoint estimation'
-                        elif task == '3d_mesh_sview_rgb_img':
-                            task_str = 'Single-view RGB image based 3D ' \
-                                f'{topic} mesh recovery'
-
-                        if task_str not in res_dict:
-                            res_dict[task_str] = [
-                                res_content[res_content.find('####'):]
-                            ]
-                        else:
-                            res_dict[task_str].append(
-                                res_content[res_content.find('####'):])
-
-    with open(cf, 'a') as file_out:
-        for (key, value) in res_dict.items():
-            file_out.write(f'\n### {key}\n\n')
-            for v in value:
-                file_out.write(v)
-                file_out.write('\n')
+    with open(f'{subtopic}.md', 'w') as f:
+        f.write('\n'.join(lines))
