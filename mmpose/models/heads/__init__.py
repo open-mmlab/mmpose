1ad31f385ef2b93994ee62d52036ee9ed8e41653
luminxu <luminxu@link.cuhk.edu.hk>
2021-06-03 17:33:51 +0800

    add vipnas_heatmap_simple_head

--- mmpose/models/heads/__init__.py
+++ mmpose/models/heads/__init__.py
@@ -8,10 +8,12 @@ from .topdown_heatmap_base_head import TopdownHeatmapBaseHead
 from .topdown_heatmap_multi_stage_head import (TopdownHeatmapMSMUHead,
                                                TopdownHeatmapMultiStageHead)
 from .topdown_heatmap_simple_head import TopdownHeatmapSimpleHead
+from .vipnas_heatmap_simple_head import ViPNASHeatmapSimpleHead
 
 __all__ = [
     'TopdownHeatmapSimpleHead', 'TopdownHeatmapMultiStageHead',
     'TopdownHeatmapMSMUHead', 'TopdownHeatmapBaseHead',
     'AEHigherResolutionHead', 'AESimpleHead', 'DeepposeRegressionHead',
-    'TemporalRegressionHead', 'Interhand3DHead', 'HMRMeshHead'
+    'TemporalRegressionHead', 'Interhand3DHead', 'HMRMeshHead',
+    'ViPNASHeatmapSimpleHead'
 ]
