3e77abf4197bd41847ef989f44be5e7a52178300
luminxu <luminxu@link.cuhk.edu.hk>
2021-06-04 15:39:10 +0800

    .

--- mmpose/models/heads/topdown_heatmap_simple_head.py
+++ mmpose/models/heads/topdown_heatmap_simple_head.py
@@ -191,9 +191,10 @@ class TopdownHeatmapSimpleHead(TopdownHeatmapBaseHead):
 
     def forward(self, x):
         """Forward function."""
-        x = self._transform_inputs(x)
+
         print(x.size(), flush=True)
         exit()
+        x = self._transform_inputs(x)
         x = self.deconv_layers(x)
         x = self.final_layer(x)
         return x
