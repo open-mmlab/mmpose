390eb6f08930c7b180cae0186ffcfde6807f691d
luminxu <luminxu@link.cuhk.edu.hk>
2021-06-04 16:09:20 +0800

    .

--- mmpose/models/backbones/vipnas_resnet.py
+++ mmpose/models/backbones/vipnas_resnet.py
@@ -677,6 +677,9 @@ class ViPNAS_ResNet(BaseBackbone):
             x = res_layer(x)
             if i in self.out_indices:
                 outs.append(x)
+        print(len(outs))
+        print(outs[0].size())
+        exit()
         if len(outs) == 1:
             return outs[0]
         return tuple(outs)
