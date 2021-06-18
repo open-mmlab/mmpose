adca568a83bd18833849448e451e491149c1b258
ly015 <liyining0712@gmail.com>
2021-06-18 14:45:10 +0800

    add unittest for backward compatibility

---
+++ tests/test_backward_compatibility/test_registry.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+def test_old_fashion_registry_importing():
+    with pytest.warns(DeprecationWarning):
+        from mmpose.models.registry import (  # noqa: F401
+            BACKBONES, NECKS, HEADS, LOSSES, POSENETS)
+    with pytest.warns(DeprecationWarning):
+        from mmpose.datasets.registry import DATASETS, PIPELINES  # noqa: F401
