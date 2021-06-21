d733619554c5ab35f5b33d663155d340f5634485
jinsheng <jinsheng@sensetime.com>
2021-06-21 17:40:25 +0800

    add adaptive wingloss

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -3,7 +3,7 @@ from .classfication_loss import BCELoss
 from .mesh_loss import GANLoss, MeshLoss
 from .mse_loss import JointsMSELoss, JointsOHKMMSELoss
 from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
-from .regression_loss import (BoneLoss, L1Loss, MPJPELoss, MSELoss,
+from .regression_loss import (AdaptiveWingLoss, BoneLoss, L1Loss, MPJPELoss, MSELoss,
                               SemiSupervisionLoss, SmoothL1Loss, SoftWingLoss,
                               WingLoss)
 
@@ -11,5 +11,5 @@ __all__ = [
     'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss',
     'MultiLossFactory', 'MeshLoss', 'GANLoss', 'SmoothL1Loss', 'WingLoss',
     'MPJPELoss', 'MSELoss', 'L1Loss', 'BCELoss', 'BoneLoss',
-    'SemiSupervisionLoss', 'SoftWingLoss'
+    'SemiSupervisionLoss', 'SoftWingLoss', 'AdaptiveWingLoss'
 ]
