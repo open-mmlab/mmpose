2f741e9829ea2a458b9bf3bbeb4030d499b2d485
cherryjm <47238203+cherryjm@users.noreply.github.com>
2021-06-23 14:09:09 +0800

    Add MPI-INF-3DHP dataset (#683)
    * add mpi-inf-3dhp dataset
    
    * increase code coverage
    
    * use mmcv.VideoReader
    
    * fix bug when need_camera_param is False
    
    * fix bug in reading image size
    
    * fix unittest & add README
    
    * increase code coverage
    
    * modify evaluation protocol and update README
    
    * remove raise error

--- mmpose/core/evaluation/__init__.py
+++ mmpose/core/evaluation/__init__.py
@@ -2,7 +2,7 @@ from .bottom_up_eval import (aggregate_results, get_group_preds,
                              get_multi_stage_outputs)
 from .eval_hooks import DistEvalHook, EvalHook
 from .mesh_eval import compute_similarity_transform
-from .pose3d_eval import keypoint_mpjpe
+from .pose3d_eval import keypoint_3d_auc, keypoint_3d_pck, keypoint_mpjpe
 from .top_down_eval import (keypoint_auc, keypoint_epe, keypoint_pck_accuracy,
                             keypoints_from_heatmaps, keypoints_from_heatmaps3d,
                             keypoints_from_regression,
@@ -15,5 +15,5 @@ __all__ = [
     'keypoint_epe', 'get_group_preds', 'get_multi_stage_outputs',
     'aggregate_results', 'compute_similarity_transform', 'post_dark_udp',
     'keypoint_mpjpe', 'keypoints_from_heatmaps3d',
-    'multilabel_classification_accuracy'
+    'multilabel_classification_accuracy', 'keypoint_3d_pck', 'keypoint_3d_auc'
 ]
