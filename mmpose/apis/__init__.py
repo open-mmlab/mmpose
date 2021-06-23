c6381c023d788151531a0c1a4e6f1f2dc39fe4e5
zengwang430521 <zengwang430521@gmail.com>
2021-06-23 16:01:48 +0800

    Interhand3d demo (#704)
    * add demo for interhand3d

--- mmpose/apis/__init__.py
+++ mmpose/apis/__init__.py
@@ -1,7 +1,8 @@
 from .inference import (inference_bottom_up_pose_model,
                         inference_top_down_pose_model, init_pose_model,
                         vis_pose_result)
-from .inference_3d import inference_pose_lifter_model, vis_3d_pose_result
+from .inference_3d import (inference_interhand_3d_model,
+                           inference_pose_lifter_model, vis_3d_pose_result)
 from .inference_tracking import get_track_id, vis_pose_tracking_result
 from .test import multi_gpu_test, single_gpu_test
 from .train import train_model
@@ -10,5 +11,6 @@ __all__ = [
     'train_model', 'init_pose_model', 'inference_top_down_pose_model',
     'inference_bottom_up_pose_model', 'multi_gpu_test', 'single_gpu_test',
     'vis_pose_result', 'get_track_id', 'vis_pose_tracking_result',
-    'inference_pose_lifter_model', 'vis_3d_pose_result'
+    'inference_pose_lifter_model', 'vis_3d_pose_result',
+    'inference_interhand_3d_model'
 ]
