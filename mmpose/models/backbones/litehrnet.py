0764ad60c1dd03777479de8c6a04eb0ccfec7357
jinsheng <jinsheng@sensetime.com>
2021-07-07 15:04:23 +0800

    add notes

--- mmpose/models/backbones/litehrnet.py
+++ mmpose/models/backbones/litehrnet.py
@@ -720,6 +720,7 @@ class LiteHRModule(nn.Module):
         if self.with_fuse:
             out_fuse = []
             for i in range(len(self.fuse_layers)):
+                # `y = 0` will lead to decreased accuracy (0.5~1 mAP)
                 y = out[0] if i == 0 else self.fuse_layers[i][0](out[0])
                 for j in range(self.num_branches):
                     if i == j:
