272c0cafb4b3266048806cfda990a32b98a19f28
ly015 <liyining0712@gmail.com>
2021-07-23 15:02:26 +0800

    update according to mmcv #1076

--- mmpose/core/evaluation/eval_hooks.py
+++ mmpose/core/evaluation/eval_hooks.py
@@ -1,18 +1,16 @@
-import os.path as osp
 import tempfile
 import warnings
 
-import torch.distributed as dist
+from mmcv.runner import DistEvalHook as _DistEvalHook
 from mmcv.runner import EvalHook as _EvalHook
-from torch.nn.modules.batchnorm import _BatchNorm
 
-_MMPOSE_GREATER_KEYS = ['acc', 'ap', 'ar', 'pck', 'auc', '3dpck', 'p-3dpck', '3dauc', 'p-3dauc']
-_MMPOSE_LESS_KEYS = ['loss', 'epe', 'nme', 'mpjpe', 'p-mpjpe', 'n-mpjpe']
+MMPOSE_GREATER_KEYS = [
+    'acc', 'ap', 'ar', 'pck', 'auc', '3dpck', 'p-3dpck', '3dauc', 'p-3dauc'
+]
+MMPOSE_LESS_KEYS = ['loss', 'epe', 'nme', 'mpjpe', 'p-mpjpe', 'n-mpjpe']
 
 
 class EvalHook(_EvalHook):
-    greater_keys = _MMPOSE_GREATER_KEYS
-    less_keys = _MMPOSE_LESS_KEYS
 
     def __init__(self,
                  dataloader,
@@ -21,8 +19,16 @@ class EvalHook(_EvalHook):
                  by_epoch=True,
                  save_best=None,
                  rule=None,
+                 test_fn=None,
+                 greater_keys=MMPOSE_GREATER_KEYS,
+                 less_keys=MMPOSE_LESS_KEYS,
                  **eval_kwargs):
-        # to bo compatible with the config before v0.15.0
+
+        if test_fn is None:
+            from mmpose.apis import single_gpu_test
+            test_fn = single_gpu_test
+
+        # to bo compatible with the config before v0.16.0
 
         # remove "gpu_collect" from eval_kwargs
         if 'gpu_collect' in eval_kwargs:
@@ -46,24 +52,7 @@ class EvalHook(_EvalHook):
             save_best = key_indicator
 
         super().__init__(dataloader, start, interval, by_epoch, save_best,
-                         rule, **eval_kwargs)
-
-    def _init_rule(self, rule, key_indicator):
-        # key_indicator is case-insensitive
-        super()._init_rule(rule, key_indicator.lower())
-        self.key_indicator = key_indicator
-
-    def _do_evaluate(self, runner):
-        """perform evaluation and save ckpt."""
-        if not self._should_evaluate(runner):
-            return
-
-        from mmpose.apis import single_gpu_test
-        results = single_gpu_test(runner.model, self.dataloader)
-        runner.log_buffer.output['eval_iter_num'] = len(self.dataloader)
-        key_score = self.evaluate(runner, results)
-        if self.save_best:
-            self._save_ckpt(runner, key_score)
+                         rule, test_fn, greater_keys, less_keys, **eval_kwargs)
 
     def evaluate(self, runner, results):
         """Evaluate the results.
@@ -92,7 +81,7 @@ class EvalHook(_EvalHook):
         return None
 
 
-class DistEvalHook(EvalHook):
+class DistEvalHook(_DistEvalHook):
 
     def __init__(self,
                  dataloader,
@@ -101,18 +90,19 @@ class DistEvalHook(EvalHook):
                  by_epoch=True,
                  save_best=None,
                  rule=None,
+                 test_fn=None,
+                 greater_keys=MMPOSE_GREATER_KEYS,
+                 less_keys=MMPOSE_LESS_KEYS,
                  broadcast_bn_buffer=True,
                  tmpdir=None,
                  gpu_collect=False,
                  **eval_kwargs):
-        # to bo compatible with the config before v0.15.0
 
-        # remove "gpu_collect" from eval_kwargs
-        if 'gpu_collect' in eval_kwargs:
-            warnings.warn(
-                '"gpu_collect" will be deprecated in EvalHook.'
-                'Please remove it from the config.', DeprecationWarning)
-            _ = eval_kwargs.pop('gpu_collect')
+        if test_fn is None:
+            from mmpose.apis import multi_gpu_test
+            test_fn = multi_gpu_test
+
+        # to bo compatible with the config before v0.16.0
 
         # update "save_best" according to "key_indicator" and remove the
         # latter from eval_kwargs
@@ -129,44 +119,32 @@ class DistEvalHook(EvalHook):
             save_best = key_indicator
 
         super().__init__(dataloader, start, interval, by_epoch, save_best,
-                         rule, **eval_kwargs)
+                         rule, test_fn, greater_keys, less_keys,
+                         broadcast_bn_buffer, tmpdir, gpu_collect,
+                         **eval_kwargs)
 
-        self.broadcast_bn_buffer = broadcast_bn_buffer
-        self.tmpdir = tmpdir
-        self.gpu_collect = gpu_collect
+    def evaluate(self, runner, results):
+        """Evaluate the results.
 
-    def _do_evaluate(self, runner):
-        """perform evaluation and save ckpt."""
-        # Synchronization of BatchNorm's buffer (running_mean
-        # and running_var) is not supported in the DDP of pytorch,
-        # which may cause the inconsistent performance of models in
-        # different ranks, so we broadcast BatchNorm's buffers
-        # of rank 0 to other ranks to avoid this.
-        if self.broadcast_bn_buffer:
-            model = runner.model
-            for name, module in model.named_modules():
-                if isinstance(module,
-                              _BatchNorm) and module.track_running_stats:
-                    dist.broadcast(module.running_var, 0)
-                    dist.broadcast(module.running_mean, 0)
+        Args:
+            runner (:obj:`mmcv.Runner`): The underlined training runner.
+            results (list): Output results.
+        """
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            eval_res = self.dataloader.dataset.evaluate(
+                results,
+                res_folder=tmp_dir,
+                logger=runner.logger,
+                **self.eval_kwargs)
 
-        if not self._should_evaluate(runner):
-            return
+        for name, val in eval_res.items():
+            runner.log_buffer.output[name] = val
+        runner.log_buffer.ready = True
 
-        tmpdir = self.tmpdir
-        if tmpdir is None:
-            tmpdir = osp.join(runner.work_dir, '.eval_hook')
+        if self.save_best is not None:
+            if self.key_indicator == 'auto':
+                # infer from eval_results
+                self._init_rule(self.rule, list(eval_res.keys())[0])
+            return eval_res[self.key_indicator]
 
-        from mmpose.apis import multi_gpu_test
-        results = multi_gpu_test(
-            runner.model,
-            self.dataloader,
-            tmpdir=tmpdir,
-            gpu_collect=self.gpu_collect)
-        if runner.rank == 0:
-            print('\n')
-            runner.log_buffer.output['eval_iter_num'] = len(self.dataloader)
-            key_score = self.evaluate(runner, results)
-
-            if self.save_best:
-                self._save_ckpt(runner, key_score)
+        return None
