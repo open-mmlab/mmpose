6c868ea568be944c7cb511983ccdfd71a892f579
Yining Li <liyining0712@gmail.com>
2021-08-03 17:00:27 +0800

    Move process_mmdet_results into inference.py (#831)
    * move process_mmdet_results into inference.py
    * add unittest

--- demo/top_down_video_demo_with_mmdet.py
+++ demo/top_down_video_demo_with_mmdet.py
@@ -4,35 +4,13 @@ from argparse import ArgumentParser
 import cv2
 
 from mmpose.apis import (inference_top_down_pose_model, init_pose_model,
-                         vis_pose_result)
+                         process_mmdet_results, vis_pose_result)
 
 try:
     from mmdet.apis import inference_detector, init_detector
     has_mmdet = True
 except (ImportError, ModuleNotFoundError):
     has_mmdet = False
-
-
-def process_mmdet_results(mmdet_results, cat_id=1):
-    """Process mmdet results, and return a list of bboxes.
-
-    :param mmdet_results:
-    :param cat_id: category id (default: 1 for human)
-    :return: a list of detected bounding boxes
-    """
-    if isinstance(mmdet_results, tuple):
-        det_results = mmdet_results[0]
-    else:
-        det_results = mmdet_results
-
-    bboxes = det_results[cat_id - 1]
-    person_results = []
-    for bbox in bboxes:
-        person = {}
-        person['bbox'] = bbox
-        person_results.append(person)
-
-    return person_results
 
 
 def main():
