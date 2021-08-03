c64d0ce8a43ddd17417a1bc5d7a22e99ec91651f
金晟 <SENSETIME\jinsheng@stit10.domain.sensetime.com>
2021-08-03 14:05:16 +0800

    update configs

--- configs/body/2d_kpt_sview_rgb_img/part_affinity_field/coco/lightweight_openpose_coco_368x368_withbg_addneck_lr.py
+++ configs/body/2d_kpt_sview_rgb_img/part_affinity_field/coco/lightweight_openpose_coco_368x368_withbg_addneck_lr.py
@@ -24,7 +24,7 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
+        dict(type='TensorboardLoggerHook')
     ])
 
 channel_cfg = dict(
