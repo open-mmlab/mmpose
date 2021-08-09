153065fdd9d7e791952b5bf3747f24c95897eedb
ZhiYuan-REN:-) <52560835+ZhiYuan-REN@users.noreply.github.com>
2021-08-09 15:36:53 +0800

    Add unittest for None as target_weights in losses (#847)
    * Modify unittests

--- tests/test_loss/test_classification_loss.py
+++ tests/test_loss/test_classification_loss.py
@@ -4,18 +4,18 @@ import torch
 def test_bce_loss():
     from mmpose.models import build_loss
 
-    # test BCE loss without target weight
+    # test BCE loss without target weight(None)
     loss_cfg = dict(type='BCELoss')
     loss = build_loss(loss_cfg)
 
     fake_pred = torch.zeros((1, 2))
     fake_label = torch.zeros((1, 2))
-    assert torch.allclose(loss(fake_pred, fake_label, None), torch.tensor(0.))
+    assert torch.allclose(loss(fake_pred, fake_label), torch.tensor(0.))
 
     fake_pred = torch.ones((1, 2)) * 0.5
     fake_label = torch.zeros((1, 2))
     assert torch.allclose(
-        loss(fake_pred, fake_label, None), -torch.log(torch.tensor(0.5)))
+        loss(fake_pred, fake_label), -torch.log(torch.tensor(0.5)))
 
     # test BCE loss with target weight
     loss_cfg = dict(type='BCELoss', use_target_weight=True)
