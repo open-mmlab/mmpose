b343b1480a39e45e2489d197cf3e7fbc502d7a2a
ly015 <liyining0712@gmail.com>
2021-08-11 18:23:36 +0800

    update digit_version function

--- mmpose/__init__.py
+++ mmpose/__init__.py
@@ -1,19 +1,59 @@
+import warnings
+
 import mmcv
-from mmcv import digit_version, parse_version_info
+from mmcv import parse_version_info
+from packaging.version import parse
 
 from .version import __version__, short_version
 
 mmcv_minimum_version = '1.3.8'
-mmcv_maximum_version = '1.4.0'
+mmcv_maximum_version = '1.5.0'
+
+
+def digit_version(version_str: str, length: int = 4):
+    """Convert a version string into a tuple of integers.
+
+    This method is usually used for comparing two versions. For pre-release
+    versions: alpha < beta < rc.
+    Args:
+        version_str (str): The version string.
+        length (int): The maximum number of version levels. Default: 4.
+    Returns:
+        tuple[int]: The version info in digits (integers).
+    """
+    version = parse(version_str)
+    assert version.release, f'failed to parse version {version_str}'
+    release = list(version.release)
+    release = release[:length]
+    if len(release) < length:
+        release = release + [0] * (length - len(release))
+    if version.is_prerelease:
+        mapping = {'a': -3, 'b': -2, 'rc': -1}
+        val = -4
+        # version.pre can be None
+        if version.pre:
+            if version.pre[0] not in mapping:
+                warnings.warn(f'unknown prerelease version {version.pre[0]}, '
+                              'version checking may go wrong')
+            else:
+                val = mapping[version.pre[0]]
+            release.extend([val, version.pre[-1]])
+        else:
+            release.extend([val, 0])
+
+    elif version.is_postrelease:
+        release.extend([1, version.post])
+    else:
+        release.extend([0, 0])
+    return tuple(release)
+
+
 mmcv_version = digit_version(mmcv.__version__)
 version_info = parse_version_info(__version__)
 
-assert digit_version(mmcv_minimum_version) <= mmcv_version, \
-        f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-        f'Please install mmcv>={mmcv_minimum_version}.'
-
-assert digit_version(mmcv_maximum_version) > mmcv_version, \
-        f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-        f'Please install mmcv<{mmcv_maximum_version}.'
+assert (mmcv_version >= digit_version(mmcv_minimum_version)
+        and mmcv_version <= digit_version(mmcv_maximum_version)), \
+    f'MMCV=={mmcv.__version__} is used but incompatible. ' \
+    f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
 
 __all__ = ['__version__', 'short_version', 'version_info']
