cfd91db04a6b38d9180f72b9d9655189dd5c4295
ly015 <liyining0712@gmail.com>
2021-08-12 16:09:18 +0800

    fix unittest

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,8 +1,12 @@
+import mmcv
+
 import mmpose
 
 
 def test_version():
     version = mmpose.__version__
+    mmcv_version = mmpose.digit_version(mmcv.__version__)
     assert isinstance(version, str)
     assert isinstance(mmpose.short_version, str)
     assert mmpose.short_version in version
+    assert mmcv_version == mmpose.mmcv_version
