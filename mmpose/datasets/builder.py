3567852947ba9eb0afcc1dfc896a116b44e053f6
luminxu <luminxu@zju.edu.cn>
2021-08-23 10:38:22 +0800

    release resource limit

--- mmpose/datasets/builder.py
+++ mmpose/datasets/builder.py
@@ -10,13 +10,13 @@ from mmcv.utils.parrots_wrapper import _get_dataloader
 
 from .samplers import DistributedSampler
 
-if platform.system() != 'Windows':
-    # https://github.com/pytorch/pytorch/issues/973
-    import resource
-    rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
-    hard_limit = rlimit[1]
-    soft_limit = min(4096, hard_limit)
-    resource.setrlimit(resource.RLIMIT_NOFILE, (soft_limit, hard_limit))
+# if platform.system() != 'Windows':
+#     # https://github.com/pytorch/pytorch/issues/973
+#     import resource
+#     rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+#     hard_limit = rlimit[1]
+#     soft_limit = min(4096, hard_limit)
+#     resource.setrlimit(resource.RLIMIT_NOFILE, (soft_limit, hard_limit))
 
 DATASETS = Registry('dataset')
 PIPELINES = Registry('pipeline')
