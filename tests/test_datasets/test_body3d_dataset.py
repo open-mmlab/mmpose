f47636974c0c9a41f897a59159a6ff56d446c3b1
ZhiYuan-REN <3295715438@qq.com>
2021-08-23 09:49:50 +0800

    add unit test for body3d_dataset_End-to-End

--- tests/test_datasets/test_body3d_dataset.py
+++ tests/test_datasets/test_body3d_dataset.py
@@ -255,3 +255,55 @@ def test_body3d_mpi_inf_3dhp_dataset():
             np.testing.assert_almost_equal(infos['P-3DPCK'], 100.)
             np.testing.assert_almost_equal(infos['3DAUC'], 30 / 31 * 100)
             np.testing.assert_almost_equal(infos['P-3DAUC'], 30 / 31 * 100)
+
+
+def test_body3d_h36m_dataset_E2E():
+    # Test Human3.6M dataset using end-to-end
+    dataset = 'Body3DH36MDataset_E2E'
+    dataset_class = DATASETS.get(dataset)
+
+    # test single-frame input
+    data_cfg = dict(
+        image_size=[256, 256],
+        heatmap_size=[64, 64, [1, 2, 4, 8, 64]],
+        heatmap3d_depth_bound=0.5,
+        num_joints=17,
+        seq_len=1,
+        seq_frame_interval=1,
+        joint_2d_src='pipeline',
+        joint_2d_det_file=None,
+        causal=False,
+        need_camera_param=True,
+        camera_param_file='tests/data/h36m/cameras.pkl')
+
+    _ = dataset_class(
+        ann_file='tests/data/h36m/test_h36m_body3d.npz',
+        img_prefix='tests/data/h36m',
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=False)
+
+    custom_dataset = dataset_class(
+        ann_file='tests/data/h36m/test_h36m_body3d.npz',
+        img_prefix='tests/data/h36m',
+        data_cfg=data_cfg,
+        pipeline=[],
+        test_mode=True)
+
+    assert custom_dataset.test_mode is True
+    _ = custom_dataset[0]
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        outputs = []
+        for result in custom_dataset:
+            outputs.append({
+                'preds': result['target'][None, ...],
+                'target_image_paths': [result['target_image_path']],
+            })
+
+        metrics = ['mpjpe', 'p-mpjpe', 'n-mpjpe']
+        infos = custom_dataset.evaluate(outputs, tmpdir, metrics)
+
+        np.testing.assert_almost_equal(infos['MPJPE'], 0.0)
+        np.testing.assert_almost_equal(infos['P-MPJPE'], 0.0)
+        np.testing.assert_almost_equal(infos['N-MPJPE'], 0.0)
