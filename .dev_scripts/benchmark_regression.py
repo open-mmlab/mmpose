da76adb77b4270f24a426b79f59afb0babddfa8d
liqikai <tiger19971215@sjtu.edu.cn>
2021-08-24 10:40:57 +0800

    modify the config and script

--- .dev_scripts/benchmark_regression.py
+++ .dev_scripts/benchmark_regression.py
@@ -4,12 +4,14 @@ import os
 import os.path as osp
 import random
 import socket
-import time
+from datetime import datetime
 
 import mmcv
 
 
-def check_port_in_use(port, host='127.0.0.1'):
+def is_port_available(port, host='127.0.0.1'):
+    """check whether a port is in use return True if the port is available else
+    False."""
     s = None
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,34 +31,30 @@ def parse_args():
     parser.add_argument(
         '--config',
         help='test config file path',
-        default='./.dev_scripts/benchmark_regression_cfg.yaml')
+        default='./.dev_scripts/benchmark_regression_cfg_tmpl.yaml')
     parser.add_argument(
         '--priority',
         nargs=2,
         type=int,
-        help='largest priority for infer and train tasks respectively',
+        help='largest priority for test and train tasks respectively',
         default=[3, 3])
 
     # runtime setting parameters
     parser.add_argument(
         '--root-work-dir', help='the root working directory to store logs')
-
     parser.add_argument(
         '--session-name', '-s', help='the tmux session name', default='test')
-
     parser.add_argument(
         '--panes-per-window',
         type=int,
         help='the maximum number of panes in each tmux window',
         default=12)
-
     parser.add_argument(
         '--env',
         help='the conda environment used to run the tasks',
         default='pt1.6')
     parser.add_argument(
-        '--partition', help='the partition name', default='openmmlab')
-
+        '--partition', help='the partition name', default='mm_human')
     parser.add_argument('--gpus', help='the total number of GPUs', default=8)
     parser.add_argument(
         '--gpus-per-node',
@@ -75,27 +73,26 @@ def main():
 
     if args.root_work_dir is None:
         # get the current time stamp
-        ts = time.ctime()
-        ts = ts.replace(' ', '_').replace(':', '_')
+        now = datetime.now()
+        ts = now.strftime('%Y_%m_%d_%H_%M')
         args.root_work_dir = f'work_dirs/benchmark_regression_{ts}'
-
     mmcv.mkdir_or_exist(osp.abspath(args.root_work_dir))
 
     cfg = mmcv.load(args.config)
 
-    # the priority for inference and training tasks respectively
-    prio_infer, prio_train = args.priority
-    prio = max(prio_infer, prio_infer) + 1
+    # priority for test and train tasks respectively
+    prio_test, prio_train = args.priority
+    prio = max(prio_test, prio_train) + 1
 
-    # the number of benchmark regression tasks
+    # number of benchmark regression tasks
     num_task = 0
     for i in range(prio):
-        if i <= prio_infer:
+        if i <= prio_test:
             num_task += len(cfg['model_list'][f'P{i}'])
         if i <= prio_train:
             num_task += len(cfg['model_list'][f'P{i}'])
 
-    # the number of windows need to be created
+    # number of windows need to be created
     num_win = math.ceil(num_task / args.panes_per_window)
 
     # create a new tmux session
@@ -112,6 +109,7 @@ def main():
         # tmux new-window -n win_1
         os.system(f'tmux new-window -n win_{i+1}')
 
+        # decide how many panes will be created in current window
         if num_task_tmp >= args.panes_per_window:
             num_cur_win_task = args.panes_per_window
             num_task_tmp -= args.panes_per_window
@@ -122,7 +120,6 @@ def main():
         for j in range(num_cur_win_task - 1):
             ratio = int(100 - 100 / (num_cur_win_task - j))
             os.system(f'tmux split-window -h -p {ratio}')
-
         os.system('tmux select-layout tiled')
 
     # the initial number of task
@@ -130,10 +127,10 @@ def main():
 
     # get the hostname
     hostname = socket.gethostname()
-    print(hostname)
+    print('hostname: ', hostname)
     # get the host ip
     ip = socket.gethostbyname(hostname)
-    print(ip)
+    print('ip: ', ip)
 
     # initialize a starting port
     cur_port = 29500
@@ -141,10 +138,10 @@ def main():
     for i in range(prio):
         models = cfg['model_list'][f'P{i}']
 
-        # modes = ['infer','train']
+        # modes = ['test','train']
         modes = []
-        if i <= prio_infer:
-            modes.append('infer')
+        if i <= prio_test:
+            modes.append('test')
         if i <= prio_train:
             modes.append('train')
 
@@ -153,9 +150,9 @@ def main():
             cur_checkpoint = model['checkpoint']
 
             if 'task_name' in model.keys():
-                cur_task_name = model['task_name']
+                task_name = model['task_name']
             else:
-                cur_task_name = osp.splitext(osp.basename(cur_config))[0]
+                task_name = osp.splitext(osp.basename(cur_config))[0]
 
             for mode in modes:
                 # select the window and pane
@@ -170,15 +167,22 @@ def main():
                 cmd = f'echo executing task: {cur_task}'
                 os.system(f'tmux send-keys "{cmd}" "C-m"')
 
-                cur_gpus = model[mode]['gpus']
-                cur_gpus_per_node = model[mode]['gpus_per_node']
-                cur_cpus_per_task = model[mode]['cpus_per_task']
-                cur_partition = model[mode]['partition']
+                cur_partition = model[mode][
+                    'partition'] if 'partition' in model[mode].keys(
+                    ) else args.partition
+                cur_gpus = model[mode]['gpus'] if 'gpus' in model[mode].keys(
+                ) else args.gpus
+                cur_gpus_per_node = model[mode][
+                    'gpus_per_node'] if 'gpus_per_node' in model[mode].keys(
+                    ) else args.gpus_per_node
+                cur_cpus_per_task = model[mode][
+                    'cpus_per_task'] if 'cpus_per_task' in model[mode].keys(
+                    ) else args.cpus_per_task
 
-                cur_task_name = mode + '_' + cur_task_name
+                cur_task_name = mode + '_' + task_name
                 cur_work_dir = osp.join(args.root_work_dir, cur_task_name)
 
-                if mode == 'infer':
+                if mode == 'test':
                     # deal with extra python arguments
                     py_cmd = f' --work-dir {cur_work_dir} '
                     if 'py_args' in model[mode].keys():
@@ -226,17 +230,19 @@ def main():
                           f'{py_cmd}'
                     os.system(f'tmux send-keys "{cmd}" "C-m"')
 
-                cur_task += 1
                 cur_port += 1
-
                 # if the port is used, use a random number for port
-                while check_port_in_use(cur_port, ip):
+                while not is_port_available(cur_port, ip):
                     cur_port = random.randint(29000, 39000)
+                print(f'port used in task {cur_task} is: {cur_port}')
+                cur_task += 1
 
     # close the base window
     os.system('tmux select-window -t 0')
     cmd = 'tmux kill-window -t 0'
     os.system(f'tmux send-keys -t {args.session_name} "{cmd}" "C-m"')
+
+    print('All tasks submitted!')
 
 
 if __name__ == '__main__':
