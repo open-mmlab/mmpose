8a0a4f02f3cb03cfaaab232dc6f78602884ff1a0
Yining Li <liyining0712@gmail.com>
2021-08-31 15:11:02 +0800

    Add python3.9 in CI (#877)
    * Add python3.9 in CI
    
    * Add py3.9 in build.yml
    * Modify build.yml to upload codecov only for python3.7 && pytorch1.6
    * update setup.py
    
    * fix build.yml

--- setup.py
+++ setup.py
@@ -173,6 +173,8 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
         ],
         url='https://github.com/open-mmlab/mmpose',
         license='Apache License 2.0',
