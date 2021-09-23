5f3c6f26d9e72d982fa096926f942a4d4cb97fca
RangiLyu <lyuchqi@gmail.com>
2021-09-23 16:19:32 +0800

    [Fix]: Use mmcv root model registry.

--- mmpose/models/builder.py
+++ mmpose/models/builder.py
@@ -1,8 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmcv.cnn import build_model_from_cfg
+from mmcv.cnn import MODELS as MMCV_MODELS
 from mmcv.utils import Registry
 
-MODELS = Registry('models', build_func=build_model_from_cfg)
+MODELS = Registry('models', build_func=build_model_from_cfg, parent=MMCV_MODELS)
 
 BACKBONES = MODELS
 NECKS = MODELS
