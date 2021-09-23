9c04674b2d3357d447cedbeeeb3c6e47c6f001dc
RangiLyu <lyuchqi@gmail.com>
2021-09-23 16:23:49 +0800

    lint

--- mmpose/models/builder.py
+++ mmpose/models/builder.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmcv.cnn import build_model_from_cfg
 from mmcv.cnn import MODELS as MMCV_MODELS
+from mmcv.cnn import build_model_from_cfg
 from mmcv.utils import Registry
 
-MODELS = Registry('models', build_func=build_model_from_cfg, parent=MMCV_MODELS)
+MODELS = Registry(
+    'models', build_func=build_model_from_cfg, parent=MMCV_MODELS)
 
 BACKBONES = MODELS
 NECKS = MODELS
