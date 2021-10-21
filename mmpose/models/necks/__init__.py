deffc2bcbf99a8c9e88458a7a3e9c65b8bd1ee5b
liqikai <tiger19971215@sjtu.edu.cn>
2021-10-21 18:47:05 +0800

    modify PoseWarper detector and PoseWarperNeck

--- mmpose/models/necks/__init__.py
+++ mmpose/models/necks/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .gap_neck import GlobalAveragePooling
-from .posewarper_neck import PoseWarper
+from .posewarper_neck import PoseWarperNeck
 
-__all__ = ['GlobalAveragePooling', 'PoseWarper']
+__all__ = ['GlobalAveragePooling', 'PoseWarperNeck']
