2b6c960e7af691de2caf79a9f2fc8a0393afb859
ly015 <liyining0712@gmail.com>
2021-11-18 20:10:59 +0800

    update webcam runner and node api

--- tools/webcam/webcam.py
+++ tools/webcam/webcam.py
@@ -1,51 +1,50 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import logging
 import time
-import uuid
 from queue import Queue
 from threading import Thread
-from typing import Any, Optional
 
 import cv2
-from webcam_utils import BufferManager, EventManager
 
-
-class Message():
-
-    def __init__(self,
-                 msg: str = '',
-                 data: Any = None,
-                 meta: Any = None,
-                 timestamp: Optional[float] = None):
-        self.msg = msg
-        self.data = data
-        self.meta = meta
-        self.timestamp = timestamp if timestamp else time.time()
-        self.id = uuid.uuid4()
+from .builder import NODES
+from .webcam_utils import BufferManager, EventManager, Message
 
 
 class WebcamRunner():
 
-    def __init__(self, args):
-        self.args = args
+    def __init__(self, cfgs):
+        self.cfgs = cfgs
         self.buffer_manager = BufferManager()
         self.event_manager = EventManager()
-        self.frame_buffer = Queue(maxsize=args.frame_buffer_size)
+        self.frame_buffer = Queue(maxsize=cfgs.frame_buffer_size)
         self.node_list = []
         self.logger = logging.getLogger()
 
-        # register default queues
-        self.buffer_manager.add_buffer('input')
-        self.buffer_manager.add_buffer('output')
+        # register default buffers
+        self.buffer_manager.add_buffer('_frame', maxlen=cfgs.frame_buffer_size)
+        self.buffer_manager.add_buffer('_input')
+        self.buffer_manager.add_buffer('_output')
+
+        # register user defined buffers
+        for buffer_name, buffer_size in cfgs.buffers:
+            self.buffer_manager.add_buffer(buffer_name, buffer_size)
 
         # register nodes
+        for node_cfg in cfgs.nodes:
+            node = NODES.build(node_cfg)
+            for buffer_info in node.input_buffers:
+                assert self.buffer_manager.has_buffer(buffer_info['name'])
+            for buffer_info in node.output_buffers:
+                assert self.buffer_manager.has_buffer(buffer_info['name'])
+
+            self.node_list.append(node)
 
     def _read_camera(self):
 
         self.logger.info('read_camera thread starts')
 
-        args = self.args
-        camera_id = args.camera_id
+        cfgs = self.cfgs
+        camera_id = cfgs.camera_id
 
         vcap = cv2.VideoCapture(camera_id)
         if not vcap.isOpened():
@@ -57,14 +56,9 @@ class WebcamRunner():
             ret_val, frame = vcap.read()
             if ret_val:
                 frame_msg = Message(data=frame)
-                self.buffer_manager.put(frame_msg, 'input')
-
-                if self.args.synchronous_mode:
-                    # synchronize input with specific operation (e.g.
-                    # model inference accomplished)
-                    self.event_manager.wait('synchronize')
-
-                self.frame_buffer.put(frame_msg)
+                self.buffer_manager.put('_input', frame_msg)
+                self.buffer_manager.put('_frame', frame_msg)
+                # self.frame_buffer.put(frame_msg)
 
             else:
                 self.frame_buffer.put(None)
@@ -79,34 +73,28 @@ class WebcamRunner():
         vwriter = None
 
         while not self.event_manager.is_set('exit'):
-            # acquire a frame from buffer
-            frame_msg = self.frame_buffer.get()
+            while self.buffer_manager.is_empty('_output'):
+                time.sleep(0.001)
+
+            # acquire output from buffer
+            output_msg = self.buffer_manager.get('_output')
+
             # None indicates input stream ends
-            if frame_msg is None:
+            if output_msg is None:
                 self.event_manager.set('exit')
                 break
 
-            img = frame_msg.data
-
-            try:
-                output_msg = self.buffer_manager.pop_from('output')
-            except IndexError:
-                # do not update output_msg
-                pass
-
-            # visualize the node outputs
-            if output_msg:
-                pass
+            img = output_msg.data
 
             # delay control
-            if self.args.display_delay > 0:
-                t_sleep = self.args.display_delay * 0.001 - (
-                    time.time() - frame_msg.timestamp)
+            if self.cfgs.display_delay > 0:
+                t_sleep = self.cfgs.display_delay * 0.001 - (
+                    time.time() - output_msg.timestamp)
                 if t_sleep > 0:
                     time.sleep(t_sleep)
 
             # show in a window
-            cv2.imshow(self.args.name, img)
+            cv2.imshow(self.cfgs.name, img)
 
             # handle keyboard input
             key = cv2.waitKey(1)
