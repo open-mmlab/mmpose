d0a9df0ee6302aebb2e30e3b3fb7ae788ce56a59
ly015 <liyining0712@gmail.com>
2021-11-26 12:18:11 +0800

    fix isort

--- mmpose/apis/__init__.py
+++ mmpose/apis/__init__.py
@@ -7,7 +7,7 @@ from .inference_3d import (extract_pose_sequence, inference_interhand_3d_model,
                            vis_3d_mesh_result, vis_3d_pose_result)
 from .inference_tracking import get_track_id, vis_pose_tracking_result
 from .test import multi_gpu_test, single_gpu_test
-from .train import train_model, init_random_seed
+from .train import init_random_seed, train_model
 
 __all__ = [
     'train_model', 'init_pose_model', 'inference_top_down_pose_model',
