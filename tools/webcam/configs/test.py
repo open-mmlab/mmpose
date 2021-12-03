4e47e8d67e313a3648c6f8bf950a1ebe2369ae86
ly015 <liyining0712@gmail.com>
2021-12-03 18:33:12 +0800

    add monitor node

--- tools/webcam/configs/test.py
+++ tools/webcam/configs/test.py
@@ -4,11 +4,11 @@ runner = dict(
     camera_id=0,
     display_delay=0,
     frame_buffer_size=20,
-    user_buffers=[('det_result', 1), ('pose_result', 1)],
+    user_buffers=[('det_result', 2), ('pose_result', 2), ('visualization', 2)],
     nodes=[
         dict(
             type='DetectorNode',
-            name='detector',
+            name='Detector',
             model_config='demo/mmdetection_cfg/'
             'ssdlite_mobilenetv2_scratch_600e_coco.py',
             model_checkpoint='https://download.openmmlab.com'
@@ -19,7 +19,7 @@ runner = dict(
             output_buffer='det_result'),
         dict(
             type='TopDownPoseEstimatorNode',
-            name='human pose estimator',
+            name='TopDown Pose Estimator',
             model_config='configs/wholebody/2d_kpt_sview_rgb_img/'
             'topdown_heatmap/coco-wholebody/'
             'vipnas_res50_coco_wholebody_256x192_dark.py',
@@ -31,8 +31,16 @@ runner = dict(
             output_buffer='pose_result'),
         dict(
             type='PoseVisualizerNode',
-            name='visualizer',
+            name='Visualizer',
+            enable_key='v',
             frame_buffer='_frame_',
             result_buffer='pose_result',
+            output_buffer='visualization'),
+        dict(
+            type='MonitorNode',
+            name='Monitor',
+            enable_key='m',
+            style='fancy',
+            input_buffer='visualization',
             output_buffer='_display_')
     ])
