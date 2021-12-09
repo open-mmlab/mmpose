39840d5590c5ad1404de3e8abbe8b719ea51c376
ly015 <liyining0712@gmail.com>
2021-12-09 18:55:17 +0800

    refactor visualition nodes

--- tools/webcam/configs/human_pose.py
+++ tools/webcam/configs/human_pose.py
@@ -2,10 +2,17 @@
 runner = dict(
     name='Debug CamRunner',
     camera_id=0,
-    camera_fps=20,
+    camera_fps=30,
     display_delay=0,
     frame_buffer_size=20,
-    user_buffers=[('det_result', 2), ('pose_result', 2), ('visualization', 2)],
+    user_buffers=[
+        ('det_result', 2),
+        ('pose_result', 2),
+        ('result', 2),
+        ('vis_pose', 2),
+        ('vis_sunglasses', 2),
+        ('vis_bugeye', 2),
+    ],
     nodes=[
         dict(
             type='DetectorNode',
@@ -31,17 +38,34 @@ runner = dict(
             input_buffer='det_result',
             output_buffer='pose_result'),
         dict(
+            type='ModelResultBindingNode',
+            name='ResultBinder',
+            frame_buffer='_frame_',
+            result_buffer='pose_result',
+            output_buffer='result'),
+        dict(
             type='PoseVisualizerNode',
             name='Visualizer',
             enable_key='v',
-            frame_buffer='_frame_',
-            result_buffer='pose_result',
-            output_buffer='visualization'),
+            frame_buffer='result',
+            output_buffer='vis_pose'),
+        dict(
+            type='SunglassesNode',
+            name='Visualizer',
+            enable_key='s',
+            frame_buffer='vis_pose',
+            output_buffer='vis_sunglasses'),
+        dict(
+            type='BugEyeNode',
+            name='Visualizer',
+            enable_key='b',
+            frame_buffer='vis_sunglasses',
+            output_buffer='vis_bugeye'),
         dict(
             type='MonitorNode',
             name='Monitor',
             enable_key='m',
             style='fancy',
-            input_buffer='visualization',
+            input_buffer='vis_bugeye',
             output_buffer='_display_')
     ])
--- tools/webcam/webcam_apis/nodes/__init__.py
+++ tools/webcam/webcam_apis/nodes/__init__.py
@@ -1,10 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .builder import NODES
+from .frame_effect_nodes import BugEyeNode, PoseVisualizerNode, SunglassesNode
+from .helper_nodes import ModelResultBindingNode, MonitorNode
 from .mmdet_nodes import DetectorNode
 from .mmpose_nodes import TopDownPoseEstimatorNode
-from .visualization_nodes import MonitorNode, PoseVisualizerNode
 
 __all__ = [
     'NODES', 'PoseVisualizerNode', 'DetectorNode', 'TopDownPoseEstimatorNode',
-    'MonitorNode'
+    'MonitorNode', 'BugEyeNode', 'SunglassesNode', 'ModelResultBindingNode'
 ]
