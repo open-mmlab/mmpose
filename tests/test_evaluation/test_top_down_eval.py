fa1f179d91cfd9029a6b049db6d733d4ba2eda4a
X00_123 <460718038@QQ.COM>
2021-12-16 11:15:50 +0800

    [Fix] Bug in post dark udp (#1079)
    * update fap (#1070)
    
    * fix a bug in post_dark_udp
    
    * fix a bug in post_dark_udp
    
    * Modified a bad comment
    
    * add unit test for udp
    
    * add unit test for udp
    
    * use numpy instead math for math ops
    
    * fix lint
    
    Co-authored-by: Jas <jinsheng@sensetime.com>
    Co-authored-by: quyang <quyang@qiyi.com>
    Co-authored-by: ly015 <liyining0712@gmail.com>

--- tests/test_evaluation/test_top_down_eval.py
+++ tests/test_evaluation/test_top_down_eval.py
@@ -32,6 +32,11 @@ def test_keypoints_from_heatmaps():
     center = np.array([[127, 127]])
     scale = np.array([[64 / 200.0, 64 / 200.0]])
 
+    udp_heatmaps = np.ones((32, 17, 64, 64), dtype=np.float32)
+    udp_heatmaps[:, :, 31, 31] = 2
+    udp_center = np.tile([127, 127], (32, 1))
+    udp_scale = np.tile([32, 32], (32, 1))
+
     preds, maxvals = keypoints_from_heatmaps(heatmaps, center, scale)
 
     assert_array_almost_equal(preds, np.array([[[126, 126]]]), decimal=4)
@@ -51,10 +56,16 @@ def test_keypoints_from_heatmaps():
     assert isinstance(preds, np.ndarray)
     assert isinstance(maxvals, np.ndarray)
 
+    # test for udp dimension problem
     preds, maxvals = keypoints_from_heatmaps(
-        heatmaps, center, scale, post_process='unbiased')
-    assert_array_almost_equal(preds, np.array([[[126, 126]]]), decimal=4)
-    assert_array_almost_equal(maxvals, np.array([[[2]]]), decimal=4)
+        udp_heatmaps,
+        udp_center,
+        udp_scale,
+        post_process='default',
+        target_type='GaussianHeatMap',
+        use_udp=True)
+    assert_array_almost_equal(preds, np.tile([76, 76], (32, 17, 1)), decimal=0)
+    assert_array_almost_equal(maxvals, np.tile([2], (32, 17, 1)), decimal=4)
     assert isinstance(preds, np.ndarray)
     assert isinstance(maxvals, np.ndarray)
 
