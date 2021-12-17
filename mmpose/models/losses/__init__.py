7504b95aac5044eec03113fd3502567b14b603a5
Jas <jinsheng@sensetime.com>
2021-12-17 14:29:53 +0800

    [Feature] Adaptive wingloss (ICCV'19) (#1072)
    * add adaptive wingloss
    
    * move adaptive wingloss to heatmap_loss
    
    * match shape in wingloss
    
    * rename loss
    
    * mean for loss
    
    * add configs
    
    * fix unittest
    
    * rename mse_loss to heatmap_loss
    
    * add docs
    
    * fix lint
    
    Co-authored-by: ly015 <liyining0712@gmail.com>

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .classfication_loss import BCELoss
+from .heatmap_loss import AdaptiveWingLoss
 from .mesh_loss import GANLoss, MeshLoss
 from .mse_loss import JointsMSELoss, JointsOHKMMSELoss
 from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
@@ -11,5 +12,5 @@ __all__ = [
     'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss',
     'MultiLossFactory', 'MeshLoss', 'GANLoss', 'SmoothL1Loss', 'WingLoss',
     'MPJPELoss', 'MSELoss', 'L1Loss', 'BCELoss', 'BoneLoss',
-    'SemiSupervisionLoss', 'SoftWingLoss'
+    'SemiSupervisionLoss', 'SoftWingLoss', 'AdaptiveWingLoss'
 ]
