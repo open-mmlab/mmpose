cfa89d147d64cc7f7ea9ef41fa158fbb40dde6b2
Yining Li <liyining0712@gmail.com>
2022-01-17 15:08:08 +0800

    [WebcamAPI] Add facwswap node and config (#1094)
    * add facwswap node
    
    * adapt to webcam_api
    
    * adapt to webcam_api
    
    * move config
    
    * add sync mode in runner
    
    * fix bugs and update nodes

--- tools/webcam/webcam_apis/nodes/__init__.py
+++ tools/webcam/webcam_apis/nodes/__init__.py
@@ -2,6 +2,7 @@
 from .builder import NODES
 from .frame_effect_nodes import (BugEyeNode, NoticeBoardNode,
                                  PoseVisualizerNode, SunglassesNode)
+from .faceswap_nodes import FaceSwapNode
 from .helper_nodes import ModelResultBindingNode, MonitorNode, RecorderNode
 from .mmdet_nodes import DetectorNode
 from .mmpose_nodes import TopDownPoseEstimatorNode
@@ -9,5 +10,5 @@ from .mmpose_nodes import TopDownPoseEstimatorNode
 __all__ = [
     'NODES', 'PoseVisualizerNode', 'DetectorNode', 'TopDownPoseEstimatorNode',
     'MonitorNode', 'BugEyeNode', 'SunglassesNode', 'ModelResultBindingNode',
-    'NoticeBoardNode', 'RecorderNode'
+    'NoticeBoardNode', 'RecorderNode', 'FaceSwapNode'
 ]
