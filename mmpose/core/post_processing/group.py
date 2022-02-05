ce3a0e94719c1e644ceb0caad40cb77ccc86cb79
Pierre Colle <piercus@gmail.com>
2022-02-05 01:25:59 +0100

    [Fix] #1169 score_per_joint

--- mmpose/core/post_processing/group.py
+++ mmpose/core/post_processing/group.py
@@ -150,6 +150,7 @@ class HeatmapParser:
         self.pool = torch.nn.MaxPool2d(cfg['nms_kernel'], 1,
                                        cfg['nms_padding'])
         self.use_udp = cfg.get('use_udp', False)
+        self.score_per_joint = cfg.get('score_per_joint', False)
 
     def nms(self, heatmaps):
         """Non-Maximum Suppression for heatmaps.
@@ -388,7 +389,10 @@ class HeatmapParser:
             else:
                 results = self.adjust(results, heatmaps)
 
-        scores = [i[:, 2].mean() for i in results[0]]
+        if self.score_per_joint:
+            scores = [i[:, 2] for i in results[0]]
+        else:
+            scores = [i[:, 2].mean() for i in results[0]]
 
         if refine:
             results = results[0]
