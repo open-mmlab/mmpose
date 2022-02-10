8e4df314a6ac10bdbdf62b8037d88e58dcb41afc
Yining Li <liyining0712@gmail.com>
2022-02-10 09:30:48 +0800

    [Feature] Add setup multi-processing both in train and test (#1146)
    * set thread number in train.py
    
    * add set_env
    
    * add setup_multi_processes in mmpose/utils
    * set thread number of both train and test
    * add unit test

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -12,3 +12,8 @@ load_from = None
 resume_from = None
 dist_params = dict(backend='nccl')
 workflow = [('train', 1)]
+
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
