a9a120c2b6a5e76f5b721d5b1968b82c2118d099
ly015 <liyining0712@gmail.com>
2022-03-14 11:46:40 +0800

    not expose timer_hook

--- mmpose/apis/train.py
+++ mmpose/apis/train.py
@@ -169,11 +169,14 @@ def train_model(model,
             optimizer_config = cfg.optimizer_config
 
     # register hooks
-    runner.register_training_hooks(cfg.lr_config, optimizer_config,
-                                   cfg.checkpoint_config, cfg.log_config,
-                                   cfg.get('momentum_config', None),
-                                   cfg.get('timer_config', dict(type='IterTimerHook')),
-                                   cfg.get('custom_hooks_config', None))
+    runner.register_training_hooks(
+        cfg.lr_config,
+        optimizer_config,
+        cfg.checkpoint_config,
+        cfg.log_config,
+        cfg.get('momentum_config', None),
+        custom_hooks_config=cfg.get('custom_hooks_config', None))
+
     if distributed:
         runner.register_hook(DistSamplerSeedHook())
 
