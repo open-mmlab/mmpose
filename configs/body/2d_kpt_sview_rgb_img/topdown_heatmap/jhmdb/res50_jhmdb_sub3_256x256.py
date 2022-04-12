55d34b4e19271ec69ed94b5351dbde4f913c053f
ly015 <liyining0712@gmail.com>
2022-04-12 23:42:10 +0800

    update halpe/jhmdb/h36m

--- configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/jhmdb/res50_jhmdb_sub3_256x256.py
+++ configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/jhmdb/res50_jhmdb_sub3_256x256.py
@@ -62,6 +62,11 @@ data_cfg = dict(
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
+    dict(
+        type='TopDownGetBboxCenterScale',
+        padding=1.25,
+        shift_prob=0.3,
+        shift_factor=0.2),
     dict(type='TopDownRandomFlip', flip_prob=0.5),
     dict(
         type='TopDownGetRandomScaleRotation', rot_factor=30,
@@ -84,6 +89,7 @@ train_pipeline = [
 
 val_pipeline = [
     dict(type='LoadImageFromFile'),
+    dict(type='TopDownGetBboxCenterScale', padding=1.25),
     dict(type='TopDownAffine'),
     dict(type='ToTensor'),
     dict(
