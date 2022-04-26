9f746841a8ed3383140af5e4e8268af4455dbb42
ly015 <liyining0712@gmail.com>
2022-04-26 20:18:56 +0800

    fix dist_trains.sh bug

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,6 +6,7 @@ GPUS=$2
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
 PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch \
@@ -16,5 +17,4 @@ python -m torch.distributed.launch \
     --master_port=$PORT \
     $(dirname "$0")/train.py \
     $CONFIG \
-    --seed 0 \
     --launcher pytorch ${@:3}
