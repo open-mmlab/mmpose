878eff165e778f31e10db300660a5103d00ce46f
Yining Li <liyining0712@gmail.com>
2022-04-27 21:08:52 +0800

    [Fix] Fix bugs distributed training (#1338)
    * fix dist_trains.sh bug
    
    * remove local-rank

--- tools/train.py
+++ tools/train.py
@@ -70,9 +70,7 @@ def parse_args():
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
         help='job launcher')
-    parser.add_argument('--local-rank', type=int, default=0)
-    parser.add_argument(
-        '--local_rank', type=int, default=0, help='An alias to --local-rank')
+    parser.add_argument('--local_rank', type=int, default=0)
     parser.add_argument(
         '--autoscale-lr',
         action='store_true',
