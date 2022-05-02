c493789220295487cae8bff91ac78caaa18179bb
zengwang430521 <zengwang430521@gmail.com>
2022-05-03 02:07:19 +0800

    [Feature] add PVT backbone and configs (#1343)
    * add pvt backbone
    
    * add more tests
    
    * add some docs
    
    * add pvt_coco.yml
    
    * add links in README
    
    Co-authored-by: ly015 <liyining0712@gmail.com>

--- mmpose/models/utils/__init__.py
+++ mmpose/models/utils/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .ckpt_convert import pvt_convert
 from .smpl import SMPL
+from .transformer import PatchEmbed, nchw_to_nlc, nlc_to_nchw
 
-__all__ = ['SMPL']
+__all__ = ['SMPL', 'PatchEmbed', 'nchw_to_nlc', 'nlc_to_nchw', 'pvt_convert']
