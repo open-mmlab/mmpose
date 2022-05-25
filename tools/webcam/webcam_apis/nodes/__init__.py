91bf91ba4f10e1e98061f54301fb5689c443e4d1
lupeng <penglu2097@gmail.com>
2022-05-25 09:51:08 +0800

    webcam add hand detector

--- tools/webcam/webcam_apis/nodes/__init__.py
+++ tools/webcam/webcam_apis/nodes/__init__.py
@@ -5,16 +5,16 @@ from .frame_effect_node import (BackgroundNode, BugEyeNode, MoustacheNode,
                                 NoticeBoardNode, PoseVisualizerNode,
                                 SaiyanNode, SunglassesNode)
 from .helper_node import ModelResultBindingNode, MonitorNode, RecorderNode
-from .mmdet_node import DetectorNode
+from .mmdet_node import DetectorNode, MultiFrameDetectorNode
 from .mmpose_node import TopDownPoseEstimatorNode
 from .pose_tracker_node import PoseTrackerNode
 from .valentinemagic_node import ValentineMagicNode
 from .xdwendwen_node import XDwenDwenNode
 
 __all__ = [
-    'NODES', 'PoseVisualizerNode', 'DetectorNode', 'TopDownPoseEstimatorNode',
-    'MonitorNode', 'BugEyeNode', 'SunglassesNode', 'ModelResultBindingNode',
-    'NoticeBoardNode', 'RecorderNode', 'FaceSwapNode', 'MoustacheNode',
-    'SaiyanNode', 'BackgroundNode', 'XDwenDwenNode', 'ValentineMagicNode',
-    'PoseTrackerNode'
+    'NODES', 'PoseVisualizerNode', 'DetectorNode', 'MultiFrameDetectorNode',
+    'TopDownPoseEstimatorNode', 'MonitorNode', 'BugEyeNode', 'SunglassesNode',
+    'ModelResultBindingNode', 'NoticeBoardNode', 'RecorderNode',
+    'FaceSwapNode', 'MoustacheNode', 'SaiyanNode', 'BackgroundNode',
+    'XDwenDwenNode', 'ValentineMagicNode', 'PoseTrackerNode'
 ]
