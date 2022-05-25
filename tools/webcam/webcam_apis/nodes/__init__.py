f647a67775063116ac74b2065f4df693135a8a91
lupeng <penglu2097@gmail.com>
2022-05-25 09:51:08 +0800

    webcam add hand detector

--- tools/webcam/webcam_apis/nodes/__init__.py
+++ tools/webcam/webcam_apis/nodes/__init__.py
@@ -5,14 +5,15 @@ from .frame_effect_node import (BackgroundNode, BugEyeNode, MoustacheNode,
                                 NoticeBoardNode, PoseVisualizerNode,
                                 SaiyanNode, SunglassesNode)
 from .helper_node import ModelResultBindingNode, MonitorNode, RecorderNode
-from .mmdet_node import DetectorNode
+from .mmdet_node import DetectorNode, MultiFrameDetectorNode
 from .mmpose_node import TopDownPoseEstimatorNode
 from .valentinemagic_node import ValentineMagicNode
 from .xdwendwen_node import XDwenDwenNode
 
 __all__ = [
-    'NODES', 'PoseVisualizerNode', 'DetectorNode', 'TopDownPoseEstimatorNode',
-    'MonitorNode', 'BugEyeNode', 'SunglassesNode', 'ModelResultBindingNode',
-    'NoticeBoardNode', 'RecorderNode', 'FaceSwapNode', 'MoustacheNode',
-    'SaiyanNode', 'BackgroundNode', 'XDwenDwenNode', 'ValentineMagicNode'
+    'NODES', 'PoseVisualizerNode', 'DetectorNode', 'MultiFrameDetectorNode',
+    'TopDownPoseEstimatorNode', 'MonitorNode', 'BugEyeNode', 'SunglassesNode',
+    'ModelResultBindingNode', 'NoticeBoardNode', 'RecorderNode',
+    'FaceSwapNode', 'MoustacheNode', 'SaiyanNode', 'BackgroundNode',
+    'XDwenDwenNode', 'ValentineMagicNode'
 ]
--- tools/webcam/webcam_apis/nodes/mmdet_node.py
+++ tools/webcam/webcam_apis/nodes/mmdet_node.py
@@ -1,5 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
+
+import numpy as np
 
 from .builder import NODES
 from .node import Node
@@ -86,3 +88,96 @@ class DetectorNode(Node):
             result['preds'].extend(preds_i)
 
         return result
+
+
+@NODES.register_module()
+class MultiFrameDetectorNode(DetectorNode):
+
+    def __init__(self,
+                 name: str,
+                 model_config: str,
+                 model_checkpoint: str,
+                 input_buffer: str,
+                 output_buffer: Union[str, List[str]],
+                 inference_frame: str = 'mid',
+                 enable_key: Optional[Union[str, int]] = None,
+                 device: str = 'cuda:0'):
+        super(MultiFrameDetectorNode,
+              self).__init__(name, model_config, model_checkpoint,
+                             input_buffer, output_buffer, enable_key, device)
+        self.inference_frame = inference_frame
+
+    def _get_input_from_buffer(self) -> Tuple[bool, Optional[Dict]]:
+        """Get and pack input data if it's ready. The function returns a tuple
+        of a status flag and a packed data dictionary. If input_buffer is
+        ready, the status flag will be True, and the packed data is a dict
+        whose items are buffer names and corresponding messages (unready
+        additional buffers will give a `None`). Otherwise, the status flag is
+        False and the packed data is None.
+
+        Returns:
+            bool: status flag
+            dict[str, Message]: the packed inputs where the key is the buffer
+                name and the value is the Message got from the corresponding
+                buffer.
+        """
+        buffer_manager = self._buffer_manager
+
+        if buffer_manager is None:
+            raise ValueError(f'{self.name}: Runner not set!')
+
+        # Check that essential buffers are ready
+        for buffer_info in self._input_buffers:
+            if buffer_info.essential and buffer_manager.is_empty(
+                    buffer_info.buffer_name):
+                return False, None
+
+        # Default input
+        result = {
+            buffer_info.input_name: None
+            for buffer_info in self._input_buffers
+        }
+
+        for buffer_info in self._input_buffers:
+            if buffer_info.input_name != 'input':
+                try:
+                    result[buffer_info.input_name] = buffer_manager.get(
+                        buffer_info.buffer_name, block=False)
+                except Empty:
+                    pass
+
+            else:
+                while not buffer_manager.is_empty(buffer_info.buffer_name):
+                    if result[buffer_info.input_name] is None:
+                        result[buffer_info.input_name] = []
+                    result[buffer_info.input_name].append(
+                        buffer_manager.get(
+                            buffer_info.buffer_name, block=False))
+
+            if buffer_info.essential and result[
+                    buffer_info.input_name] is None:
+                return False, None
+
+        return True, result
+
+    def process(self, input_msgs):
+        input_msg = input_msgs['input']
+        if self.inference_frame == 'last':
+            key_frame = input_msg[-1]
+        elif self.inference_frame == 'mid':
+            key_frame = input_msg[len(input_msg) // 2]
+        elif self.inference_frame == 'begin':
+            key_frame = input_msg[0]
+        else:
+            raise ValueError(f'Invalid inference_frame {self.inference_frame}')
+
+        img = key_frame.get_image()
+
+        preds = inference_detector(self.model, img)
+        det_result = self._post_process(preds)
+
+        imgs = [frame.get_image() for frame in input_msg]
+        key_frame.set_image(np.stack(imgs, axis=0))
+
+        key_frame.add_detection_result(det_result, tag=self.name)
+        return key_frame
