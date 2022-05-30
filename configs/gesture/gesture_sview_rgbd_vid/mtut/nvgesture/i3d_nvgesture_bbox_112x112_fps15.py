2bc31edf36a32446d760b168736b13a97758d2fd
lupeng <penglu2097@gmail.com>
2022-05-30 12:36:53 +0800

    update gesture recognition configs and results

--- configs/gesture/gesture_sview_rgbd_vid/mtut/nvgesture/i3d_nvgesture_bbox_112x112_fps15.py
+++ configs/gesture/gesture_sview_rgbd_vid/mtut/nvgesture/i3d_nvgesture_bbox_112x112_fps15.py
@@ -1,104 +1,108 @@
 _base_ = [
-    '../../../_base_/default_runtime.py',
-    '../../../_base_/datasets/nvgesture.py'
+    '../../../../_base_/default_runtime.py',
+    '../../../../_base_/datasets/nvgesture.py'
 ]
 
 checkpoint_config = dict(interval=5)
-evaluation = dict(interval=10, metric='AP', save_best='mAP')
+evaluation = dict(interval=5, metric='AP', save_best='AP_rgb')
 
 optimizer = dict(
     type='SGD',
-    lr=1e-2,
+    lr=1e-1,
     momentum=0.9,
 )
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(
-    policy='step',
-    gamma=0.1,
-    warmup='linear',
-    warmup_iters=10,
-    warmup_ratio=0.1,
-    step=[30, 50])
-total_epochs = 60
-log_config = dict(
-    interval=5,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
-    ])
+lr_config = dict(policy='step', gamma=0.1, step=[30, 60, 90, 110])
+total_epochs = 130
+log_config = dict(interval=10)
 
-custom_hooks = [dict(type='ModelSetEpochHook')]
+custom_hooks_config = [dict(type='ModelSetEpochHook')]
 
 model = dict(
     type='GestureRecognizer',
-    modality='rgb',
-    pretrained='https://github.com/hassony2/kinetics_i3d_pytorch/'
-    'raw/master/model/model_rgb.pth',
+    modality=['rgb', 'depth'],
+    pretrained=dict(
+        rgb='https://github.com/hassony2/kinetics_i3d_pytorch/'
+        'raw/master/model/model_rgb.pth',
+        depth='https://github.com/hassony2/kinetics_i3d_pytorch/'
+        'raw/master/model/model_rgb.pth',
+    ),
     backbone=dict(
-        type='I3D',
-        in_channels=3,
-        expansion=1,
+        rgb=dict(
+            type='I3D',
+            in_channels=3,
+            expansion=1,
+        ),
+        depth=dict(
+            type='I3D',
+            in_channels=1,
+            expansion=1,
+        ),
     ),
     cls_head=dict(
         type='MultiModalSSAHead',
         num_classes=25,
+        avg_pool_kernel=(1, 2, 2),
     ),
     train_cfg=dict(
         beta=2,
-        lambda_=5e-3,
-        ssa_start_epoch=60,
+        lambda_=1e-3,
+        ssa_start_epoch=111,
     ),
     test_cfg=dict(),
 )
 
+data_root = 'data/nvgesture'
 data_cfg = dict(
     video_size=[320, 240],
-    modality='rgb',
+    modality=['rgb', 'depth'],
+    bbox_file=f'{data_root}/annotations/bboxes.json',
 )
 
 train_pipeline = [
     dict(type='LoadVideoFromFile'),
     dict(type='ModalWiseChannelProcess'),
     dict(type='CropValidClip'),
-    dict(type='RandomTemporalCrop'),
-    dict(type='ResizeGivenShortEdge', length=256),
-    dict(type='RandomAlignedSpatialCrop', length=224),
+    dict(type='TemporalPooling', length=16, ref_fps=15),
+    dict(type='MultiFrameBBoxMerge'),
+    dict(
+        type='ResizedCropByBBox',
+        size=112,
+        scale=(0.8, 1.25),
+        ratio=(0.75, 1.33),
+        shift=0.3),
+    dict(type='NVGestureRandomFlip'),
+    dict(type='VideoColorJitter', brightness=0.4, contrast=0.3),
     dict(type='MultiModalVideoToTensor'),
     dict(
         type='VideoNormalizeTensor',
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]),
     dict(
-        type='Collect',
-        keys=['video', 'label'],
-        meta_keys=[
-            'video_file', 'height', 'width', 'num_frames', 'fps', 'modality'
-        ]),
+        type='Collect', keys=['video', 'label'], meta_keys=['fps',
+                                                            'modality']),
 ]
 
 val_pipeline = [
     dict(type='LoadVideoFromFile'),
     dict(type='ModalWiseChannelProcess'),
     dict(type='CropValidClip'),
-    dict(type='ResizeGivenShortEdge', length=256),
-    dict(type='CenterSpatialCrop', length=224),
+    dict(type='TemporalPooling', length=-1, ref_fps=15),
+    dict(type='MultiFrameBBoxMerge'),
+    dict(type='ResizedCropByBBox', size=112),
     dict(type='MultiModalVideoToTensor'),
     dict(
         type='VideoNormalizeTensor',
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]),
     dict(
-        type='Collect',
-        keys=['video', 'label'],
-        meta_keys=[
-            'video_file', 'height', 'width', 'num_frames', 'fps', 'modality'
-        ]),
+        type='Collect', keys=['video', 'label'], meta_keys=['fps',
+                                                            'modality']),
 ]
 
 test_pipeline = val_pipeline
 
-data_root = 'data/nvgesture'
 data = dict(
     samples_per_gpu=6,
     workers_per_gpu=2,
