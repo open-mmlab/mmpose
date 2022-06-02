84e949b5d7740e8be0692ac1f0aef9a99048a4fe
Yining Li <liyining0712@gmail.com>
2022-06-03 00:14:49 +0800

    [Fix] fix doc page of model-zoo (#1408)
    * i3d -> backbone
    
    * fix collect.py

--- docs/zh_cn/collect.py
+++ docs/zh_cn/collect.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Copyright (c) OpenMMLab. All rights reserved.
 import os
+import os.path as osp
 import re
 from glob import glob
 
@@ -9,25 +10,52 @@ from titlecase import titlecase
 os.makedirs('topics', exist_ok=True)
 os.makedirs('papers', exist_ok=True)
 
+
+def _parse_task(task):
+    """Parse task name.
+
+    Data modality is represented by a string of 4 or 5 parts like:
+    - 2d_kpt_sview_rgb_img
+    - gesture_sview_rgbd_vid
+    """
+
+    parts = task.split('_')
+    if len(parts) == 5:
+        pass
+    elif len(parts) == 4:
+        # The first part "spatial dimension" is optional
+        parts = [''] + parts
+    else:
+        raise ValueError('Invalid modality')
+
+    return parts
+
+
 # Step 1: get subtopics: a mix of topic and task
 minisections = [
-    x.split('/')[-2:] for x in glob('../../configs/*/*') if '_base_' not in x
+    x.split(osp.sep)[-2:] for x in glob('../../configs/*/*')
+    if '_base_' not in x
 ]
 alltopics = sorted(list(set(x[0] for x in minisections)))
 subtopics = []
-for t in alltopics:
-    data = [x[1].split('_') for x in minisections if x[0] == t]
+for topic in alltopics:
+    tasks = [_parse_task(x[1]) for x in minisections if x[0] == topic]
     valid_ids = []
-    for i in range(len(data[0])):
-        if len(set(x[i] for x in data)) > 1:
+    for i in range(len(tasks[0])):
+        if len(set(x[i] for x in tasks)) > 1:
             valid_ids.append(i)
     if len(valid_ids) > 0:
-        subtopics.extend([
-            f"{titlecase(t)}({','.join([d[i].title() for i in valid_ids])})",
-            t, '_'.join(d)
-        ] for d in data)
+        for task in tasks:
+            appendix = ','.join(
+                [task[i].title() for i in valid_ids if task[i]])
+            subtopic = [
+                f'{titlecase(topic)}({appendix})',
+                topic,
+                '_'.join(t for t in task if t),
+            ]
+            subtopics.append(subtopic)
     else:
-        subtopics.append([titlecase(t), t, '_'.join(data[0])])
+        subtopics.append([titlecase(topic), topic, '_'.join(tasks[0])])
 
 contents = {}
 for subtopic, topic, task in sorted(subtopics):
@@ -35,14 +63,14 @@ for subtopic, topic, task in sorted(subtopics):
     datasets = sorted(
         list(
             set(
-                x.split('/')[-2]
+                x.split(osp.sep)[-2]
                 for x in glob(f'../../configs/{topic}/{task}/*/*/'))))
     contents[subtopic] = {d: {} for d in datasets}
     for dataset in datasets:
         # Step 3: get all settings: algorithm + backbone + trick
         for file in glob(f'../../configs/{topic}/{task}/*/{dataset}/*.md'):
-            keywords = (file.split('/')[-3],
-                        *file.split('/')[-1].split('_')[:-1])
+            keywords = (file.split(osp.sep)[-3],
+                        *file.split(osp.sep)[-1].split('_')[:-1])
             with open(file, 'r') as f:
                 contents[subtopic][dataset][keywords] = f.read()
 
@@ -67,7 +95,7 @@ for subtopic, datasets in contents.items():
         f.write('\n'.join(lines))
 
 # Step 5: write files by paper
-allfiles = [x.split('/')[-2:] for x in glob('../en/papers/*/*.md')]
+allfiles = [x.split(osp.sep)[-2:] for x in glob('../en/papers/*/*.md')]
 sections = sorted(list(set(x[0] for x in allfiles)))
 for section in sections:
     lines = [f'# {titlecase(section)}', '']
