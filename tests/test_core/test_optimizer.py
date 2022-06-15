d4ae1778e841e433d52a459bf4754ece319ac81b
Jas <jinsheng13@foxmail.com>
2022-06-16 01:42:45 +0800

    [Feature] Add layer decay optimizer constructor and learning rate decay optimizer constructor (#1423)
    * add layer_decay_optimizer
    
    * rename model
    
    * fix mmdet to mmpose
    
    * fix unittest

--- tests/test_core/test_optimizer.py
+++ tests/test_core/test_optimizer.py
@@ -1,8 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import pytest
 import torch
 import torch.nn as nn
+from mmcv.runner import DefaultOptimizerConstructor
 
-from mmpose.core import build_optimizers
+from mmpose.core import (OPTIMIZER_BUILDERS, build_optimizer_constructor,
+                         build_optimizers)
 
 
 class ExampleModel(nn.Module):
@@ -14,6 +17,35 @@ class ExampleModel(nn.Module):
 
     def forward(self, x):
         return x
+
+
+base_lr = 0.01
+base_wd = 0.0001
+momentum = 0.9
+
+
+def test_build_optimizer_constructor():
+    optimizer_cfg = dict(
+        type='SGD', lr=base_lr, weight_decay=base_wd, momentum=momentum)
+    optim_constructor_cfg = dict(
+        type='DefaultOptimizerConstructor', optimizer_cfg=optimizer_cfg)
+    optim_constructor = build_optimizer_constructor(optim_constructor_cfg)
+    # Test whether optimizer constructor can be built from parent.
+    assert type(optim_constructor) is DefaultOptimizerConstructor
+
+    @OPTIMIZER_BUILDERS.register_module()
+    class MyOptimizerConstructor(DefaultOptimizerConstructor):
+        pass
+
+    optim_constructor_cfg = dict(
+        type='MyOptimizerConstructor', optimizer_cfg=optimizer_cfg)
+    optim_constructor = build_optimizer_constructor(optim_constructor_cfg)
+    # Test optimizer constructor can be built from child registry.
+    assert type(optim_constructor) is MyOptimizerConstructor
+
+    # Test unregistered constructor cannot be built
+    with pytest.raises(KeyError):
+        build_optimizer_constructor(dict(type='A'))
 
 
 def test_build_optimizers():
