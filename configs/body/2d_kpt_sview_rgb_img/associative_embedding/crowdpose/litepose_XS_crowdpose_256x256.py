013f4bfd9ae99f5a7a0d349628a68331c1c1dc35
Junyan Li <lijunyan668@outlook.com>
2022-06-27 14:18:42 +0000

    configs

--- configs/body/2d_kpt_sview_rgb_img/associative_embedding/crowdpose/litepose_XS_crowdpose_256x256.py
+++ configs/body/2d_kpt_sview_rgb_img/associative_embedding/crowdpose/litepose_XS_crowdpose_256x256.py
@@ -1,14 +1,13 @@
 _base_ = [
     '../../../../_base_/default_runtime.py',
-    '../../../../_base_/datasets/coco.py'
+    '../../../../_base_/datasets/crowdpose.py'
 ]
-checkpoint_config = dict(interval=50)
-evaluation = dict(interval=100, metric='mAP', save_best='AP')
 
-optimizer = dict(
-    type='Adam',
-    lr=0.001,
-)
+log_config = dict(interval=10)
+checkpoint_config = dict(interval=25)
+evaluation = dict(interval=50, metric='mAP', save_best='AP')
+
+optimizer = dict(type='Adam', lr=0.006)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
@@ -16,52 +15,51 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    step=[350, 480])
-total_epochs = 500
+    step=[450, 550])
+total_epochs = 600
 channel_cfg = dict(
-    dataset_joints=17,
+    dataset_joints=14,
     dataset_channel=[
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
     ],
-    inference_channel=[
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
-    ])
+    inference_channel=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
 
 data_cfg = dict(
-    image_size=448,
-    base_size=448,
+    image_size=256,
+    base_size=256,
     base_sigma=2,
-    heatmap_size=[112, 224],
+    heatmap_size=[64, 128],
     num_joints=channel_cfg['dataset_joints'],
     dataset_channel=channel_cfg['dataset_channel'],
     inference_channel=channel_cfg['inference_channel'],
     num_scales=2,
-    scale_aware_sigma=False,
-)
+    scale_aware_sigma=False)
 
 model = dict(
     type='AssociativeEmbedding',
+    pretrained='/home/junyan/litepose-model-zoo'
+    '/crowd/crowdpose-XS-pretrain-mmpose.pth',
     backbone=dict(
         type='LitePose',
         input_channel=16,
         num_blocks=(6, 8, 10, 10),
         strides=(2, 2, 2, 1),
-        channels=(16, 32, 48, 120),
+        channels=(16, 32, 48, 80),
         block_settings=([[6, 7]] * 6, [[6, 7]] * 8, [[6, 7]] * 10,
                         [[6, 7]] * 10)),
     keypoint_head=dict(
         type='LitePoseHead',
-        deconv_setting=(32, 24, 32),
+        deconv_setting=[16, 24, 24],
         num_deconv_layers=3,
-        num_deconv_kernels=(4, 4, 4),
-        num_joints=17,
+        num_deconv_kernels=[4, 4, 4],
+        num_joints=channel_cfg['dataset_joints'],
         tag_per_joint=True,
-        with_heatmaps_loss=(True, True),
-        with_ae_loss=(True, False),
-        channels=(16, 16, 32, 48, 120),
+        with_heatmaps_loss=[True, True],
+        with_ae_loss=[True, False],
+        channels=(16, 16, 32, 48, 80),
         loss_keypoint=dict(
             type='MultiLossFactory',
-            num_joints=17,
+            num_joints=channel_cfg['dataset_joints'],
             num_stages=2,
             ae_loss_type='exp',
             with_ae_loss=[True, False],
@@ -137,30 +135,31 @@ val_pipeline = [
 
 test_pipeline = val_pipeline
 
-data_root = 'data/coco'
+# TODO: unify dataset path
+data_root = '/dev/shm/pose/data/crowd'
 data = dict(
-    workers_per_gpu=4,
-    train_dataloader=dict(samples_per_gpu=32),
+    workers_per_gpu=8,
+    train_dataloader=dict(samples_per_gpu=48),
     val_dataloader=dict(samples_per_gpu=1),
     test_dataloader=dict(samples_per_gpu=1),
     train=dict(
-        type='BottomUpCocoDataset',
-        ann_file=f'{data_root}/annotations/person_keypoints_train2017.json',
-        img_prefix=f'{data_root}/train2017/',
+        type='BottomUpCrowdPoseDataset',
+        ann_file=f'{data_root}/json/crowdpose_trainval.json',
+        img_prefix=f'{data_root}/images/',
         data_cfg=data_cfg,
         pipeline=train_pipeline,
         dataset_info={{_base_.dataset_info}}),
     val=dict(
-        type='BottomUpCocoDataset',
-        ann_file=f'{data_root}/annotations/person_keypoints_val2017.json',
-        img_prefix=f'{data_root}/val2017/',
+        type='BottomUpCrowdPoseDataset',
+        ann_file=f'{data_root}/json/crowdpose_test.json',
+        img_prefix=f'{data_root}/images/',
         data_cfg=data_cfg,
         pipeline=val_pipeline,
         dataset_info={{_base_.dataset_info}}),
     test=dict(
-        type='BottomUpCocoDataset',
-        ann_file=f'{data_root}/annotations/person_keypoints_val2017.json',
-        img_prefix=f'{data_root}/val2017/',
+        type='BottomUpCrowdPoseDataset',
+        ann_file=f'{data_root}/json/crowdpose_test.json',
+        img_prefix=f'{data_root}/images/',
         data_cfg=data_cfg,
         pipeline=test_pipeline,
         dataset_info={{_base_.dataset_info}}),
