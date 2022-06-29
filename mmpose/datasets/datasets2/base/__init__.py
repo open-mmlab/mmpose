a370ac5f9afcbeda17dea0a3bb067c5c0a286049
liqikai1.vendor <liqikai@pjlab.org.cn>
2022-06-29 08:06:27 +0000

    [Enhance] Merge BasePoseDataset into BaseCocoDataset

--- mmpose/datasets/datasets2/base/__init__.py
+++ mmpose/datasets/datasets2/base/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base_coco_dataset import BaseCocoDataset
-from .base_pose_dataset import BasePoseDataset
 
-__all__ = ['BasePoseDataset', 'BaseCocoDataset']
+__all__ = ['BaseCocoDataset']
