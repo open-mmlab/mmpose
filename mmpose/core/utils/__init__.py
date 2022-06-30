970112ca80a9bb3a383da39c30189e4dced46de5
ly015 <liyining0712@gmail.com>
2022-06-30 22:22:34 +0800

    add BasePoseEstimator class

--- mmpose/core/utils/__init__.py
+++ mmpose/core/utils/__init__.py
@@ -1,5 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .dist_utils import allreduce_grads, sync_random_seed
 from .regularizations import WeightNormClipHook
+from .typing import ConfigType, MultiConfig, OptConfigType, OptMultiConfig
 
-__all__ = ['allreduce_grads', 'WeightNormClipHook', 'sync_random_seed']
+__all__ = [
+    'allreduce_grads', 'WeightNormClipHook', 'sync_random_seed', 'ConfigType',
+    'MultiConfig', 'OptConfigType', 'OptMultiConfig'
+]
