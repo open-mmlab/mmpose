f4be33ef2a0be7ba4d8d46574d7cd9a546d3db22
674106399 <674106399@qq.com>
2022-07-25 12:10:08 +0800

    add __init__ file for regression head

--- mmpose/models/heads/__init__.py
+++ mmpose/models/heads/__init__.py
@@ -3,3 +3,4 @@ from .base_head import BaseHead
 from .heatmap_heads import CPMHead, HeatmapHead, MSPNHead, ViPNASHead
 
 __all__ = ['BaseHead', 'HeatmapHead', 'CPMHead', 'MSPNHead', 'ViPNASHead']
+
---
+++ mmpose/models/heads/regression_heads/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .integral_regression_head import IntegralRegressionHead
+from .regression_head import RegressionHead
+
+__all__ = ['RegressionHead', 'IntegralRegressionHead']
