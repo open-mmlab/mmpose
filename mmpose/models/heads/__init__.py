6e6b41d0e9267d5c4e13a0b232558be718707849
Yining Li <liyining0712@gmail.com>
2022-07-25 11:29:58 +0800

    [Refactor] add unittest for `HeatmapHead` (#1503)
    * add unittest for HeatmapHead
    
    * add unittest
    
    * fix comments typo
    
    Co-authored-by: Tau <674106399@qq.com>

---
+++ mmpose/models/heads/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .base_head import BaseHead
+from .heatmap_heads import HeatmapHead
+
+__all__ = ['BaseHead', 'HeatmapHead']
--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -2,14 +2,14 @@
 from .classification_loss import BCELoss
 from .heatmap_loss import AdaptiveWingLoss
 from .mesh_loss import GANLoss, MeshLoss
-from .mse_loss import JointsMSELoss, JointsOHKMMSELoss
+from .mse_loss import KeypointMSELoss, KeypointOHKMMSELoss
 from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
 from .regression_loss import (BoneLoss, L1Loss, MPJPELoss, MSELoss, RLELoss,
                               SemiSupervisionLoss, SmoothL1Loss, SoftWingLoss,
                               WingLoss)
 
 __all__ = [
-    'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss',
+    'KeypointMSELoss', 'KeypointOHKMMSELoss', 'HeatmapLoss', 'AELoss',
     'MultiLossFactory', 'MeshLoss', 'GANLoss', 'SmoothL1Loss', 'WingLoss',
     'MPJPELoss', 'MSELoss', 'L1Loss', 'BCELoss', 'BoneLoss',
     'SemiSupervisionLoss', 'SoftWingLoss', 'AdaptiveWingLoss', 'RLELoss'
