61bc10fb8e8d6573462cdab4d1df6f5de6f8dbc5
liqikai <tiger19971215@sjtu.edu.cn>
2022-07-28 19:13:28 +0800

    Add test_alignment for CocoMetric

--- tests/test_metrics/test_coco_metric.py
+++ tests/test_metrics/test_coco_metric.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import copy
 import os.path as osp
 import tempfile
 from collections import defaultdict
@@ -120,7 +121,6 @@ class TestCocoMetric(TestCase):
     def test_topdown_evaluate(self):
         """test topdown-style COCO metric evaluation."""
         # case 1: score_mode='bbox', nms_mode='none'
-
         coco_metric = CocoMetric(
             ann_file=self.ann_file,
             outfile_prefix=f'{self.tmp_dir.name}/test',
@@ -235,3 +235,183 @@ class TestCocoMetric(TestCase):
                 AssertionError,
                 'Ground truth annotations are required for evaluation'):
             _ = CocoMetric(ann_file=tmp_ann_file, format_only=False)
+
+    def test_topdown_alignment(self):
+        """Test whether the output of CocoMetric and the original
+        TopDownCocoDataset are the same."""
+        data = []
+        for ann in self.db['annotations']:
+            w, h = ann['bbox'][2], ann['bbox'][3]
+            data_batch = {}
+            data_batch['inputs'] = None
+            data_batch['data_sample'] = {
+                'id': ann['id'],
+                'img_id': ann['image_id'],
+                'bbox_scales': np.array([w * 1.25, h * 1.25]).reshape(-1, 2),
+            }
+            predictions = {}
+            keypoints = np.array(
+                ann['keypoints'], dtype=np.float32).reshape(1, 17, 3)
+            keypoints[0, :, 0] = keypoints[0, :, 0] * 0.98
+            keypoints[0, :, 1] = keypoints[0, :, 1] * 1.02
+            keypoints[0, :, 2] = keypoints[0, :, 2] * 0.8
+
+            predictions['pred_instances'] = {
+                'keypoints': torch.tensor(keypoints).reshape((1, -1, 3)),
+                'scores': torch.ones((1, 1)) * 0.98
+            }
+            # batch size = 1
+            data_batch = [data_batch]
+            predictions = [predictions]
+            data.append((data_batch, predictions))
+
+        # case 1:
+        # typical setting: score_mode='bbox_keypoint', nms_mode='oks_nms'
+        coco_metric = CocoMetric(
+            ann_file=self.ann_file,
+            outfile_prefix=f'{self.tmp_dir.name}/test',
+            score_mode='bbox_keypoint',
+            nms_mode='oks_nms')
+        coco_metric.dataset_meta = self.coco_dataset_meta
+
+        # process samples
+        for batch, preds in data:
+            coco_metric.process(batch, preds)
+
+        eval_results = coco_metric.evaluate(size=len(data))
+
+        target = {
+            'coco/AP': 0.5287458745874587,
+            'coco/AP .5': 0.9042904290429042,
+            'coco/AP .75': 0.5009900990099009,
+            'coco/AP (M)': 0.42475247524752474,
+            'coco/AP (L)': 0.6219554455445544,
+            'coco/AR': 0.5833333333333333,
+            'coco/AR .5': 0.9166666666666666,
+            'coco/AR .75': 0.5833333333333334,
+            'coco/AR (M)': 0.44000000000000006,
+            'coco/AR (L)': 0.6857142857142857,
+        }
+
+        for key in eval_results.keys():
+            self.assertAlmostEqual(eval_results[key], target[key])
+
+        self.assertTrue(
+            osp.isfile(osp.join(self.tmp_dir.name, 'test.keypoints.json')))
+
+        # case 2: score_mode='bbox_rle', nms_mode='oks_nms'
+        coco_metric = CocoMetric(
+            ann_file=self.ann_file,
+            outfile_prefix=f'{self.tmp_dir.name}/test',
+            score_mode='bbox_rle',
+            nms_mode='oks_nms')
+        coco_metric.dataset_meta = self.coco_dataset_meta
+
+        # process samples
+        for batch, preds in data:
+            coco_metric.process(batch, preds)
+
+        eval_results = coco_metric.evaluate(size=len(data))
+
+        target = {
+            'coco/AP': 0.5004950495049505,
+            'coco/AP .5': 0.8836633663366337,
+            'coco/AP .75': 0.4679867986798679,
+            'coco/AP (M)': 0.42475247524752474,
+            'coco/AP (L)': 0.5814108910891089,
+            'coco/AR': 0.5833333333333333,
+            'coco/AR .5': 0.9166666666666666,
+            'coco/AR .75': 0.5833333333333334,
+            'coco/AR (M)': 0.44000000000000006,
+            'coco/AR (L)': 0.6857142857142857,
+        }
+
+        for key in eval_results.keys():
+            self.assertAlmostEqual(eval_results[key], target[key])
+
+        self.assertTrue(
+            osp.isfile(osp.join(self.tmp_dir.name, 'test.keypoints.json')))
+
+        data = []
+        anns = self.db['annotations']
+        for i, ann in enumerate(anns):
+            w, h = ann['bbox'][2], ann['bbox'][3]
+            data_batch0 = {}
+            data_batch0['inputs'] = None
+            data_batch0['data_sample'] = {
+                'id': ann['id'],
+                'img_id': ann['image_id'],
+                'bbox_scales': np.array([w * 1.25, h * 1.25]).reshape(-1, 2),
+            }
+            predictions0 = {}
+            keypoints = np.array(
+                ann['keypoints'], dtype=np.float32).reshape(1, 17, 3)
+            keypoints[0, :, 0] = keypoints[0, :, 0] * (1 - i / 100)
+            keypoints[0, :, 1] = keypoints[0, :, 1] * (1 + i / 100)
+            keypoints[0, :, 2] = keypoints[0, :, 2] * (1 - i / 100)
+
+            predictions0['pred_instances'] = {
+                'keypoints': torch.tensor(keypoints).reshape((1, -1, 3)),
+                'scores': torch.ones((1, 1))
+            }
+
+            data_batch1 = {}
+            data_batch1['inputs'] = None
+            data_batch1['data_sample'] = {
+                'id': ann['id'] + 1,
+                'img_id': ann['image_id'],
+                'bbox_scales': np.array([w * 1.25, h * 1.25]).reshape(-1, 2),
+            }
+            predictions1 = {}
+            keypoints = np.array(
+                ann['keypoints'], dtype=np.float32).reshape(1, 17, 3)
+            keypoints[0, :, 0] = keypoints[0, :, 0] * (1 + i / 100)
+            keypoints[0, :, 1] = keypoints[0, :, 1] * (1 - i / 100)
+            keypoints[0, :, 2] = keypoints[0, :, 2] * (1 - 2 * i / 100)
+
+            predictions1['pred_instances'] = {
+                'keypoints':
+                torch.tensor(copy.deepcopy(keypoints)).reshape((1, -1, 3)),
+                'scores':
+                torch.ones((1, 1)) * (1 - 2 * i / 100)
+            }
+
+            # batch size = 2
+            data_batch = [data_batch0, data_batch1]
+            predictions = [predictions0, predictions1]
+            data.append((data_batch, predictions))
+
+        # case 3: score_mode='bbox_keypoint', nms_mode='oks_nms'
+        coco_metric = CocoMetric(
+            ann_file=self.ann_file,
+            outfile_prefix=f'{self.tmp_dir.name}/test',
+            score_mode='bbox_keypoint',
+            keypoint_score_thr=0.2,
+            nms_thr=0.9,
+            nms_mode='oks_nms')
+        coco_metric.dataset_meta = self.coco_dataset_meta
+
+        # process samples
+        for batch, preds in data:
+            coco_metric.process(batch, preds)
+
+        eval_results = coco_metric.evaluate(size=len(data) * 2)
+
+        target = {
+            'coco/AP': 0.19344059405940592,
+            'coco/AP .5': 0.297029702970297,
+            'coco/AP .75': 0.10891089108910887,
+            'coco/AP (M)': 0.0,
+            'coco/AP (L)': 0.3329561527581329,
+            'coco/AR': 0.2416666666666666,
+            'coco/AR .5': 0.3333333333333333,
+            'coco/AR .75': 0.16666666666666666,
+            'coco/AR (M)': 0.0,
+            'coco/AR (L)': 0.41428571428571426,
+        }
+
+        for key in eval_results.keys():
+            self.assertAlmostEqual(eval_results[key], target[key])
+
+        self.assertTrue(
+            osp.isfile(osp.join(self.tmp_dir.name, 'test.keypoints.json')))
