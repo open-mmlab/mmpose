186742d3df5a7f82af2a02f968110b07467a00da
Yining Li <liyining0712@gmail.com>
2022-08-03 00:33:01 +0800

    [Refactor] reorganize config folder structure (#1528)
    * reorganize config folder structure
    
    * add runtime and schedule configs

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,23 +1,39 @@
-checkpoint_config = dict(interval=10)
+default_scope = 'mmpose'
 
-log_config = dict(
-    interval=50,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
-    ])
+# hooks
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', interval=1),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    # TODO: Add visualizer hook
+    # visualization=dict(type='PoseVisualizationHook'))
+)
 
-log_level = 'INFO'
-load_from = None
-resume_from = None
-dist_params = dict(backend='nccl')
-workflow = [('train', 1)]
+# multi-processing backend
+env_cfg = dict(
+    cudnn_benchmark=False,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'),
+)
 
-# disable opencv multithreading to avoid system being overloaded
-opencv_num_threads = 0
-# set multi-process start method as `fork` to speed up the training
-mp_start_method = 'fork'
-
+# visualizer
 vis_backends = [dict(type='LocalVisBackend')]
 visualizer = dict(
     type='PoseLocalVisualizer', vis_backends=vis_backends, name='visualizer')
+
+# logger
+log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
+log_level = 'INFO'
+load_from = None
+resume = False
+
+# file I/O backend
+file_client_args = dict(backend='disk')
+# file_client_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/': 's3://openmmlab/datasets/pose/',
+#         'data/': 's3://openmmlab/datasets/pose/'
+#     }))
