932509eeca61b6fb7265bae8fb061dac1dc18202
Yining Li <liyining0712@gmail.com>
2022-08-16 20:52:27 +0800

    [Feature] add `MultilevelPixelData` (#1569)
    * add MultilevelPixelData
    
    * complete docstrings

--- mmpose/structures/__init__.py
+++ mmpose/structures/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .multilevel_pixel_data import MultilevelPixelData
 from .pose_data_sample import PoseDataSample
 
-__all__ = ['PoseDataSample']
+__all__ = ['PoseDataSample', 'MultilevelPixelData']
