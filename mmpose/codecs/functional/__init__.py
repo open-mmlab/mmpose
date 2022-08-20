1306fa08cbc8c7b0110ac8f287788de35ff8ae9d
ly015 <liyining0712@gmail.com>
2022-08-20 15:18:32 +0800

    refactor decoding [wip]

--- mmpose/codecs/functional/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .gaussian_heatmap import (generate_gaussian_heatmaps,
-                               generate_udp_gaussian_heatmaps,
-                               generate_unbiased_gaussian_heatmaps)
-
-__all__ = [
-    'generate_gaussian_heatmaps', 'generate_udp_gaussian_heatmaps',
-    'generate_unbiased_gaussian_heatmaps'
-]
