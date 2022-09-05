af2cacd8492bea3b18d7eb407c5b9e52af1ad2fb
Peng Lu <penglu2097@gmail.com>
2022-09-05 11:57:33 +0800

    add TopDownGetBboxCenterScale in pipelines for res50_mpii_256x256_rle (#1632)

--- configs/body/2d_kpt_sview_rgb_img/deeppose/mpii/res50_mpii_256x256_rle.py
+++ configs/body/2d_kpt_sview_rgb_img/deeppose/mpii/res50_mpii_256x256_rle.py
@@ -60,6 +60,7 @@ data_cfg = dict(
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
+    dict(type='TopDownGetBboxCenterScale', padding=1.25),
     dict(type='TopDownRandomFlip', flip_prob=0.5),
     dict(
         type='TopDownGetRandomScaleRotation', rot_factor=40, scale_factor=0.5),
@@ -81,6 +82,7 @@ train_pipeline = [
 
 val_pipeline = [
     dict(type='LoadImageFromFile'),
+    dict(type='TopDownGetBboxCenterScale', padding=1.25),
     dict(type='TopDownAffine'),
     dict(type='ToTensor'),
     dict(
