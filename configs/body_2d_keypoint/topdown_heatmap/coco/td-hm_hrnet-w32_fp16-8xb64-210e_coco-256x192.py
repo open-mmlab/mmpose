fd9b2d9e1a92d511d79a31f8e4f6b98c7bee493d
lupeng <penglu2097@gmail.com>
2022-09-07 10:29:33 +0800

    fix configs with setting fp16

--- configs/body_2d_keypoint/topdown_heatmap/coco/td-hm_hrnet-w32_fp16-8xb64-210e_coco-256x192.py
+++ configs/body_2d_keypoint/topdown_heatmap/coco/td-hm_hrnet-w32_fp16-8xb64-210e_coco-256x192.py
@@ -1,4 +1,7 @@
 _base_ = ['./td-hm_hrnet-w32_8xb64-210e_coco-256x192.py']
 
 # fp16 settings
-fp16 = dict(loss_scale='dynamic')
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    loss_scale='dynamic',
+)
