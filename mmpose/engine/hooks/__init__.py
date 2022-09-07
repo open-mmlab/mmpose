5c8c56e32ff7956bd7e73605d051957866341f92
Tau-J <674106399@qq.com>
2022-09-07 21:24:42 +0800

    remove hook

--- mmpose/engine/hooks/__init__.py
+++ mmpose/engine/hooks/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .model_util_hook import ModelSetEpochHook
 from .visualization_hook import PoseVisualizationHook
 
-__all__ = ['PoseVisualizationHook', 'ModelSetEpochHook']
+__all__ = ['PoseVisualizationHook']
