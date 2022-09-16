0603de883b959120746672865dc740136bbb8e64
Qikai Li <87690686+liqikai9@users.noreply.github.com>
2022-09-16 18:11:30 +0800

    [Refactor] Move `file_client_args` to `default_runtime` and rename `test_pipeline` to `val_pipeline` (#1651)

--- configs/animal_2d_keypoint/topdown_heatmap/locust/td-hm_res152_8xb32-210e_locust-160x160.py
+++ configs/animal_2d_keypoint/topdown_heatmap/locust/td-hm_res152_8xb32-210e_locust-160x160.py
@@ -63,11 +63,9 @@ dataset_type = 'LocustDataset'
 data_mode = 'topdown'
 data_root = 'data/locust/'
 
-file_client_args = dict(backend='disk')
-
 # pipelines
 train_pipeline = [
-    dict(type='LoadImage', file_client_args=file_client_args),
+    dict(type='LoadImage', file_client_args={{_base_.file_client_args}}),
     dict(type='GetBBoxCenterScale', padding=0.8),
     dict(type='RandomFlip', direction='horizontal'),
     dict(
@@ -79,8 +77,8 @@ train_pipeline = [
     dict(type='GenerateTarget', target_type='heatmap', encoder=codec),
     dict(type='PackPoseInputs')
 ]
-test_pipeline = [
-    dict(type='LoadImage', file_client_args=file_client_args),
+val_pipeline = [
+    dict(type='LoadImage', file_client_args={{_base_.file_client_args}}),
     dict(type='GetBBoxCenterScale', padding=0.8),
     dict(type='TopdownAffine', input_size=codec['input_size']),
     dict(type='PackPoseInputs')
@@ -113,7 +111,7 @@ val_dataloader = dict(
         ann_file='annotations/locust_test.json',
         data_prefix=dict(img='images/'),
         test_mode=True,
-        pipeline=test_pipeline,
+        pipeline=val_pipeline,
     ))
 test_dataloader = val_dataloader
 
--- configs/body_2d_keypoint/topdown_heatmap/coco/td-hm_vipnas-res50_8xb64-210e_coco-256x192.py
+++ configs/body_2d_keypoint/topdown_heatmap/coco/td-hm_vipnas-res50_8xb64-210e_coco-256x192.py
@@ -59,11 +59,9 @@ dataset_type = 'CocoDataset'
 data_mode = 'topdown'
 data_root = 'data/coco/'
 
-file_client_args = dict(backend='disk')
-
 # pipelines
 train_pipeline = [
-    dict(type='LoadImage', file_client_args=file_client_args),
+    dict(type='LoadImage', file_client_args={{_base_.file_client_args}}),
     dict(type='GetBBoxCenterScale'),
     dict(type='RandomFlip', direction='horizontal'),
     dict(type='RandomHalfBody'),
@@ -75,8 +73,8 @@ train_pipeline = [
     dict(type='GenerateTarget', target_type='heatmap', encoder=codec),
     dict(type='PackPoseInputs')
 ]
-test_pipeline = [
-    dict(type='LoadImage', file_client_args=file_client_args),
+val_pipeline = [
+    dict(type='LoadImage', file_client_args={{_base_.file_client_args}}),
     dict(type='GetBBoxCenterScale'),
     dict(type='TopdownAffine', input_size=codec['input_size']),
     dict(type='PackPoseInputs')
@@ -111,7 +109,7 @@ val_dataloader = dict(
         'COCO_val2017_detections_AP_H_56_person.json',
         data_prefix=dict(img='val2017/'),
         test_mode=True,
-        pipeline=test_pipeline,
+        pipeline=val_pipeline,
     ))
 test_dataloader = val_dataloader
 
