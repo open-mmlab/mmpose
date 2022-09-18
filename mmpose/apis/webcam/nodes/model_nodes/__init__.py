3f1210a0f3648eccef15b4aaf103a10e05a21545
Peng Lu <penglu2097@gmail.com>
2022-09-19 02:03:36 +0800

    [Refactor] Add webcam API and demo (#1638)

---
+++ mmpose/apis/webcam/nodes/model_nodes/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .detector_node import DetectorNode
+from .pose_estimator_node import TopDownPoseEstimatorNode
+
+__all__ = ['DetectorNode', 'TopDownPoseEstimatorNode']
