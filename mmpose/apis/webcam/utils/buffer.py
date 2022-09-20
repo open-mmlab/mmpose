3edf57da5186808280cc5f0399b599f989eff31b
lupeng <penglu2097@gmail.com>
2022-09-20 10:00:32 +0800

    use string typehint for compatibility with python3.6

--- mmpose/apis/webcam/utils/buffer.py
+++ mmpose/apis/webcam/utils/buffer.py
@@ -1,5 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from __future__ import annotations
 from functools import wraps
 from queue import Queue
 from typing import Any, Dict, List, Optional
@@ -174,7 +173,7 @@ class BufferManager():
         """
         return self._buffers[name].full()
 
-    def get_sub_manager(self, buffer_names: List[str]) -> BufferManager:
+    def get_sub_manager(self, buffer_names: List[str]) -> 'BufferManager':
         """Return a :class:`BufferManager` instance that covers a subset of the
         buffers in the parent. The is usually used to partially share the
         buffers of the executor to the node.
