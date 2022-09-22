46d0b72e05eebe3ccf477e8d3eff97fbb697f5b7
Tau-J <674106399@qq.com>
2022-09-22 16:45:45 +0800

    fix normalize

--- mmpose/codecs/simcc_label.py
+++ mmpose/codecs/simcc_label.py
@@ -239,7 +239,7 @@ class SimCCLabel(BaseKeypointCodec):
             target_x[n, k] = np.exp(-((x - mu_x)**2) / (2 * self.sigma**2))
             target_y[n, k] = np.exp(-((y - mu_y)**2) / (2 * self.sigma**2))
 
-        if self.normalized:
+        if self.normalize:
             norm_value = self.sigma * np.sqrt(np.pi * 2)
             target_x /= norm_value
             target_y /= norm_value
