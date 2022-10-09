8a5f7252c18cd31e387ccce2be96d2b1b2abd3c7
Qikai Li <87690686+liqikai9@users.noreply.github.com>
2022-10-09 16:07:39 +0800

    [Fix] Fix udp regress (#1682)

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -2,16 +2,17 @@
 from .classification_loss import BCELoss, JSDiscretLoss, KLDiscretLoss
 from .heatmap_loss import AdaptiveWingLoss
 from .loss_wrappers import MultipleLossWrapper
-from .mse_loss import KeypointMSELoss, KeypointOHKMMSELoss
+from .mse_loss import (CombinedTargetMSELoss, KeypointMSELoss,
+                       KeypointOHKMMSELoss)
 from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
 from .regression_loss import (BoneLoss, L1Loss, MPJPELoss, MSELoss, RLELoss,
                               SemiSupervisionLoss, SmoothL1Loss, SoftWingLoss,
                               WingLoss)
 
 __all__ = [
-    'KeypointMSELoss', 'KeypointOHKMMSELoss', 'HeatmapLoss', 'AELoss',
-    'MultiLossFactory', 'SmoothL1Loss', 'WingLoss', 'MPJPELoss', 'MSELoss',
-    'L1Loss', 'BCELoss', 'BoneLoss', 'SemiSupervisionLoss', 'SoftWingLoss',
-    'AdaptiveWingLoss', 'RLELoss', 'KLDiscretLoss', 'MultipleLossWrapper',
-    'JSDiscretLoss'
+    'KeypointMSELoss', 'KeypointOHKMMSELoss', 'CombinedTargetMSELoss',
+    'HeatmapLoss', 'AELoss', 'MultiLossFactory', 'SmoothL1Loss', 'WingLoss',
+    'MPJPELoss', 'MSELoss', 'L1Loss', 'BCELoss', 'BoneLoss',
+    'SemiSupervisionLoss', 'SoftWingLoss', 'AdaptiveWingLoss', 'RLELoss',
+    'KLDiscretLoss', 'MultipleLossWrapper', 'JSDiscretLoss'
 ]
