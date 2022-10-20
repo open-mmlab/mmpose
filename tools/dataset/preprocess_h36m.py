28a0d7de2dbdafe6e45d365226e22e2a9e8ebe00
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 07:45:08 +0000

    Adding tarfile member sanitization to extractall()

--- tools/dataset/preprocess_h36m.py
+++ tools/dataset/preprocess_h36m.py
@@ -98,7 +98,26 @@ class PreprocessH36m:
                 filename = join(cur_dir, file + '.tgz')
                 print(f'Extracting {filename} ...')
                 with tarfile.open(filename) as tar:
-                    tar.extractall(self.extracted_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, self.extracted_dir)
         print('Extraction done.\n')
 
     def generate_cameras_file(self):
