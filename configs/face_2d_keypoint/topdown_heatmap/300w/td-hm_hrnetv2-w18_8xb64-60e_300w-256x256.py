d1908b78f062de34220086a5764fc4ef042c69bd
ly015 <liyining0712@gmail.com>
2022-10-24 14:29:46 +0800

    fix save-best rule for NME

--- configs/face_2d_keypoint/topdown_heatmap/300w/td-hm_hrnetv2-w18_8xb64-60e_300w-256x256.py
+++ configs/face_2d_keypoint/topdown_heatmap/300w/td-hm_hrnetv2-w18_8xb64-60e_300w-256x256.py
@@ -27,8 +27,7 @@ param_scheduler = [
 auto_scale_lr = dict(base_batch_size=512)
 
 # hooks
-default_hooks = dict(
-    checkpoint=dict(save_best='nme/@[36, 45]', rule='greater'))
+default_hooks = dict(checkpoint=dict(save_best='nme/@[36, 45]', rule='less'))
 
 # codec settings
 codec = dict(
