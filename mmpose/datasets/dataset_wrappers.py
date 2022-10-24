d753c43c2fd7167920393031622120140931bad5
lupeng <penglu2097@gmail.com>
2022-10-24 14:39:35 +0800

    support combined dataset

--- mmpose/datasets/dataset_wrappers.py
+++ mmpose/datasets/dataset_wrappers.py
@@ -1,5 +1,13 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .builder import DATASETS
+
+from copy import deepcopy
+from typing import Any, Callable, List, Tuple, Union
+
+from mmengine.dataset import BaseDataset
+from mmengine.registry import build_from_cfg
+
+from mmpose.registry import DATASETS
+from .datasets.utils import parse_pose_metainfo
 
 
 @DATASETS.register_module()
@@ -29,3 +37,106 @@ class RepeatDataset:
     def __len__(self):
         """Length after repetition."""
         return self.times * self._ori_len
+
+
+@DATASETS.register_module()
+class CombinedDataset(BaseDataset):
+    """A wrapper of combined dataset.
+
+    Args:
+        metainfo (dict): The meta information of combined dataset.
+        datasets (list): The configs of datasets to be combined.
+        pipeline (list, optional): Processing pipeline. Defaults to [].
+    """
+
+    def __init__(self,
+                 metainfo: dict,
+                 datasets: list,
+                 pipeline: List[Union[dict, Callable]] = [],
+                 **kwargs):
+
+        self.datasets = []
+
+        for cfg in datasets:
+            dataset = build_from_cfg(cfg['dataset'], DATASETS)
+            self.datasets.append(dataset)
+
+        self._lens = [len(dataset) for dataset in self.datasets]
+        self._len = sum(self._lens)
+
+        super(CombinedDataset, self).__init__(pipeline=pipeline, **kwargs)
+        self._metainfo = parse_pose_metainfo(metainfo)
+
+    @property
+    def metainfo(self):
+        return deepcopy(self._metainfo)
+
+    def __len__(self):
+        return self._len
+
+    def _get_subset_index(self, index: int) -> Tuple[int, int]:
+        """Given a data sample's global index, return the index of the sub-
+        dataset the data sample belongs to, and the local index within that
+        sub-dataset.
+
+        Args:
+            index (int): The global data sample index
+
+        Returns:
+            tuple[int, int]:
+            - subset_index (int): The index of the sub-dataset
+            - local_index (int): The index of the data sample within
+                the sub-dataset
+        """
+        if index >= len(self) or index < -len(self):
+            raise ValueError(
+                f'index({index}) is out of bounds for dataset with '
+                f'length({len(self)}).')
+
+        if index < 0:
+            index = index + len(self)
+
+        subset_index = 0
+        while index >= self._lens[subset_index]:
+            index -= self._lens[subset_index]
+            subset_index += 1
+        return subset_index, index
+
+    def prepare_data(self, idx: int) -> Any:
+        """Get data processed by ``self.pipeline``.The source dataset is
+        depending on the index.
+
+        Args:
+            idx (int): The index of ``data_info``.
+
+        Returns:
+            Any: Depends on ``self.pipeline``.
+        """
+
+        subset_idx, sample_idx = self._get_subset_index(idx)
+        # Get data sample from the subset
+        data_info = self.datasets[subset_idx].get_data_info(sample_idx)
+
+        # Add metainfo items that are required in the pipeline and the model
+        metainfo_keys = [
+            'upper_body_ids', 'lower_body_ids', 'flip_pairs',
+            'dataset_keypoint_weights', 'flip_indices'
+        ]
+
+        for key in metainfo_keys:
+            assert key not in data_info, (
+                f'"{key}" is a reserved key for `metainfo`, but already '
+                'exists in the `data_info`.')
+            data_info[key] = deepcopy(self._metainfo[key])
+
+        return self.pipeline(data_info)
+
+    def full_init(self):
+        """Fully initialize all sub datasets."""
+
+        if self._fully_initialized:
+            return
+
+        for dataset in self.datasets:
+            dataset.full_init()
+        self._fully_initialized = True
---
+++ tests/test_datasets/test_transforms/test_converting.py
@@ -0,0 +1,36 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from unittest import TestCase
+
+from mmpose.datasets.transforms import KeypointConverter
+from mmpose.testing import get_coco_sample
+
+
+class TestKeypointConverter(TestCase):
+
+    def setUp(self):
+        # prepare dummy bottom-up data sample with COCO metainfo
+        self.data_info = get_coco_sample(
+            img_shape=(240, 320), num_instances=4, with_bbox_cs=True)
+
+    def test_transform(self):
+        mapping = [(3, 0), (6, 1), (16, 2), (5, 3)]
+        transform = KeypointConverter(num_keypoints=5, mapping=mapping)
+        results = transform(self.data_info.copy())
+
+        # check shape
+        self.assertEqual(results['keypoints'].shape[0],
+                         self.data_info['keypoints'].shape[0])
+        self.assertEqual(results['keypoints'].shape[1], 5)
+        self.assertEqual(results['keypoints'].shape[2], 2)
+        self.assertEqual(results['keypoints_visible'].shape[0],
+                         self.data_info['keypoints_visible'].shape[0])
+        self.assertEqual(results['keypoints_visible'].shape[1], 5)
+
+        # check value
+        for source_index, target_index in mapping:
+            self.assertTrue((results['keypoints'][:, target_index] ==
+                             self.data_info['keypoints'][:,
+                                                         source_index]).all())
+            self.assertTrue(
+                (results['keypoints_visible'][:, target_index] ==
+                 self.data_info['keypoints_visible'][:, source_index]).all())
