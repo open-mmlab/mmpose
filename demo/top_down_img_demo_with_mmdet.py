8efb1696cb8fd240837bd8d208dea1715a0a4ef7
dineshreddy91 <you@example.com>
2022-10-27 17:59:09 -0400

    update inference per car

--- demo/top_down_img_demo_with_mmdet.py
+++ demo/top_down_img_demo_with_mmdet.py
@@ -71,8 +71,10 @@ def main():
     assert args.det_config is not None
     assert args.det_checkpoint is not None
 
+    '''
     det_model = init_detector(
         args.det_config, args.det_checkpoint, device=args.device.lower())
+    '''
     # build the pose model from a config file and a checkpoint file
     pose_model = init_pose_model(
         args.pose_config, args.pose_checkpoint, device=args.device.lower())
@@ -86,52 +88,82 @@ def main():
             DeprecationWarning)
     else:
         dataset_info = DatasetInfo(dataset_info)
+    '''
+    python3 demo/top_down_img_demo_with_mmdet.py mmdetection/configs/swin/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco_20210903_104808-b92c91f1.pth configs/vehicle/2d_kpt_sview_rgb_img/topdown_heatmap/carfusion/hrnet_w32_carfusion_384x288.py ./trained_files/latest.pth --bbox-thr 0.12 --kpt-thr 0.4 --thickness 2 --radius 6 --det-cat-id 3 --img-root data/ --out-img-root run/ --img o_10015.jpg
+    python3 demo/top_down_img_demo_with_mmdet.py mmdetection/configs/swin/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco_20210903_104808-b92c91f1.pth configs/vehicle/2d_kpt_sview_rgb_img/topdown_heatmap/carfusion/hrnet_w32_carfusion_384x288.py ./trained_files/latest.pth --bbox-thr 0.3 --kpt-thr 0.4 --thickness 2 --radius 6 --det-cat-id 3 --img-root ../CVPR2023/data/shaler3/Cwalt_database/ --out-img-root ../CVPR2023/data/shaler3/ --img 1.jpg
+    '''
 
-    image_name = os.path.join(args.img_root, args.img)
+    import glob
+    root = args.img_root
+    for folder in glob.glob(root+'/*'):
+        print(folder)
+        for img_name in glob.glob(folder+'/*.jpg'):
+            args.img = img_name
+            if '.npy' in img_name:
+                continue
+            if os.path.exists(img_name + '.npz'):
+                continue
+
+            image_name = os.path.join(args.img)
+
+            '''
 
     # test a single image, the resulting box is (x1, y1, x2, y2)
     mmdet_results = inference_detector(det_model, image_name)
 
     # keep the person class bounding boxes.
     person_results = process_mmdet_results(mmdet_results, args.det_cat_id)
+            '''
 
-    # test a single image, with a list of bboxes.
+            person_results_single = []
+            import numpy as np
+            import imagesize
+    
 
-    # optional
-    return_heatmap = False
+            width, height = imagesize.get(image_name)
+            print(image_name, width, height)
 
-    # e.g. use ('backbone', ) to return backbone feature
-    output_layer_names = None
+            person_results_single.append({'bbox': np.array([0,0,width,height,0.99], dtype='float32')})
+            person_results = person_results_single
 
-    pose_results, returned_outputs = inference_top_down_pose_model(
-        pose_model,
-        image_name,
-        person_results,
-        bbox_thr=args.bbox_thr,
-        format='xyxy',
-        dataset=dataset,
-        dataset_info=dataset_info,
-        return_heatmap=return_heatmap,
-        outputs=output_layer_names)
+            # test a single image, with a list of bboxes.
 
-    if args.out_img_root == '':
-        out_file = None
-    else:
-        os.makedirs(args.out_img_root, exist_ok=True)
-        out_file = os.path.join(args.out_img_root, f'vis_{args.img}')
+            # optional
+            return_heatmap = False
 
-    # show the results
-    vis_pose_result(
-        pose_model,
-        image_name,
-        pose_results,
-        dataset=dataset,
-        dataset_info=dataset_info,
-        kpt_score_thr=args.kpt_thr,
-        radius=args.radius,
-        thickness=args.thickness,
-        show=args.show,
-        out_file=out_file)
+            # e.g. use ('backbone', ) to return backbone feature
+            output_layer_names = None
+
+            pose_results, returned_outputs = inference_top_down_pose_model(
+                pose_model,
+                image_name,
+                person_results,
+                bbox_thr=args.bbox_thr,
+                format='xyxy',
+                dataset=dataset,
+                dataset_info=dataset_info,
+                return_heatmap=return_heatmap,
+                outputs=output_layer_names)
+            np.save(img_name+'.npy',pose_results)
+
+            if args.out_img_root == '':
+                out_file = None
+            else:
+                os.makedirs(args.out_img_root, exist_ok=True)
+                out_file = os.path.join(args.out_img_root, f'vis_{args.img}')
+
+            # show the results
+            vis_pose_result(
+                pose_model,
+                image_name,
+                pose_results,
+                dataset=dataset,
+                dataset_info=dataset_info,
+                kpt_score_thr=args.kpt_thr,
+                radius=args.radius,
+                thickness=args.thickness,
+                show=args.show,
+                out_file=out_file)
 
 
 if __name__ == '__main__':
