71e5b4ad06c2936d376c61f7983e00172f5342e7
Tau-J <674106399@qq.com>
2022-11-09 13:19:03 +0800

    import in __init__.py

--- mmpose/codecs/utils/__init__.py
+++ mmpose/codecs/utils/__init__.py
@@ -4,7 +4,8 @@ from .gaussian_heatmap import (generate_gaussian_heatmaps,
                                generate_unbiased_gaussian_heatmaps)
 from .offset_heatmap import generate_offset_heatmap
 from .post_processing import (batch_heatmap_nms, gaussian_blur,
-                              get_heatmap_maximum, get_simcc_maximum)
+                              gaussian_blur1d, get_heatmap_maximum,
+                              get_simcc_maximum)
 from .refinement import (refine_keypoints, refine_keypoints_dark,
                          refine_keypoints_dark_udp, refine_simcc_dark)
 
@@ -13,5 +14,5 @@ __all__ = [
     'generate_unbiased_gaussian_heatmaps', 'gaussian_blur',
     'get_heatmap_maximum', 'get_simcc_maximum', 'generate_offset_heatmap',
     'batch_heatmap_nms', 'refine_keypoints', 'refine_keypoints_dark',
-    'refine_keypoints_dark_udp', 'refine_simcc_dark'
+    'refine_keypoints_dark_udp', 'refine_simcc_dark', 'gaussian_blur1d'
 ]
