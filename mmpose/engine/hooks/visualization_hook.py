4a0b9622e502f3715adb0a848eddc8d098437291
ChenZhenGui <1045463610@qq.com>
2022-11-10 21:35:26 +0800

    fix-visualization-hook

--- mmpose/engine/hooks/visualization_hook.py
+++ mmpose/engine/hooks/visualization_hook.py
@@ -96,11 +96,12 @@ class PoseVisualizationHook(Hook):
         total_curr_iter = runner.iter + batch_idx
 
         # Visualize only the first data
-        img_path = data_batch[0]['data_sample'].get('img_path')
+        img_path = data_batch['data_sample'][0].get('img_path')
         img_bytes = self.file_client.get(img_path)
         img = mmcv.imfrombytes(img_bytes, channel_order='rgb')
-
         data_sample = outputs[0]
+        # revert the heatmap
+        data_sample = merge_data_samples([data_sample])
         if total_curr_iter % self.interval == 0:
             self._visualizer.add_datasample(
                 os.path.basename(img_path) if self.show else 'val_img',
