f24f3a2c1b313d2e1c2e86046bb078711b3be1b0
liqikai <tiger19971215@sjtu.edu.cn>
2022-12-01 15:41:13 +0800

    [Fix] fix AP10k metric

--- mmpose/evaluation/metrics/coco_metric.py
+++ mmpose/evaluation/metrics/coco_metric.py
@@ -177,6 +177,7 @@ class CocoMetric(BaseMetric):
             pred['img_id'] = data_sample['img_id']
             pred['keypoints'] = keypoints
             pred['keypoint_scores'] = keypoint_scores
+            pred['category_id'] = data_sample.get('category_id', 1)
             bbox_scores = data_sample['gt_instances']['bbox_scores']
             if len(bbox_scores) != len(keypoints):
                 # bottom-up models might output different number of
@@ -343,6 +344,7 @@ class CocoMetric(BaseMetric):
                 instance = {
                     'id': pred['id'],
                     'img_id': pred['img_id'],
+                    'category_id': pred['category_id'],
                     'keypoints': pred['keypoints'][idx],
                     'keypoint_scores': pred['keypoint_scores'][idx],
                     'bbox_score': pred['bbox_scores'][idx],
@@ -443,7 +445,6 @@ class CocoMetric(BaseMetric):
             str: The json file name of keypoint results.
         """
         # the results with category_id
-        cat_id = 1
         cat_results = []
 
         for _, img_kpts in keypoints.items():
@@ -455,7 +456,7 @@ class CocoMetric(BaseMetric):
 
             result = [{
                 'image_id': img_kpt['img_id'],
-                'category_id': cat_id,
+                'category_id': img_kpt['category_id'],
                 'keypoints': keypoint.tolist(),
                 'score': float(img_kpt['score']),
             } for img_kpt, keypoint in zip(img_kpts, _keypoints)]
@@ -532,266 +533,3 @@ class CocoMetric(BaseMetric):
                     del kpts[img_id][i]
 
         return kpts
-
-
-@METRICS.register_module()
-class AP10KCocoMetric(CocoMetric):
-    """AP-10K evaluation metric.
-
-    Evaluate AR, AP, and mAP for keypoint detection tasks. Support AP-10K
-    dataset with annotations in COCO format. Please refer to
-    `COCO keypoint evaluation <https://cocodataset.org/#keypoints-eval>`__
-    for more details.
-
-    Args:
-        ann_file (str): Path to the coco format annotation file.
-        use_area (bool): Whether to use ``'area'`` message in the annotations.
-            If the ground truth annotations (e.g. CrowdPose, AIC) do not have
-            the field ``'area'``, please set ``use_area=False``.
-            Default: ``True``.
-        iou_type (str): The same parameter as `iouType` in
-            :class:`xtcocotools.COCOeval`, which can be ``'keypoints'``, or
-            ``'keypoints_crowd'`` (used in CrowdPose dataset).
-            Defaults to ``'keypoints'``.
-        score_mode (str): The mode to score the prediction results which
-            should be one of the following options:
-
-                - ``'bbox'``: Take the score of bbox as the score of the
-                    prediction results.
-                - ``'bbox_keypoint'``: Use keypoint score to rescore the
-                    prediction results.
-                - ``'bbox_rle'``: Use rle_score to rescore the
-                    prediction results.
-
-            Defaults to ``'bbox_keypoint'`
-        keypoint_score_thr (float): The threshold of keypoint score. The
-            keypoints with score lower than it will not be included to
-            rescore the prediction results. Valid only when ``score_mode`` is
-            ``bbox_keypoint``. Defaults to ``0.2``
-        nms_mode (str): The mode to perform Non-Maximum Suppression (NMS),
-            which should be one of the following options:
-
-                - ``'oks_nms'``: Use Object Keypoint Similarity (OKS) to
-                    perform NMS.
-                - ``'soft_oks_nms'``: Use Object Keypoint Similarity (OKS)
-                    to perform soft NMS.
-                - ``'none'``: Do not perform NMS. Typically for bottomup mode
-                    output.
-
-            Defaults to ``'oks_nms'`
-        nms_thr (float): The Object Keypoint Similarity (OKS) threshold
-            used in NMS when ``nms_mode`` is ``'oks_nms'`` or
-            ``'soft_oks_nms'``. Will retain the prediction results with OKS
-            lower than ``nms_thr``. Defaults to ``0.9``
-        format_only (bool): Whether only format the output results without
-            doing quantitative evaluation. This is designed for the need of
-            test submission when the ground truth annotations are absent. If
-            set to ``True``, ``outfile_prefix`` should specify the path to
-            store the output results. Default: ``False``.
-        outfile_prefix (str | None): The prefix of json files. It includes
-            the file path and the prefix of filename, e.g., ``'a/b/prefix'``.
-            If not specified, a temp file will be created. Default: ``None``.
-        collect_device (str): Device name used for collecting results from
-            different ranks during distributed training. Must be ``'cpu'`` or
-            ``'gpu'``. Default: ``'cpu'``.
-        prefix (str, optional): The prefix that will be added in the metric
-            names to disambiguate homonymous metrics of different evaluators.
-            If prefix is not provided in the argument, ``self.default_prefix``
-            will be used instead. Default: ``None``.
-    """
-
-    default_prefix: Optional[str] = 'ap10k'
-
-    def process(self, data_batch: Sequence[dict],
-                predictions: Sequence[dict]) -> None:
-        """Process one batch of data samples and predictions. The processed
-        results should be stored in ``self.results``, which will be used to
-        compute the metrics when all batches have been processed.
-
-        Args:
-            data_batch (Sequence[dict]): A batch of data
-                from the dataloader.
-            predictions (Sequence[dict]): A batch of outputs from
-                the model, each of which has the following keys:
-
-                - 'id': The id of the sample
-                - 'img_id': The image_id of the sample
-                - 'category': The category of instance(s)
-                - 'pred_instances': The prediction results of instance(s)
-        """
-        for _, pred in zip(data_batch, predictions):
-            if 'pred_instances' not in pred:
-                raise ValueError(
-                    '`pred_instances` are required to process the '
-                    f'predictions results in {self.__class__.__name__}. ')
-
-            # keypoints.shape: [N, K, 2],
-            # N: number of instances, K: number of keypoints
-            # for topdown-style output, N is usually 1, while for
-            # bottomup-style output, N is the number of instances in the image
-            keypoints = pred['pred_instances']['keypoints']
-            # [N, K], the scores for all keypoints of all instances
-            keypoint_scores = pred['pred_instances']['keypoint_scores']
-            assert keypoint_scores.shape == keypoints.shape[:2]
-
-            result = dict()
-            result['id'] = pred['id']
-            result['img_id'] = pred['img_id']
-            result['keypoints'] = keypoints
-            result['keypoint_scores'] = keypoint_scores
-            result['bbox_scores'] = pred['pred_instances']['bbox_scores']
-            result['category'] = pred['category']
-
-            # get area information
-            if 'bbox_scales' in pred['gt_instances']:
-                result['areas'] = np.prod(
-                    pred['gt_instances']['bbox_scales'], axis=1)
-            # add converted result to the results list
-            self.results.append(result)
-
-    def compute_metrics(self, results: list) -> Dict[str, float]:
-        """Compute the metrics from processed results.
-
-        Args:
-            results (list): The processed results of each batch.
-
-        Returns:
-            Dict[str, float]: The computed metrics. The keys are the names of
-            the metrics, and the values are corresponding results.
-        """
-        logger: MMLogger = MMLogger.get_current_instance()
-
-        tmp_dir = None
-        if self.outfile_prefix is None:
-            tmp_dir = tempfile.TemporaryDirectory()
-            outfile_prefix = osp.join(tmp_dir.name, 'results')
-        else:
-            outfile_prefix = self.outfile_prefix
-
-        kpts = defaultdict(list)
-
-        # group the results by img_id
-        for result in results:
-            img_id = result['img_id']
-            for idx in range(len(result['bbox_scores'])):
-                instance = {
-                    'id': result['id'],
-                    'img_id': result['img_id'],
-                    'keypoints': result['keypoints'][idx],
-                    'keypoint_scores': result['keypoint_scores'][idx],
-                    'bbox_score': result['bbox_scores'][idx],
-                    'category': result['category'],
-                }
-
-                if 'areas' in result:
-                    instance['area'] = result['areas'][idx]
-                else:
-                    # use keypoint to calculate bbox and get area
-                    keypoints = result['keypoints'][idx]
-                    area = (
-                        np.max(keypoints[:, 0]) - np.min(keypoints[:, 0])) * (
-                            np.max(keypoints[:, 1]) - np.min(keypoints[:, 1]))
-                    instance['area'] = area
-
-                kpts[img_id].append(instance)
-
-        # sort keypoint results according to id and remove duplicate ones
-        kpts = self._sort_and_unique_bboxes(kpts, key='id')
-
-        # score the prediction results according to `score_mode`
-        # and perform NMS according to `nms_mode`
-        valid_kpts = defaultdict(list)
-        num_keypoints = self.dataset_meta['num_keypoints']
-        for img_id, instances in kpts.items():
-            for instance in instances:
-                # concatenate the keypoint coordinates and scores
-                instance['keypoints'] = np.concatenate([
-                    instance['keypoints'], instance['keypoint_scores'][:, None]
-                ],
-                                                       axis=-1)
-                if self.score_mode == 'bbox':
-                    instance['score'] = instance['bbox_score']
-                else:
-                    bbox_score = instance['bbox_score']
-                    if self.score_mode == 'bbox_rle':
-                        keypoint_scores = instance['keypoint_scores']
-                        instance['score'] = float(bbox_score +
-                                                  np.mean(keypoint_scores) +
-                                                  np.max(keypoint_scores))
-
-                    else:  # self.score_mode == 'bbox_keypoint':
-                        mean_kpt_score = 0
-                        valid_num = 0
-                        for kpt_idx in range(num_keypoints):
-                            kpt_score = instance['keypoint_scores'][kpt_idx]
-                            if kpt_score > self.keypoint_score_thr:
-                                mean_kpt_score += kpt_score
-                                valid_num += 1
-                        if valid_num != 0:
-                            mean_kpt_score /= valid_num
-                        instance['score'] = bbox_score * mean_kpt_score
-            # perform nms
-            if self.nms_mode == 'none':
-                valid_kpts[img_id] = instances
-            else:
-                nms = oks_nms if self.nms_mode == 'oks_nms' else soft_oks_nms
-                keep = nms(
-                    instances,
-                    self.nms_thr,
-                    sigmas=self.dataset_meta['sigmas'])
-                valid_kpts[img_id] = [instances[_keep] for _keep in keep]
-
-        # convert results to coco style and dump into a json file
-        self.results2json(valid_kpts, outfile_prefix=outfile_prefix)
-
-        # only format the results without doing quantitative evaluation
-        if self.format_only:
-            logger.info('results are saved in '
-                        f'{osp.dirname(outfile_prefix)}')
-            return {}
-
-        # evaluation results
-        eval_results = OrderedDict()
-        logger.info(f'Evaluating {self.__class__.__name__}...')
-        info_str = self._do_python_keypoint_eval(outfile_prefix)
-        name_value = OrderedDict(info_str)
-        eval_results.update(name_value)
-
-        if tmp_dir is not None:
-            tmp_dir.cleanup()
-        return eval_results
-
-    def results2json(self, keypoints: Dict[int, list],
-                     outfile_prefix: str) -> str:
-        """Dump the keypoint detection results to a COCO style json file.
-
-        Args:
-            keypoints (Dict[int, list]): Keypoint detection results
-                of the dataset.
-            outfile_prefix (str): The filename prefix of the json files. If the
-                prefix is "somepath/xxx", the json files will be named
-                "somepath/xxx.keypoints.json",
-
-        Returns:
-            str: The json file name of keypoint results.
-        """
-        cat_results = []
-
-        for _, img_kpts in keypoints.items():
-            _keypoints = np.array(
-                [img_kpt['keypoints'] for img_kpt in img_kpts])
-            num_keypoints = self.dataset_meta['num_keypoints']
-            # collect all the person keypoints in current image
-            _keypoints = _keypoints.reshape(-1, num_keypoints * 3)
-
-            result = [{
-                'image_id': img_kpt['img_id'],
-                'category_id': img_kpt['category'],
-                'keypoints': keypoint.tolist(),
-                'score': float(img_kpt['score']),
-            } for img_kpt, keypoint in zip(img_kpts, _keypoints)]
-
-            cat_results.extend(result)
-
-        res_file = f'{outfile_prefix}.keypoints.json'
-        dump(cat_results, res_file, sort_keys=True, indent=4)
