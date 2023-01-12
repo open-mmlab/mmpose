ce8d585a4a0eb07f5814a94a2257d8efd5a52c3e
Tau-J <674106399@qq.com>
2023-01-12 11:53:04 +0800

    fix bug in CombinedDataset

--- mmpose/datasets/dataset_wrappers.py
+++ mmpose/datasets/dataset_wrappers.py
@@ -11,35 +11,6 @@ from .datasets.utils import parse_pose_metainfo
 
 
 @DATASETS.register_module()
-class RepeatDataset:
-    """A wrapper of repeated dataset.
-
-    The length of repeated dataset will be `times` larger than the original
-    dataset. This is useful when the data loading time is long but the dataset
-    is small. Using RepeatDataset can reduce the data loading time between
-    epochs.
-
-    Args:
-        dataset (:obj:`Dataset`): The dataset to be repeated.
-        times (int): Repeat times.
-    """
-
-    def __init__(self, dataset, times):
-        self.dataset = dataset
-        self.times = times
-
-        self._ori_len = len(self.dataset)
-
-    def __getitem__(self, idx):
-        """Get data."""
-        return self.dataset[idx % self._ori_len]
-
-    def __len__(self):
-        """Length after repetition."""
-        return self.times * self._ori_len
-
-
-@DATASETS.register_module()
 class CombinedDataset(BaseDataset):
     """A wrapper of combined dataset.
 
@@ -116,7 +87,8 @@ class CombinedDataset(BaseDataset):
         subset_idx, sample_idx = self._get_subset_index(idx)
         # Get data sample from the subset
         data_info = self.datasets[subset_idx].get_data_info(sample_idx)
-        data_info = self.datasets[subset_idx].pipeline(data_info)
+        if hasattr(self.datasets[subset_idx], 'pipeline'):
+            data_info = self.datasets[subset_idx].pipeline(data_info)
 
         # Add metainfo items that are required in the pipeline and the model
         metainfo_keys = [
