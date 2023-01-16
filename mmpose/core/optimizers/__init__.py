40c46bae84669b12db20182ee7b15ba04f905e47
Yufei <annblessus@gmail.com>
2023-01-16 17:10:26 +0800

    reformat the init file

--- mmpose/core/optimizers/__init__.py
+++ mmpose/core/optimizers/__init__.py
@@ -1,10 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .layer_decay_optimizer_constructor import (
-    LayerDecayOptimizerConstructor, LearningRateDecayOptimizerConstructor)
 from .builder import (OPTIMIZER_BUILDERS, OPTIMIZERS,
                       build_optimizer_constructor, build_optimizers)
+from .layer_decay_optimizer_constructor import (
+    LayerDecayOptimizerConstructor, LearningRateDecayOptimizerConstructor)
 
 __all__ = [
     'build_optimizers', 'build_optimizer_constructor', 'OPTIMIZERS',
-    'OPTIMIZER_BUILDERS', 'LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor'
+    'OPTIMIZER_BUILDERS', 'LearningRateDecayOptimizerConstructor',
+    'LayerDecayOptimizerConstructor'
 ]
