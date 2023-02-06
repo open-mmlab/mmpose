63ce29f3beaa4163cf3eab5f91984c3136b61b2f
Tau <674106399@qq.com>
2023-02-06 22:31:38 +0800

    [Feature] add multi-source sampler (#1938)

--- mmpose/datasets/__init__.py
+++ mmpose/datasets/__init__.py
@@ -2,6 +2,7 @@
 from .builder import build_dataset
 from .dataset_wrappers import CombinedDataset
 from .datasets import *  # noqa
+from .samplers import MultiSourceSampler
 from .transforms import *  # noqa
 
-__all__ = ['build_dataset', 'CombinedDataset']
+__all__ = ['build_dataset', 'CombinedDataset', 'MultiSourceSampler']
