0009e31a5aa8c48c11897ed7d7d9e505a1ab6aa7
Tau-J <674106399@qq.com>
2023-02-20 12:27:15 +0800

    update

--- mmpose/models/heads/__init__.py
+++ mmpose/models/heads/__init__.py
@@ -2,10 +2,10 @@
 from .base_head import BaseHead
 from .heatmap_heads import (AssociativeEmbeddingHead, CIDHead, CPMHead,
                             HeatmapHead, MSPNHead, RTMHead, SimCCHead,
-                            ViPNASHead,)
+                            ViPNASHead)
 from .hybrid_heads import DEKRHead
 from .regression_heads import (DSNTHead, IntegralRegressionHead,
-                               RegressionHead, RLEHead,)
+                               RegressionHead, RLEHead)
 
 __all__ = [
     'BaseHead', 'HeatmapHead', 'CPMHead', 'MSPNHead', 'ViPNASHead',
