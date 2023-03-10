74b44c5e10c494392410ac449016e766bb841cbd
lupeng <penglu2097@gmail.com>
2023-03-10 15:59:00 +0800

    add docstring

--- mmpose/utils/config_utils.py
+++ mmpose/utils/config_utils.py
@@ -2,7 +2,17 @@
 from mmpose.utils.typing import ConfigDict
 
 
-def convert_mmdet_test_pipeline(cfg: ConfigDict):
+def convert_mmdet_test_pipeline(cfg: ConfigDict) -> ConfigDict:
+    """Converts pipeline types in MMDetection's test dataloader to use the
+    'mmdet' namespace.
+
+    Args:
+        cfg (ConfigDict): Configuration dictionary for MMDetection.
+
+    Returns:
+        ConfigDict: Configuration dictionary with updated pipeline types.
+    """
+    # use lazy import to avoid hard dependence on mmdet
     from mmdet.datasets import transforms
 
     if 'test_dataloader' not in cfg:
