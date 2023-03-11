30154f0a79a9ff230da4fdae90cc39c43ddca70e
ly015 <liyining0712@gmail.com>
2023-03-12 04:21:49 +0800

    update get_flops.py

--- tools/analysis_tools/get_flops.py
+++ tools/analysis_tools/get_flops.py
@@ -1,71 +1,32 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
-from functools import partial
 
 import torch
-from mmengine.config import DictAction
-
-from mmpose.apis.inference import init_model
 
 try:
-    from mmcv.cnn import get_model_complexity_info
+    from fvcore.nn import (ActivationCountAnalysis, FlopCountAnalysis,
+                           flop_count_str, flop_count_table, parameter_count)
 except ImportError:
-    raise ImportError('Please upgrade mmcv to >0.6.2')
+    print('You may need to install fvcore for flops computation, '
+          'and you can use `pip install fvcore` to set up the environment')
+from fvcore.nn.print_model_statistics import _format_size
+from mmengine import Config
+
+from mmpose.models import build_pose_estimator
+from mmpose.utils import register_all_modules
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a recognizer')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument(
-        '--device',
-        default='cuda:0',
-        help='Device used for model initialization')
-    parser.add_argument(
-        '--cfg-options',
-        nargs='+',
-        action=DictAction,
-        default={},
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. For example, '
-        "'--cfg-options model.backbone.depth=18 model.backbone.with_cp=True'")
+    parser = argparse.ArgumentParser(description='Get model flops and params')
+    parser.add_argument('config', help='config file path')
     parser.add_argument(
         '--shape',
         type=int,
         nargs='+',
         default=[256, 192],
         help='input image size')
-    parser.add_argument(
-        '--input-constructor',
-        '-c',
-        type=str,
-        choices=['none', 'batch'],
-        default='none',
-        help='If specified, it takes a callable method that generates '
-        'input. Otherwise, it will generate a random tensor with '
-        'input shape to calculate FLOPs.')
-    parser.add_argument(
-        '--batch-size', '-b', type=int, default=1, help='input batch size')
-    parser.add_argument(
-        '--not-print-per-layer-stat',
-        '-n',
-        action='store_true',
-        help='Whether to print complexity information'
-        'for each layer in a model')
     args = parser.parse_args()
     return args
-
-
-def batch_constructor(flops_model, batch_size, input_shape):
-    """Generate a batch of tensors to the model."""
-    batch = {}
-
-    inputs = torch.ones(()).new_empty(
-        (batch_size, *input_shape),
-        dtype=next(flops_model.parameters()).dtype,
-        device=next(flops_model.parameters()).device)
-
-    batch['inputs'] = inputs
-    return batch
 
 
 def main():
@@ -79,37 +40,44 @@ def main():
     else:
         raise ValueError('invalid input shape')
 
-    model = init_model(
-        args.config,
-        checkpoint=None,
-        device=args.device,
-        cfg_options=args.cfg_options)
+    cfg = Config.fromfile(args.config)
+    model = build_pose_estimator(cfg.model)
+    model.eval()
 
-    if args.input_constructor == 'batch':
-        input_constructor = partial(batch_constructor, model, args.batch_size)
-    else:
-        input_constructor = None
-
-    if hasattr(model, '_forward'):
-        model.forward = model._forward
+    if hasattr(model, 'extract_feat'):
+        model.forward = model.extract_feat
     else:
         raise NotImplementedError(
             'FLOPs counter is currently not currently supported with {}'.
             format(model.__class__.__name__))
 
-    flops, params = get_model_complexity_info(
-        model,
-        input_shape,
-        input_constructor=input_constructor,
-        print_per_layer_stat=(not args.not_print_per_layer_stat))
+    inputs = (torch.randn((1, *input_shape)), )
+    flops_ = FlopCountAnalysis(model, inputs)
+    activations_ = ActivationCountAnalysis(model, inputs)
+
+    flops = _format_size(flops_.total())
+    activations = _format_size(activations_.total())
+    params = _format_size(parameter_count(model)[''])
+
+    flop_table = flop_count_table(
+        flops=flops_,
+        activations=activations_,
+        show_param_shapes=True,
+    )
+    flop_str = flop_count_str(flops=flops_, activations=activations_)
+
+    print('\n' + flop_str)
+    print('\n' + flop_table)
+
     split_line = '=' * 30
-    input_shape = (args.batch_size, ) + input_shape
     print(f'{split_line}\nInput shape: {input_shape}\n'
-          f'Flops: {flops}\nParams: {params}\n{split_line}')
+          f'Flops: {flops}\nParams: {params}\n'
+          f'Activation: {activations}\n{split_line}')
     print('!!!Please be cautious if you use the results in papers. '
           'You may need to check if all ops are supported and verify that the '
           'flops computation is correct.')
 
 
 if __name__ == '__main__':
+    register_all_modules()
     main()
