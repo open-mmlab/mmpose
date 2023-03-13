3b0e12d08572ec8a18fb6fce81e874c87d250f27
ly015 <liyining0712@gmail.com>
2023-03-13 11:37:31 +0800

    use mmengine

--- tools/analysis_tools/get_flops.py
+++ tools/analysis_tools/get_flops.py
@@ -1,83 +1,85 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
 
-import torch
+from mmengine.config import Config, DictAction
+
+from mmpose.registry import MODELS
+from mmpose.utils import register_all_modules
 
 try:
-    from fvcore.nn import (ActivationCountAnalysis, FlopCountAnalysis,
-                           flop_count_str, flop_count_table, parameter_count)
+    from mmengine.analysis import get_model_complexity_info
 except ImportError:
-    print('You may need to install fvcore for flops computation, '
-          'and you can use `pip install fvcore` to set up the environment')
-from fvcore.nn.print_model_statistics import _format_size
-from mmengine import Config
-
-from mmpose.models import build_pose_estimator
-from mmpose.utils import register_all_modules
+    raise ImportError('Please upgrade mmcv to >0.6.2')
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Get model flops and params')
-    parser.add_argument('config', help='config file path')
+    parser = argparse.ArgumentParser(description='Train a detector')
+    parser.add_argument('config', help='train config file path')
     parser.add_argument(
         '--shape',
         type=int,
         nargs='+',
-        default=[256, 192],
+        default=[1280, 800],
         help='input image size')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
     args = parser.parse_args()
     return args
 
 
 def main():
-
+    register_all_modules()
     args = parse_args()
 
     if len(args.shape) == 1:
-        input_shape = (3, args.shape[0], args.shape[0])
+        h = w = args.shape[0]
     elif len(args.shape) == 2:
-        input_shape = (3, ) + tuple(args.shape)
+        h, w = args.shape
     else:
         raise ValueError('invalid input shape')
+    input_shape = (3, h, w)
 
     cfg = Config.fromfile(args.config)
-    model = build_pose_estimator(cfg.model)
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
+
+    model = MODELS.build(cfg.model)
     model.eval()
 
-    if hasattr(model, 'extract_feat'):
-        model.forward = model.extract_feat
-    else:
-        raise NotImplementedError(
-            'FLOPs counter is currently not currently supported with {}'.
-            format(model.__class__.__name__))
+    analysis_results = get_model_complexity_info(
+        model, input_shape, show_table=True, show_arch=False)
 
-    inputs = (torch.randn((1, *input_shape)), )
-    flops_ = FlopCountAnalysis(model, inputs)
-    activations_ = ActivationCountAnalysis(model, inputs)
-
-    flops = _format_size(flops_.total())
-    activations = _format_size(activations_.total())
-    params = _format_size(parameter_count(model)[''])
-
-    flop_table = flop_count_table(
-        flops=flops_,
-        activations=activations_,
-        show_param_shapes=True,
-    )
-    flop_str = flop_count_str(flops=flops_, activations=activations_)
-
-    print('\n' + flop_str)
-    print('\n' + flop_table)
+    # ayalysis_results = {
+    #     'flops': flops,
+    #     'flops_str': flops_str,
+    #     'activations': activations,
+    #     'activations_str': activations_str,
+    #     'params': params,
+    #     'params_str': params_str,
+    #     'out_table': complexity_table,
+    #     'out_arch': complexity_arch
+    # }
 
     split_line = '=' * 30
     print(f'{split_line}\nInput shape: {input_shape}\n'
-          f'Flops: {flops}\nParams: {params}\n'
-          f'Activation: {activations}\n{split_line}')
+          f'Flops: {analysis_results["flops"]}\n'
+          f'Params: {analysis_results["params"]}\n{split_line}')
+
+    print(analysis_results['activations'])
+    # print(analysis_results['complexity_table'])
+    # print(complexity_str)
     print('!!!Please be cautious if you use the results in papers. '
           'You may need to check if all ops are supported and verify that the '
           'flops computation is correct.')
 
 
 if __name__ == '__main__':
-    register_all_modules()
     main()
