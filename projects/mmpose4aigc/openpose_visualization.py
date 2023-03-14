f21436005d85976f677445d0266ec1a32baa99b2
Ruid <374082733@qq.com>
2023-03-14 16:33:59 +0800

    openpose visualization

--- projects/mmpose4aigc/openpose_visualization.py
+++ projects/mmpose4aigc/openpose_visualization.py
@@ -19,7 +19,10 @@ try:
 except (ImportError, ModuleNotFoundError):
     has_mmdet = False
 
-import cv2,math
+import math
+
+import cv2
+
 
 def mmpose_to_openpose_visualization(args, img_path, detector, pose_estimator):
     """Visualize predicted keypoints of one image in openpose format."""
@@ -37,58 +40,71 @@ def mmpose_to_openpose_visualization(args, img_path, detector, pose_estimator):
     # predict keypoints
     pose_results = inference_topdown(pose_estimator, img_path, bboxes)
     data_samples = merge_data_samples(pose_results)
-    #concatenate scores and keypoints
-    keypoints = np.concatenate((data_samples.pred_instances.keypoints,data_samples.pred_instances.keypoint_scores.reshape(-1,17,1)),axis=-1)
-    #compute neck joint
-    neck = (keypoints[:,5] + keypoints[:,6])/2
-    neck[:,2] = keypoints[:,5,2]*keypoints[:,6,2]
-    #17 keypoints to 18 keypoints
-    new_keypoints = np.insert(keypoints[:,], 17, neck, axis=1)
-    #mmpose format to openpose format
-    new_keypoints[:,[15,14,17,16,2,6,3,7,4,8,12,9,13,10,1],:] = new_keypoints[:,[1,2,3,4,6,7,8,9,10,12,13,14,15,16,17],:]
-    #out file name .png
-    out_file = 'openpose_'+os.path.splitext(os.path.basename(img_path))[0]+'.png'
+    # concatenate scores and keypoints
+    keypoints = np.concatenate(
+        (data_samples.pred_instances.keypoints,
+         data_samples.pred_instances.keypoint_scores.reshape(-1, 17, 1)),
+        axis=-1)
+    # compute neck joint
+    neck = (keypoints[:, 5] + keypoints[:, 6]) / 2
+    neck[:, 2] = keypoints[:, 5, 2] * keypoints[:, 6, 2]
+    # 17 keypoints to 18 keypoints
+    new_keypoints = np.insert(keypoints[:, ], 17, neck, axis=1)
+    # mmpose format to openpose format
+    new_keypoints[:, [15, 14, 17, 16, 2, 6, 3, 7, 4, 8, 12, 9, 13, 10, 1
+                      ], :] = new_keypoints[:, [
+                          1, 2, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17
+                      ], :]
+    # out file name .png
+    out_file = 'openpose_' + os.path.splitext(
+        os.path.basename(img_path))[0] + '.png'
     # show the results
     img = mmcv.imread(img_path, channel_order='rgb')
     # black background
     black_img = np.zeros_like(img)
 
-    #openpose format
+    # openpose format
     stickwidth = 4
-    limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10], \
-               [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17], \
-               [1, 16], [16, 18]]
+    limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10],
+               [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15],
+               [15, 17], [1, 16], [16, 18]]
 
-    colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
-              [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
-              [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
-    
-    #draw keypoints
+    colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0],
+              [170, 255, 0], [85, 255, 0], [0, 255, 0], [0, 255, 85],
+              [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255],
+              [0, 0, 255], [85, 0, 255], [170, 0, 255], [255, 0, 255],
+              [255, 0, 170], [255, 0, 85]]
+
+    # draw keypoints
     cur_black_img = black_img.copy()
     for i in range(len(new_keypoints)):
         for j in range(18):
-            x,y,conf = new_keypoints[i][j]
+            x, y, conf = new_keypoints[i][j]
             if conf == 0:
                 continue
-            cv2.circle(cur_black_img, (int(x), int(y)), 4,colors[j] , thickness=-1) 
+            cv2.circle(
+                cur_black_img, (int(x), int(y)), 4, colors[j], thickness=-1)
     black_img = cv2.addWeighted(black_img, 0.4, cur_black_img, 0.6, 0)
 
-    #draw links
+    # draw links
     cur_black_img = black_img.copy()
     for i in range(len(new_keypoints)):
         for link in range(17):
-            
+
             Y = new_keypoints[i][np.array(limbSeq[link]) - 1, 0]
             X = new_keypoints[i][np.array(limbSeq[link]) - 1, 1]
             mX = np.mean(X)
             mY = np.mean(Y)
-            length = ((X[0] - X[1]) ** 2 + (Y[0] - Y[1]) ** 2) ** 0.5
+            length = ((X[0] - X[1])**2 + (Y[0] - Y[1])**2)**0.5
             angle = math.degrees(math.atan2(X[0] - X[1], Y[0] - Y[1]))
-            polygon = cv2.ellipse2Poly((int(mY), int(mX)), (int(length / 2), stickwidth), int(angle), 0, 360, 1)
+            polygon = cv2.ellipse2Poly(
+                (int(mY), int(mX)), (int(length / 2), stickwidth), int(angle),
+                0, 360, 1)
             cv2.fillConvexPoly(cur_black_img, polygon, colors[link])
     black_img = cv2.addWeighted(black_img, 0.4, cur_black_img, 0.6, 0)
-    #save image
-    cv2.imwrite(out_file,black_img[:,:,[2, 1, 0]])
+    # save image
+    cv2.imwrite(out_file, black_img[:, :, [2, 1, 0]])
+
 
 def main():
     """Visualize the demo images.
@@ -96,12 +112,27 @@ def main():
     Using mmdet to detect the human.
     """
     parser = ArgumentParser()
-    parser.add_argument('--det_config', help='Config file for detection',default='../rtmpose/rtmdet/person/rtmdet_nano_320-8xb32_coco-person.py')
-    parser.add_argument('--det_checkpoint', help='Checkpoint file for detection',default='models/rtmdet_nano_8xb32-100e_coco-obj365-person-05d8511e.pth')
-    parser.add_argument('--pose_config', help='Config file for pose',default='../rtmpose/rtmpose/body_2d_keypoint/rtmpose-m_8xb256-420e_coco-256x192.py')
-    parser.add_argument('--pose_checkpoint', help='Checkpoint file for pose',default='models/rtmpose-m_simcc-aic-coco_pt-aic-coco_420e-256x192-63eb25f7_20230126.pth')
     parser.add_argument(
-        'input', type=str, help='input Image file')
+        '--det_config',
+        help='Config file for detection',
+        default='../rtmpose/rtmdet/person/rtmdet_nano_320-8xb32_coco-person.py'
+    )
+    parser.add_argument(
+        '--det_checkpoint',
+        help='Checkpoint file for detection',
+        default='models/rtmdet_nano_8xb32-100e_coco-obj365-person-05d8511e.pth'
+    )
+    parser.add_argument(
+        '--pose_config',
+        help='Config file for pose',
+        default='../rtmpose/rtmpose/body_2d_keypoint/ \
+        rtmpose-m_8xb256-420e_coco-256x192.py')
+    parser.add_argument(
+        '--pose_checkpoint',
+        help='Checkpoint file for pose',
+        default='models/rtmpose-m_simcc-aic-coco_pt-aic- \
+        coco_420e-256x192-63eb25f7_20230126.pth')
+    parser.add_argument('input', type=str, help='input Image file')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
@@ -136,7 +167,7 @@ def main():
         type=int,
         default=1,
         help='Link thickness for visualization')
-    
+
     assert has_mmdet, 'Please install mmdet to run the demo.'
 
     args = parser.parse_args()
@@ -167,10 +198,9 @@ def main():
 
     input_type = mimetypes.guess_type(args.input)[0].split('/')[0]
     if input_type == 'image':
-        mmpose_to_openpose_visualization(
-            args,
-            args.input,
-            detector,
-            pose_estimator)
+        mmpose_to_openpose_visualization(args, args.input, detector,
+                                         pose_estimator)
+
+
 if __name__ == '__main__':
     main()
