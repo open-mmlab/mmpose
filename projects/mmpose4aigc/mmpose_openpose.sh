ca26ed2512c1d5ee1ac104b16c5a61a6ea02df45
Tau-J <674106399@qq.com>
2023-03-21 13:25:18 +0800

    refine codes

--- projects/mmpose4aigc/mmpose_openpose.sh
+++ projects/mmpose4aigc/mmpose_openpose.sh
@@ -4,9 +4,9 @@
 INPUT_IMAGE=$1
 
 python openpose_visualization.py \
-    --det_config ../rtmdet/person/rtmdet_nano_320-8xb32_coco-person.py \
-    --det_checkpoint models/rtmdet_nano_8xb32-100e_coco-obj365-person-05d8511e.pth \
-    --pose_config ../rtmpose/rtmpose/body_2d_keypoint/rtmpose-m_8xb256-420e_coco-256x192.py \
-    --pose_checkpoint models/rtmpose-m_simcc-aic-coco_pt-aic-coco_420e-256x192-63eb25f7_20230126.pth \
+    ../rtmpose/rtmdet/person/rtmdet_nano_320-8xb32_coco-person.py \
+    models/rtmdet_nano_8xb32-100e_coco-obj365-person-05d8511e.pth \
+    ../rtmpose/rtmpose/body_2d_keypoint/rtmpose-m_8xb256-420e_coco-256x192.py \
+    models/rtmpose-m_simcc-aic-coco_pt-aic-coco_420e-256x192-63eb25f7_20230126.pth \
     --input $INPUT_IMAGE \
-    --device cpu \
+    --device cuda:0 \
