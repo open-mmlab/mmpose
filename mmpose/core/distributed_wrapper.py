30608aed50a809012bb4d92cd47c4db9c18edb26
Fang Xiaolong <xiaoloongfang@gmail.com>
2023-03-24 10:42:54 +0800

    [NPU] add Ascend NPU device support for apis/train.py

--- mmpose/core/distributed_wrapper.py
+++ mmpose/core/distributed_wrapper.py
@@ -5,7 +5,10 @@ from mmcv.parallel import MODULE_WRAPPERS as MMCV_MODULE_WRAPPERS
 from mmcv.parallel import MMDistributedDataParallel
 from mmcv.parallel.scatter_gather import scatter_kwargs
 from mmcv.utils import Registry
+from mmcv.utils.device import get_device
 from torch.cuda._utils import _get_device_index
+
+from mmpose.utils.util_distribution import build_ddp
 
 MODULE_WRAPPERS = Registry('module wrapper', parent=MMCV_MODULE_WRAPPERS)
 
@@ -79,14 +82,18 @@ class DistributedDataParallelWrapper(nn.Module):
 
         It only wraps the modules with parameters.
         """
+        device = get_device()
+        assert device in ['cuda', 'npu'], \
+            'Only available for cuda or npu devices.'
         for name, module in self.module._modules.items():
             if next(module.parameters(), None) is None:
-                module = module.cuda()
+                module = getattr(module, device)()
             elif all(not p.requires_grad for p in module.parameters()):
-                module = module.cuda()
+                module = getattr(module, device)()
             else:
-                module = MMDistributedDataParallel(
-                    module.cuda(),
+                module = build_ddp(
+                    module,
+                    device=device,
                     device_ids=device_ids,
                     dim=dim,
                     broadcast_buffers=broadcast_buffers,
---
+++ mmpose/utils/util_distribution.py
@@ -0,0 +1,63 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
+
+dp_factory = {'cuda': MMDataParallel, 'cpu': MMDataParallel}
+
+ddp_factory = {'cuda': MMDistributedDataParallel}
+
+
+def build_dp(model, device='cuda', dim=0, *args, **kwargs):
+    """build DataParallel module by device type.
+
+    if device is cuda, return a MMDataParallel model; if device is npu,
+    return a NPUDataParallel model.
+
+    Args:
+        model (:class:`nn.Module`): model to be parallelized.
+        device (str): device type, cuda, cpu or npu. Defaults to cuda.
+        dim (int): Dimension used to scatter the data. Defaults to 0.
+
+    Returns:
+        nn.Module: the model to be parallelized.
+    """
+    if device == 'npu':
+        from mmcv.device.npu import NPUDataParallel
+        dp_factory['npu'] = NPUDataParallel
+        torch.npu.set_device(kwargs['device_ids'][0])
+        torch.npu.set_compile_mode(jit_compile=False)
+        model = model.npu()
+    elif device == 'cuda':
+        model = model.cuda(kwargs['device_ids'][0])
+
+    return dp_factory[device](model, dim=dim, *args, **kwargs)
+
+
+def build_ddp(model, device='cuda', *args, **kwargs):
+    """Build DistributedDataParallel module by device type.
+
+    If device is cuda, return a MMDistributedDataParallel model;
+    if device is npu, return a NPUDistributedDataParallel model.
+
+    Args:
+        model (:class:`nn.Module`): module to be parallelized.
+        device (str): device type, npu or cuda.
+
+    Returns:
+        :class:`nn.Module`: the module to be parallelized
+
+    References:
+        .. [1] https://pytorch.org/docs/stable/generated/torch.nn.parallel.
+                     DistributedDataParallel.html
+    """
+    assert device in ['cuda', 'npu'], \
+        'Only available for cuda or npu devices.'
+    if device == 'npu':
+        from mmcv.device.npu import NPUDistributedDataParallel
+        torch.npu.set_compile_mode(jit_compile=False)
+        ddp_factory['npu'] = NPUDistributedDataParallel
+        model = model.npu()
+    elif device == 'cuda':
+        model = model.cuda()
+
+    return ddp_factory[device](model, *args, **kwargs)
