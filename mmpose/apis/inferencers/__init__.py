5fbd0e1f155255c42119a21697e910995b42c80c
Peng Lu <penglu2097@gmail.com>
2023-03-31 22:18:09 +0800

    refine inferencer doc & support displaying alias (#2137)

--- mmpose/apis/inferencers/__init__.py
+++ mmpose/apis/inferencers/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .mmpose_inferencer import MMPoseInferencer
 from .pose2d_inferencer import Pose2DInferencer
+from .utils import get_model_aliases
 
-__all__ = ['Pose2DInferencer', 'MMPoseInferencer']
+__all__ = ['Pose2DInferencer', 'MMPoseInferencer', 'get_model_aliases']
