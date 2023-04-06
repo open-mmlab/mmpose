3aa5ef057f7aebd265310c75423c807507a4c908
Peng Lu <penglu2097@gmail.com>
2023-04-06 14:55:31 +0800

    [Refactor] add config checker (#2153)

--- mmpose/models/utils/__init__.py
+++ mmpose/models/utils/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .check_and_update_config import check_and_update_config
 from .ckpt_convert import pvt_convert
 from .rtmcc_block import RTMCCBlock, rope
 from .transformer import PatchEmbed, nchw_to_nlc, nlc_to_nchw
 
 __all__ = [
     'PatchEmbed', 'nchw_to_nlc', 'nlc_to_nchw', 'pvt_convert', 'RTMCCBlock',
-    'rope'
+    'rope', 'check_and_update_config'
 ]
