aec5e9b0e4e54eee8d10564faf8225b7c532f150
Xin Li <williamlee.xin@gmail.com>
2023-04-16 10:20:56 +0800

    fix lint

--- tools/analysis_tools/get_flops.py
+++ tools/analysis_tools/get_flops.py
@@ -1,120 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
-from functools import partial
-
-import torch
-from mmengine.config import DictAction
-
-from mmpose.apis.inference import init_model
-
-try:
-    from mmcv.cnn import get_model_complexity_info
-except ImportError:
-    raise ImportError('Please upgrade mmcv to >0.6.2')
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='Train a recognizer')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument(
-        '--device',
-        default='cuda:0',
-        help='Device used for model initialization')
-    parser.add_argument(
-        '--cfg-options',
-        nargs='+',
-        action=DictAction,
-        default={},
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. For example, '
-        "'--cfg-options model.backbone.depth=18 model.backbone.with_cp=True'")
-    parser.add_argument(
-        '--shape',
-        type=int,
-        nargs='+',
-        default=[256, 192],
-        help='input image size')
-    parser.add_argument(
-        '--input-constructor',
-        '-c',
-        type=str,
-        choices=['none', 'batch'],
-        default='none',
-        help='If specified, it takes a callable method that generates '
-        'input. Otherwise, it will generate a random tensor with '
-        'input shape to calculate FLOPs.')
-    parser.add_argument(
-        '--batch-size', '-b', type=int, default=1, help='input batch size')
-    parser.add_argument(
-        '--not-print-per-layer-stat',
-        '-n',
-        action='store_true',
-        help='Whether to print complexity information'
-        'for each layer in a model')
-    args = parser.parse_args()
-    return args
-
-
-def batch_constructor(flops_model, batch_size, input_shape):
-    """Generate a batch of tensors to the model."""
-    batch = {}
-
-    inputs = torch.ones(()).new_empty(
-        (batch_size, *input_shape),
-        dtype=next(flops_model.parameters()).dtype,
-        device=next(flops_model.parameters()).device)
-
-    batch['inputs'] = inputs
-    return batch
-
-
-def main():
-
-    args = parse_args()
-
-    if len(args.shape) == 1:
-        input_shape = (3, args.shape[0], args.shape[0])
-    elif len(args.shape) == 2:
-        input_shape = (3, ) + tuple(args.shape)
-    else:
-        raise ValueError('invalid input shape')
-
-    model = init_model(
-        args.config,
-        checkpoint=None,
-        device=args.device,
-        cfg_options=args.cfg_options)
-
-    if args.input_constructor == 'batch':
-        input_constructor = partial(batch_constructor, model, args.batch_size)
-    else:
-        input_constructor = None
-
-    if hasattr(model, '_forward'):
-        model.forward = model._forward
-    else:
-        raise NotImplementedError(
-            'FLOPs counter is currently not currently supported with {}'.
-            format(model.__class__.__name__))
-
-    flops, params = get_model_complexity_info(
-        model,
-        input_shape,
-        input_constructor=input_constructor,
-        print_per_layer_stat=(not args.not_print_per_layer_stat))
-    split_line = '=' * 30
-    input_shape = (args.batch_size, ) + input_shape
-    print(f'{split_line}\nInput shape: {input_shape}\n'
-          f'Flops: {flops}\nParams: {params}\n{split_line}')
-    print('!!!Please be cautious if you use the results in papers. '
-          'You may need to check if all ops are supported and verify that the '
-          'flops computation is correct.')
-
-
-if __name__ == '__main__':
-    main()
-# Copyright (c) OpenMMLab. All rights reserved.
-import argparse
 import tempfile
 from functools import partial
 from pathlib import Path
@@ -156,7 +41,8 @@ def parse_args():
         default=[256, 192],
         help='input image size')
     parser.add_argument(
-        '--input-constructor', '-c',
+        '--input-constructor',
+        '-c',
         type=str,
         choices=['none', 'batch'],
         default='none',
@@ -166,7 +52,8 @@ def parse_args():
     parser.add_argument(
         '--batch-size', '-b', type=int, default=1, help='input batch size')
     parser.add_argument(
-        '--not-show-complexity-table', '-n',
+        '--not-show-complexity-table',
+        '-n',
         action='store_true',
         help='Whether to show complexity information')
     args = parser.parse_args()
@@ -196,9 +83,10 @@ def main():
         input_shape = (3, ) + tuple(args.shape)
     else:
         raise ValueError('invalid input shape')
-    
+
     if args.device == 'cuda:0':
-        assert torch.cuda.is_available(), 'No valid cuda detected, please double check...'
+        assert torch.cuda.is_available(
+        ), 'No valid cuda detected, please double check...'
 
     model = init_model(
         args.config,
@@ -217,7 +105,7 @@ def main():
         raise NotImplementedError(
             'FLOPs counter is currently not currently supported with {}'.
             format(model.__class__.__name__))
-    
+
     config_name = Path(args.config)
     if not config_name.exists():
         logger.error(f'{config_name} not found.')
@@ -230,7 +118,7 @@ def main():
         cfg.merge_from_dict(args.cfg_options)
 
     init_default_scope(cfg.get('default_scope', 'mmpose'))
-    
+
     outputs = get_model_complexity_info(
         model,
         input_shape,
