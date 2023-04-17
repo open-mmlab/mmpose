d584ede86cffaa6e1f8ee0a0fa5a76cc534c556f
xin-li-67 <williamlee.xin@gmail.com>
2023-04-17 17:30:27 +0800

    change main->dev-1.x in both merge_docs.sh

--- docs/en/merge_docs.sh
+++ docs/en/merge_docs.sh
@@ -2,7 +2,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
 sed -i '$a\\n' ../../demo/docs/en/*_demo.md
-cat ../../demo/docs/en/*_demo.md | sed "s/^## 2D\(.*\)Demo/##\1Estimation/" | sed "s/md###t/html#t/g" | sed '1i\# Demos\n' | sed 's=](/docs/en/=](/=g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' >demos.md
+cat ../../demo/docs/en/*_demo.md | sed "s/^## 2D\(.*\)Demo/##\1Estimation/" | sed "s/md###t/html#t/g" | sed '1i\# Demos\n' | sed 's=](/docs/en/=](/=g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' >demos.md
 
  # remove /docs/ for link used in doc site
 sed -i 's=](/docs/en/=](=g' overview.md
@@ -18,14 +18,14 @@ sed -i 's=](/docs/en/=](=g' ./notes/*.md
 sed -i 's=](/docs/en/=](=g' ./projects/*.md
 
 
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' overview.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' installation.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' quick_run.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' migration.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' ./advanced_guides/*.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' ./model_zoo/*.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' ./model_zoo_papers/*.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' ./user_guides/*.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' ./dataset_zoo/*.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' ./notes/*.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' ./projects/*.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' overview.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' installation.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' quick_run.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' migration.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' ./advanced_guides/*.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' ./model_zoo/*.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' ./model_zoo_papers/*.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' ./user_guides/*.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' ./dataset_zoo/*.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' ./notes/*.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' ./projects/*.md
--- docs/zh_cn/merge_docs.sh
+++ docs/zh_cn/merge_docs.sh
@@ -2,7 +2,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
 sed -i '$a\\n' ../../demo/docs/zh_cn/*_demo.md
-cat ../../demo/docs/zh_cn/*_demo.md | sed "s/^## 2D\(.*\)Demo/##\1Estimation/" | sed "s/md###t/html#t/g" | sed '1i\# Demos\n' | sed 's=](/docs/en/=](/=g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/main/=g' >demos.md
+cat ../../demo/docs/zh_cn/*_demo.md | sed "s/^## 2D\(.*\)Demo/##\1Estimation/" | sed "s/md###t/html#t/g" | sed '1i\# Demos\n' | sed 's=](/docs/en/=](/=g' | sed 's=](/=](https://github.com/open-mmlab/mmpose/tree/dev-1.x/=g' >demos.md
 
  # remove /docs/ for link used in doc site
 sed -i 's=](/docs/zh_cn/=](=g' overview.md
