ec93cd452c980cad85fe68f14bf6ae0d9dd5c396
Tau-J <674106399@qq.com>
2023-04-20 13:29:24 +0800

    update published model links

--- projects/mmpose4aigc/download_models.sh
+++ projects/mmpose4aigc/download_models.sh
@@ -11,7 +11,7 @@ cd models
 wget https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_nano_8xb32-100e_coco-obj365-person-05d8511e.pth
 
 # Download pose model
-wget https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmpose-m_simcc-aic-coco_pt-aic-coco_420e-256x192-63eb25f7_20230126.pth
+wget https://download.openmmlab.com/mmpose/v1/projects/rtmposev1/rtmpose-m_simcc-aic-coco_pt-aic-coco_420e-256x192-63eb25f7_20230126.pth
 
 # Go back mmpose4aigc
 cd ..
