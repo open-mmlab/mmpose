628b304fd64d797c9a8aae1ef4a36224c02284ec
Xin Li <7219519+xin-li-67@users.noreply.github.com>
2023-04-21 20:25:02 +0800

    Update configs/face_2d_keypoint/topdown_regression/wflw/td-reg_res50_8x64e-210e_wflw-256x256.py
    Co-authored-by: Tau <taujiang@outlook.com>

--- configs/face_2d_keypoint/topdown_regression/wflw/td-reg_res50_8x64e-210e_wflw-256x256.py
+++ configs/face_2d_keypoint/topdown_regression/wflw/td-reg_res50_8x64e-210e_wflw-256x256.py
@@ -112,7 +112,7 @@ val_dataloader = dict(
 test_dataloader = val_dataloader
 
 # hooks
-default_hooks = dict(checkpoint=dict(save_best='NME', rule='greater'))
+default_hooks = dict(checkpoint=dict(save_best='NME', rule='less'))
 
 # evaluators
 val_evaluator = dict(
