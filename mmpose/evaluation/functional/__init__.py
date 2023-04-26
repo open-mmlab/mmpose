d2bedef5fd9e5730aaf5af6a0607ba58ee429084
Peng Lu <penglu2097@gmail.com>
2023-04-26 14:00:09 +0800

    [Refactor] add mpjpe metric (#2247)

--- mmpose/evaluation/functional/__init__.py
+++ mmpose/evaluation/functional/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .keypoint_eval import (keypoint_auc, keypoint_epe, keypoint_nme,
-                            keypoint_pck_accuracy,
+from .keypoint_eval import (keypoint_auc, keypoint_epe, keypoint_mpjpe,
+                            keypoint_nme, keypoint_pck_accuracy,
                             multilabel_classification_accuracy,
                             pose_pck_accuracy, simcc_pck_accuracy)
 from .nms import nms, oks_nms, soft_oks_nms
@@ -8,5 +8,5 @@ from .nms import nms, oks_nms, soft_oks_nms
 __all__ = [
     'keypoint_pck_accuracy', 'keypoint_auc', 'keypoint_nme', 'keypoint_epe',
     'pose_pck_accuracy', 'multilabel_classification_accuracy',
-    'simcc_pck_accuracy', 'nms', 'oks_nms', 'soft_oks_nms'
+    'simcc_pck_accuracy', 'nms', 'oks_nms', 'soft_oks_nms', 'keypoint_mpjpe'
 ]
