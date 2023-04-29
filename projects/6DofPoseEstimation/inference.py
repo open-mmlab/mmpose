ca77b186187d54087d7066ace21368cb2e1934ad
liuyoufu <liuyiaddress@126.com>
2023-04-30 01:00:04 +0800

    add inference

--- projects/6DofPoseEstimation/inference.py
+++ projects/6DofPoseEstimation/inference.py
@@ -17,10 +17,8 @@ from mmpose.evaluation.functional import nms
 from mmdet.apis import inference_detector, init_detector
 from mmengine.visualization import Visualizer
 
-import time
 
-
-def predict(image_path, start_time):
+def predict(image_path):
     
     Visualizer.get_instance('visualization_hook')
     
@@ -42,11 +40,9 @@ def predict(image_path, start_time):
         cfg_options={'model': {'test_cfg': {'output_heatmaps': True}}})
     
     init_default_scope(detector.cfg.get('default_scope', 'mmdet'))
-    
-    start_time = time.time()
-        
+            
     detect_result = inference_detector(detector, image_path)
-    CONF_THRES = 0.004
+    CONF_THRES = 0.5
 
     pred_instance = detect_result.pred_instances.cpu().numpy()
     bboxes = np.concatenate((pred_instance.bboxes, pred_instance.scores[:, None]), axis=1)
@@ -57,12 +53,13 @@ def predict(image_path, start_time):
     data_samples = merge_data_samples(pose_results)
     keypoints = data_samples.pred_instances.keypoints.astype('int')
     
-    return keypoints, start_time
+    return keypoints
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='demo')
     parser.add_argument('--image-path', help='image path')
+    parser.add_argument('--root-path', type=str, help='root path')
     parser.add_argument('--id', type=str, help='object id, for example: 01')
     args = parser.parse_args()
     return args
@@ -74,25 +71,22 @@ def main():
     obj_id = args.id
     
     # 根据图像文件路径，划分物体id 根目录 与 图像名称
-    file_name = os.path.basename(image_path)
-    root_path = '/'.join(image_path.split('/')[:-4]) + '/'
+    root_path = args.root_path
     
     # get model_info_dict, obj_id
     model_info_path = root_path + 'models/'
     model_info_dict = mmengine.load(model_info_path + 'models_info.yml')
     object_path = os.path.join(root_path, f'data/{args.id}/')
     info_dict = mmengine.load(object_path + 'info.yml')
-    gt_dict = mmengine.load(object_path + 'gt.yml')
     
     # 根据图像名，获取对应图像的内参
-    intrinsic = np.array(info_dict[int(file_name.split(".")[0])]['cam_K']).reshape(3,3)
+    intrinsic = np.array(info_dict[0]['cam_K']).reshape(3,3)
     
     # get corner3D (8*3)
     corners3D = get_3D_corners(model_info_dict, obj_id)
     
-    # get gt and prediction
-    start_time = 0
-    keypoint_pr, start_time = predict(image_path, start_time)
+    # get prediction
+    keypoint_pr = predict(image_path)
     corners2D_pr = keypoint_pr.reshape(-1,2)
     
     # Compute [R|t] by pnp  =====  pred
@@ -102,11 +96,6 @@ def main():
     Rt_pr = np.concatenate((R_pr, t_pr), axis=1)
     proj_corners_pr = np.transpose(compute_projection(corners3D, Rt_pr, intrinsic)) 
     
-    # Compute [R|t] by pnp  =====  gt
-    R_gt = np.array(gt_dict[int(file_name.split(".")[0])][0]['cam_R_m2c']).reshape(3,3)
-    t_gt = np.array(gt_dict[int(file_name.split(".")[0])][0]['cam_t_m2c']).reshape(3,1)
-    Rt_gt = np.concatenate((R_gt, t_gt), axis=1)
-    proj_corners_gt = np.transpose(compute_projection(corners3D, Rt_gt, intrinsic)) 
     
     image = mmcv.imread(image_path)
     height = image.shape[0]
@@ -126,7 +115,6 @@ def main():
                      [2, 6], [3, 7], [4, 5], [4, 6], [5, 7], [6, 7]]
     for edge in edges_corners:
         plt.plot(proj_corners_pr[edge, 0], proj_corners_pr[edge, 1], color='g', linewidth=2.0)
-        plt.plot(proj_corners_gt[edge, 0], proj_corners_gt[edge, 1], color='g', linewidth=2.0)
     plt.gca().invert_yaxis()
     plt.show()
     plt.pause(0)
