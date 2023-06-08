2dc0f2e0ee6c8ff3756af5e52dbbaae8965f3545
Tau-J <674106399@qq.com>
2023-06-08 17:26:30 +0800

    fix tar.gz.00

--- tools/dataset_converters/scripts/preprocess_300w.sh
+++ tools/dataset_converters/scripts/preprocess_300w.sh
@@ -3,5 +3,6 @@
 DOWNLOAD_DIR=$1
 DATA_ROOT=$2
 
-tar -zxvf $DOWNLOAD_DIR/300w/raw/300w.tar.gz.00 -C $DATA_ROOT
-rm -rf $DATA_ROOT/300w
+tar -zxvf $DOWNLOAD_DIR/300w/raw/300w.tar.gz.00 -C $DOWNLOAD_DIR/
+tar -xvf $DOWNLOAD_DIR/300w/300w.tar.00 -C $DATA_ROOT/
+rm -rf $DOWNLOAD_DIR/300w
--- tools/dataset_converters/scripts/preprocess_onehand10k.sh
+++ tools/dataset_converters/scripts/preprocess_onehand10k.sh
@@ -3,5 +3,6 @@
 DOWNLOAD_DIR=$1
 DATA_ROOT=$2
 
-tar -zxvf $DOWNLOAD_DIR/OneHand10K/raw/OneHand10K.tar.gz.00 -C $DATA_ROOT
-rm -rf $DATA_ROOT/OneHand10K
+tar -zxvf $DOWNLOAD_DIR/OneHand10K/raw/OneHand10K.tar.gz.00 -C $DOWNLOAD_DIR/
+tar -xvf $DOWNLOAD_DIR/OneHand10K/OneHand10K.tar.00 -C $DATA_ROOT/
+rm -rf $DOWNLOAD_DIR/OneHand10K
