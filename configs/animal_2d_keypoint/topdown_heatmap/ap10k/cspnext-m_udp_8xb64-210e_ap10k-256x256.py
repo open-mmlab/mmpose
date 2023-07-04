52e4925724443f9792ceecd09276d727dcbad645
Peng Lu <penglu2097@gmail.com>
2023-07-04 16:28:34 +0800

    [Fix] fix ckpt urls & test settings (#2510)

--- configs/animal_2d_keypoint/topdown_heatmap/ap10k/cspnext-m_udp_8xb64-210e_ap10k-256x256.py
+++ configs/animal_2d_keypoint/topdown_heatmap/ap10k/cspnext-m_udp_8xb64-210e_ap10k-256x256.py
@@ -73,7 +73,7 @@ model = dict(
         loss=dict(type='KeypointMSELoss', use_target_weight=True),
         decoder=codec),
     test_cfg=dict(
-        flip_test=False,
+        flip_test=True,
         flip_mode='heatmap',
         shift_heatmap=False,
     ))
