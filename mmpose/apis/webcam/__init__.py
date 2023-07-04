e690291f0117caf0901696955216265bbb74efed
lupeng <penglu2097@gmail.com>
2023-07-04 16:01:31 +0800

    remove webcam api

--- mmpose/apis/webcam/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .webcam_executor import WebcamExecutor
-
-__all__ = ['WebcamExecutor']
--- mmpose/apis/webcam/nodes/model_nodes/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .detector_node import DetectorNode
-from .pose_estimator_node import TopdownPoseEstimatorNode
-
-__all__ = ['DetectorNode', 'TopdownPoseEstimatorNode']
