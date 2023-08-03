371a01fa6bca8ce4741c19014e1cf205e81073a5
Indigo6 <1401322857@qq.com>
2023-08-03 16:46:36 +0800

    fix rename bug

--- mmpose/engine/hooks/__init__.py
+++ mmpose/engine/hooks/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .badcase_hook import BadCaseAnalysisHook
 from .ema_hook import ExpMomentumEMA
 from .visualization_hook import PoseVisualizationHook
-from .badcase_hook import BadCaseAnalyzeHook
 
-__all__ = ['PoseVisualizationHook', 'ExpMomentumEMA', 'BadCaseAnalyzeHook']
+__all__ = ['PoseVisualizationHook', 'ExpMomentumEMA', 'BadCaseAnalysisHook']
