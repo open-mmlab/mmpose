ac462c7dd072e307258fc8dcb1d6a15eae775222
Indigo6 <1401322857@qq.com>
2023-08-03 15:51:02 +0800

    set and sort default cfg of badcase

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -10,10 +10,10 @@ default_hooks = dict(
     visualization=dict(type='PoseVisualizationHook', enable=False),
     badcase=dict(
         type='BadCaseAnalysisHook',
+        enable=False,
+        out_dir='badcase',
         metric_type='loss',
-        badcase_thr=100,
-        show=True,
-        out_dir='badcase'))
+        badcase_thr=5))
 
 # custom hooks
 custom_hooks = [
