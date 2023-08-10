cb5e8df95a63513a1584f3664cc75459e8e0d1bc
lupeng <penglu2097@gmail.com>
2023-08-10 18:31:20 +0800

    update dataset processing scripts

--- tools/dataset_converters/scripts/preprocess_lapa.sh
+++ tools/dataset_converters/scripts/preprocess_lapa.sh
@@ -3,5 +3,5 @@
 DOWNLOAD_DIR=$1
 DATA_ROOT=$2
 
-tar -zxvf $DOWNLOAD_DIR/LaPa/raw/LaPa.tar.gz -C $DATA_ROOT
-rm -rf $DOWNLOAD_DIR/LaPa
+tar -zxvf $DOWNLOAD_DIR/OpenDataLab___LaPa/raw/LaPa.tar.gz -C $DATA_ROOT
+rm -rf $DOWNLOAD_DIR/OpenDataLab___LaPa
--- tools/dataset_converters/scripts/preprocess_onehand10k.sh
+++ tools/dataset_converters/scripts/preprocess_onehand10k.sh
@@ -3,6 +3,6 @@
 DOWNLOAD_DIR=$1
 DATA_ROOT=$2
 
-tar -zxvf $DOWNLOAD_DIR/OneHand10K/raw/OneHand10K.tar.gz.00 -C $DOWNLOAD_DIR/
+tar -zxvf $DOWNLOAD_DIR/OpenDataLab___OneHand10K/raw/OneHand10K.tar.gz.00 -C $DOWNLOAD_DIR/
 tar -xvf $DOWNLOAD_DIR/OneHand10K/OneHand10K.tar.00 -C $DATA_ROOT/
-rm -rf $DOWNLOAD_DIR/OneHand10K
+rm -rf $DOWNLOAD_DIR/OneHand10K $DOWNLOAD_DIR/OpenDataLab___OneHand10K
