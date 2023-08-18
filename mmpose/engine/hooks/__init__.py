836ffd09b2cc04e801919efff69eb34d16c33aab
lupeng <penglu2097@gmail.com>
2023-08-18 16:15:51 +0800

    add engine

--- mmpose/engine/hooks/__init__.py
+++ mmpose/engine/hooks/__init__.py
@@ -1,6 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .badcase_hook import BadCaseAnalysisHook
 from .ema_hook import ExpMomentumEMA
+from .mode_switch_hooks import YOLOPoseModeSwitchHook
+from .sync_norm_hook import SyncNormHook
 from .visualization_hook import PoseVisualizationHook
 
-__all__ = ['PoseVisualizationHook', 'ExpMomentumEMA', 'BadCaseAnalysisHook']
+__all__ = [
+    'PoseVisualizationHook', 'ExpMomentumEMA', 'BadCaseAnalysisHook',
+    'YOLOPoseModeSwitchHook', 'SyncNormHook'
+]
