8bb1b8f6b74805caa3465e8af39add258dbdd48d
xiexinch <xiexinch@outlook.com>
2023-08-22 15:42:41 +0800

    --other=add dataset to registry

---
+++ configs/_base_/datasets/ubody3d.py
@@ -0,0 +1,12 @@
+dataset_info = dict(
+    dataset_name='ubody3d',
+    paper_info=dict(
+        author='Jing Lin, Ailing Zeng, Haoqian Wang, Lei Zhang, Yu Li',
+        title='One-Stage 3D Whole-Body Mesh Recovery with Component Aware'
+        'Transformer',
+        container='IEEE Computer Society Conference on Computer Vision and '
+        'Pattern Recognition (CVPR)',
+        year='2023',
+        homepage='https://github.com/IDEA-Research/OSX',
+    ),
+    keypoint_info={})
--- mmpose/datasets/datasets/body3d/__init__.py
+++ mmpose/datasets/datasets/body3d/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .h36m_dataset import Human36mDataset
+from .ubody3d_dataset import UBody3dDataset
 
-__all__ = ['Human36mDataset']
+__all__ = ['Human36mDataset', 'UBody3dDataset']
