c8261d7e4063664c3771b790713048d7c5a50fbd
lupeng <penglu2097@gmail.com>
2023-08-29 11:44:39 +0800

    add readme and yaml for yoloxpose models

--- configs/body_2d_keypoint/yoloxpose/coco/yoloxpose_m_8xb32-300e_coco-640.py
+++ configs/body_2d_keypoint/yoloxpose/coco/yoloxpose_m_8xb32-300e_coco-640.py
@@ -1,9 +1,9 @@
 _base_ = './yolopose_s_8xb32-300e_coco-640.py'
 
-widen_factor = 1
-deepen_factor = 1
-checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox' \
-    '_x_8x8_300e_coco/yolox_x_8x8_300e_coco_20211126_140254-1ef88d67.pth'
+widen_factor = 0.75
+deepen_factor = 0.67
+checkpoint = 'https://download.openmmlab.com/mmpose/v1/pretrained_models/' \
+             'yolox_m_8x8_300e_coco_20230829.pth'
 
 # model settings
 model = dict(
@@ -12,6 +12,5 @@ model = dict(
         widen_factor=widen_factor,
         init_cfg=dict(checkpoint=checkpoint),
     ),
-    neck=dict(
-        in_channels=[256, 512, 1024], out_channels=256, num_csp_blocks=3),
+    neck=dict(in_channels=[192, 384, 768], out_channels=192, num_csp_blocks=2),
     head=dict(head_module_cfg=dict(widen_factor=widen_factor)))
--- configs/body_2d_keypoint/yoloxpose/coco/yoloxpose_tiny_4xb64-300e_coco-416.py
+++ configs/body_2d_keypoint/yoloxpose/coco/yoloxpose_tiny_4xb64-300e_coco-416.py
@@ -1,16 +1,16 @@
 _base_ = './yolopose_s_8xb32-300e_coco-640.py'
 
+# model settings
 widen_factor = 0.375
 deepen_factor = 0.33
 checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_' \
     'tiny_8x8_300e_coco/yolox_tiny_8x8_300e_coco_20211124_171234-b4047906.pth'
 
-# model settings
 model = dict(
     data_preprocessor=dict(batch_augments=[
         dict(
             type='BatchSyncRandomResize',
-            random_size_range=(320, 512),
+            random_size_range=(320, 640),
             size_divisor=32,
             interval=1),
     ]),
@@ -19,24 +19,23 @@ model = dict(
         widen_factor=widen_factor,
         init_cfg=dict(checkpoint=checkpoint),
     ),
-    neck=dict(in_channels=[96, 192, 384], out_channels=96),
-    head=dict(head_module_cfg=dict(widen_factor=widen_factor)))
+    neck=dict(
+        in_channels=[96, 192, 384],
+        out_channels=96,
+    ),
+    head=dict(head_module_cfg=dict(widen_factor=widen_factor), ))
 
 # dataset settings
-input_size = (416, 416)
-
-codec = dict(type='YOLOXPoseAnnotationProcessor', input_size=input_size)
-
 train_pipeline_stage1 = [
     dict(type='LoadImage', backend_args=None),
     dict(
         type='Mosaic',
-        img_scale=input_size,
+        img_scale=_base_.input_size,
         pad_val=114.0,
         pre_transform=[dict(type='LoadImage', backend_args=None)]),
     dict(
         type='BottomupRandomAffine',
-        input_size=input_size,
+        input_size=_base_.input_size,
         shift_factor=0.1,
         rotate_factor=10,
         scale_factor=(0.75, 1.0),
@@ -49,7 +48,7 @@ train_pipeline_stage1 = [
     dict(type='YOLOXHSVRandomAug'),
     dict(type='RandomFlip'),
     dict(type='FilterAnnotations', by_kpt=True, by_box=True, keep_empty=False),
-    dict(type='GenerateTarget', encoder=codec),
+    dict(type='GenerateTarget', encoder=_base_.codec),
     dict(
         type='PackPoseInputs',
         extra_mapping_labels={
@@ -60,36 +59,10 @@ train_pipeline_stage1 = [
             'area': 'areas'
         }),
 ]
-train_pipeline_stage2 = [
-    dict(type='LoadImage'),
-    dict(
-        type='BottomupRandomAffine',
-        input_size=input_size,
-        shift_prob=0,
-        rotate_prob=0,
-        scale_prob=0,
-        scale_type='long',
-        pad_val=(114, 114, 114),
-        bbox_keep_corner=False,
-        clip_border=True,
-    ),
-    dict(type='YOLOXHSVRandomAug'),
-    dict(type='RandomFlip'),
-    dict(type='FilterAnnotations', by_kpt=True, by_box=True, keep_empty=False),
-    dict(type='GenerateTarget', encoder=codec),
-    dict(
-        type='PackPoseInputs',
-        extra_mapping_labels={
-            'bbox': 'bboxes',
-            'bbox_labels': 'labels',
-            'keypoints': 'keypoints',
-            'keypoints_visible': 'keypoints_visible',
-            'area': 'areas'
-        }),
-]
+train_dataloader = dict(
+    batch_size=64, dataset=dict(pipeline=train_pipeline_stage1))
 
-train_dataloader = dict(dataset=dict(pipeline=train_pipeline_stage1))
-
+input_size = (416, 416)
 val_pipeline = [
     dict(type='LoadImage'),
     dict(
@@ -102,19 +75,3 @@ val_pipeline = [
 
 val_dataloader = dict(dataset=dict(pipeline=val_pipeline, ))
 test_dataloader = val_dataloader
-
-custom_hooks = [
-    dict(
-        type='YOLOXPoseModeSwitchHook',
-        num_last_epochs=20,
-        new_train_pipeline=train_pipeline_stage2,
-        priority=48),
-    dict(type='SyncNormHook', priority=48),
-    dict(
-        type='EMAHook',
-        ema_type='ExpMomentumEMA',
-        momentum=0.0002,
-        update_buffers=True,
-        strict_load=False,
-        priority=49),
-]
