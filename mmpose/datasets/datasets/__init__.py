1ab2c427b12bb97a84bc0b9b25209b1fde27fadb
谢昕辰 <xiexinch@outlook.com>
2023-09-20 21:15:58 +0800

    [Fix] Move ubody3d dataset to wholebody3d (#2712)

--- mmpose/datasets/datasets/__init__.py
+++ mmpose/datasets/datasets/__init__.py
@@ -7,3 +7,4 @@ from .face import *  # noqa: F401, F403
 from .fashion import *  # noqa: F401, F403
 from .hand import *  # noqa: F401, F403
 from .wholebody import *  # noqa: F401, F403
+from .wholebody3d import *  # noqa: F401, F403
--- mmpose/datasets/datasets/body3d/__init__.py
+++ mmpose/datasets/datasets/body3d/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .h36m_dataset import Human36mDataset
-from .ubody3d_dataset import UBody3dDataset
 
-__all__ = ['Human36mDataset', 'UBody3dDataset']
+__all__ = ['Human36mDataset']
