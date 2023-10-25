5f9d8f0fa6b7697602ea8a4ae25d4e1fea418c0c
Yang-Changhui <2275029710@qq.com>
2023-10-25 19:58:47 +0800

    support_exlpose

--- configs/_base_/datasets/exlpose.py
+++ configs/_base_/datasets/exlpose.py
@@ -1,7 +1,8 @@
 dataset_info = dict(
     dataset_name='exlpose',
     paper_info=dict(
-        author='Sohyun Lee, Jaesung Rim, Boseung Jeong, Geonu Kim, ByungJu Woo, Haechan Lee, Sunghyun Cho, Suha Kwak',
+        author='Sohyun Lee, Jaesung Rim, Boseung Jeong, Geonu Kim,'
+               'ByungJu Woo, Haechan Lee, Sunghyun Cho, Suha Kwak',
         title='Human Pose Estimation in Extremely Low-Light Conditions',
         container='arXiv',
         year='2023',
@@ -108,14 +109,18 @@ dataset_info = dict(
         1: dict(
             link=('neck', 'left_shoulder'), id=1, color=[51, 153, 255]),
         2: dict(link=('neck', 'right_shoulder'), id=2, color=[51, 153, 255]),
-        3: dict(link=('left_shoulder', 'left_elbow'), id=3, color=[0, 255, 0]),
+        3: dict(link=('left_shoulder', 'left_elbow'), id=3,
+                color=[0, 255, 0]),
         4: dict(link=('left_elbow', 'left_wrist'), id=4, color=[0, 255, 0]),
-        5: dict(link=('right_shoulder', 'right_elbow'), id=5, color=[255, 128, 0]),
-        6: dict(link=('right_elbow', 'right_wrist'), id=6, color=[255, 128, 0]),
+        5: dict(link=('right_shoulder', 'right_elbow'), id=5,
+                color=[255, 128, 0]),
+        6: dict(link=('right_elbow', 'right_wrist'), id=6,
+                color=[255, 128, 0]),
         7: dict(link=('neck', 'right_hip'), id=7, color=[51, 153, 255]),
         8: dict(link=('neck', 'left_hip'), id=8, color=[51, 153, 255]),
         9: dict(link=('right_hip', 'right_knee'), id=9, color=[255, 128, 0]),
-        10: dict(link=('right_knee', 'right_ankle'), id=10, color=[255, 128, 0]),
+        10: dict(link=('right_knee', 'right_ankle'), id=10,
+                 color=[255, 128, 0]),
         11: dict(link=('left_hip', 'left_knee'), id=11, color=[0, 255, 0]),
         12: dict(link=('left_knee', 'left_ankle'), id=12, color=[0, 255, 0]),
     },
