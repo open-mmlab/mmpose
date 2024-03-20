fix loss computation in mspn head

mmpose/models/heads/heatmap_heads/mspn_head.py
@@ -394,7 +394,9 @@ class MSPNHead(BaseHead):
 
         keypoint_weights = torch.cat([
             d.gt_instance_labels.keypoint_weights for d in batch_data_samples
-        ])  # shape: [B*N, L, K]
+        ],
+                                     dim=1)
+        keypoint_weights = keypoint_weights.transpose(0, 1)  # [B*N, L, K]
 
         # calculate losses over multiple stages and multiple units
         losses = dict()