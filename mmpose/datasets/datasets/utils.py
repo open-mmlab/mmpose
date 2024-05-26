e72f06a1800f668d040f0824bfd3912f9c23228e
Raphael Attali <raphael@shopperai.ai>
2024-05-26 15:59:03 +0300

    added mmpose repo path

--- mmpose/datasets/datasets/utils.py
+++ mmpose/datasets/datasets/utils.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import os
 import os.path as osp
 import warnings
 
@@ -89,35 +90,46 @@ def parse_pose_metainfo(metainfo: dict):
         - "sigmas" (numpy.ndarray): Same as the ``"sigmas"`` in the input
     """
 
-    if 'from_file' in metainfo:
-        cfg_file = metainfo['from_file']
+    if "from_file" in metainfo:
+        cfg_file = metainfo["from_file"]
+        cfg_file = os.path.join("mmpose_repo", cfg_file)
+
         if not osp.isfile(cfg_file):
             # Search configs in 'mmpose/.mim/configs/' in case that mmpose
             # is installed in non-editable mode.
             import mmpose
+
             mmpose_path = osp.dirname(mmpose.__file__)
-            _cfg_file = osp.join(mmpose_path, '.mim', 'configs', '_base_',
-                                 'datasets', osp.basename(cfg_file))
+            _cfg_file = osp.join(
+                mmpose_path,
+                ".mim",
+                "configs",
+                "_base_",
+                "datasets",
+                osp.basename(cfg_file),
+            )
             if osp.isfile(_cfg_file):
                 warnings.warn(
                     f'The metainfo config file "{cfg_file}" does not exist. '
                     f'A matched config file "{_cfg_file}" will be used '
-                    'instead.')
+                    "instead."
+                )
                 cfg_file = _cfg_file
             else:
                 raise FileNotFoundError(
-                    f'The metainfo config file "{cfg_file}" does not exist.')
+                    f'The metainfo config file "{cfg_file}" does not exist.'
+                )
 
         # TODO: remove the nested structure of dataset_info
         # metainfo = Config.fromfile(metainfo['from_file'])
         metainfo = Config.fromfile(cfg_file).dataset_info
 
     # check data integrity
-    assert 'dataset_name' in metainfo
-    assert 'keypoint_info' in metainfo
-    assert 'skeleton_info' in metainfo
-    assert 'joint_weights' in metainfo
-    assert 'sigmas' in metainfo
+    assert "dataset_name" in metainfo
+    assert "keypoint_info" in metainfo
+    assert "skeleton_info" in metainfo
+    assert "joint_weights" in metainfo
+    assert "sigmas" in metainfo
 
     # parse metainfo
     parsed = dict(
@@ -137,47 +149,48 @@ def parse_pose_metainfo(metainfo: dict):
         sigmas=None,
     )
 
-    parsed['dataset_name'] = metainfo['dataset_name']
+    parsed["dataset_name"] = metainfo["dataset_name"]
 
     # parse keypoint information
-    parsed['num_keypoints'] = len(metainfo['keypoint_info'])
+    parsed["num_keypoints"] = len(metainfo["keypoint_info"])
 
-    for kpt_id, kpt in metainfo['keypoint_info'].items():
-        kpt_name = kpt['name']
-        parsed['keypoint_id2name'][kpt_id] = kpt_name
-        parsed['keypoint_name2id'][kpt_name] = kpt_id
-        parsed['keypoint_colors'].append(kpt.get('color', [255, 128, 0]))
+    for kpt_id, kpt in metainfo["keypoint_info"].items():
+        kpt_name = kpt["name"]
+        parsed["keypoint_id2name"][kpt_id] = kpt_name
+        parsed["keypoint_name2id"][kpt_name] = kpt_id
+        parsed["keypoint_colors"].append(kpt.get("color", [255, 128, 0]))
 
-        kpt_type = kpt.get('type', '')
-        if kpt_type == 'upper':
-            parsed['upper_body_ids'].append(kpt_id)
-        elif kpt_type == 'lower':
-            parsed['lower_body_ids'].append(kpt_id)
+        kpt_type = kpt.get("type", "")
+        if kpt_type == "upper":
+            parsed["upper_body_ids"].append(kpt_id)
+        elif kpt_type == "lower":
+            parsed["lower_body_ids"].append(kpt_id)
 
-        swap_kpt = kpt.get('swap', '')
-        if swap_kpt == kpt_name or swap_kpt == '':
-            parsed['flip_indices'].append(kpt_name)
+        swap_kpt = kpt.get("swap", "")
+        if swap_kpt == kpt_name or swap_kpt == "":
+            parsed["flip_indices"].append(kpt_name)
         else:
-            parsed['flip_indices'].append(swap_kpt)
+            parsed["flip_indices"].append(swap_kpt)
             pair = (swap_kpt, kpt_name)
-            if pair not in parsed['flip_pairs']:
-                parsed['flip_pairs'].append(pair)
+            if pair not in parsed["flip_pairs"]:
+                parsed["flip_pairs"].append(pair)
 
     # parse skeleton information
-    parsed['num_skeleton_links'] = len(metainfo['skeleton_info'])
-    for _, sk in metainfo['skeleton_info'].items():
-        parsed['skeleton_links'].append(sk['link'])
-        parsed['skeleton_link_colors'].append(sk.get('color', [96, 96, 255]))
+    parsed["num_skeleton_links"] = len(metainfo["skeleton_info"])
+    for _, sk in metainfo["skeleton_info"].items():
+        parsed["skeleton_links"].append(sk["link"])
+        parsed["skeleton_link_colors"].append(sk.get("color", [96, 96, 255]))
 
     # parse extra information
-    parsed['dataset_keypoint_weights'] = np.array(
-        metainfo['joint_weights'], dtype=np.float32)
-    parsed['sigmas'] = np.array(metainfo['sigmas'], dtype=np.float32)
+    parsed["dataset_keypoint_weights"] = np.array(
+        metainfo["joint_weights"], dtype=np.float32
+    )
+    parsed["sigmas"] = np.array(metainfo["sigmas"], dtype=np.float32)
 
-    if 'stats_info' in metainfo:
-        parsed['stats_info'] = {}
-        for name, val in metainfo['stats_info'].items():
-            parsed['stats_info'][name] = np.array(val, dtype=np.float32)
+    if "stats_info" in metainfo:
+        parsed["stats_info"] = {}
+        for name, val in metainfo["stats_info"].items():
+            parsed["stats_info"][name] = np.array(val, dtype=np.float32)
 
     # formatting
     def _map(src, mapping: dict):
@@ -187,16 +200,19 @@ def parse_pose_metainfo(metainfo: dict):
         else:
             return mapping[src]
 
-    parsed['flip_pairs'] = _map(
-        parsed['flip_pairs'], mapping=parsed['keypoint_name2id'])
-    parsed['flip_indices'] = _map(
-        parsed['flip_indices'], mapping=parsed['keypoint_name2id'])
-    parsed['skeleton_links'] = _map(
-        parsed['skeleton_links'], mapping=parsed['keypoint_name2id'])
+    parsed["flip_pairs"] = _map(
+        parsed["flip_pairs"], mapping=parsed["keypoint_name2id"]
+    )
+    parsed["flip_indices"] = _map(
+        parsed["flip_indices"], mapping=parsed["keypoint_name2id"]
+    )
+    parsed["skeleton_links"] = _map(
+        parsed["skeleton_links"], mapping=parsed["keypoint_name2id"]
+    )
 
-    parsed['keypoint_colors'] = np.array(
-        parsed['keypoint_colors'], dtype=np.uint8)
-    parsed['skeleton_link_colors'] = np.array(
-        parsed['skeleton_link_colors'], dtype=np.uint8)
+    parsed["keypoint_colors"] = np.array(parsed["keypoint_colors"], dtype=np.uint8)
+    parsed["skeleton_link_colors"] = np.array(
+        parsed["skeleton_link_colors"], dtype=np.uint8
+    )
 
     return parsed
