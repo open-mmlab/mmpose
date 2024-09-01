b08a0d0a0e80f21e7ce0bf538af7ad1d9f9f5a2b
Abhishek V <tdicommons@gmail.com>
2024-09-01 13:08:35 +0530

    Update simcc_3d_label.py
    line 213 should have Tuple. python version mismatch found, updated

--- projects/rtmpose3d/rtmpose3d/simcc_3d_label.py
+++ projects/rtmpose3d/rtmpose3d/simcc_3d_label.py
@@ -210,7 +210,7 @@ class SimCC3DLabel(BaseKeypointCodec):
         self,
         keypoints: np.ndarray,
         keypoints_visible: Optional[np.ndarray] = None
-    ) -> tuple[ndarray, ndarray, ndarray, ndarray]:
+    ) -> Tuple[ndarray, ndarray, ndarray, ndarray]:
         """Encoding keypoints into SimCC labels with Gaussian Label Smoothing
         strategy."""
 
