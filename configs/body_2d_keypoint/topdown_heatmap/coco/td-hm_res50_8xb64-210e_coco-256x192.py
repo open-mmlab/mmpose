82130605b1c462bebab09afc85a6ada263d5c423
rostyslavhereha <rostyslav.hereha@lvairo.com>
2025-02-13 16:35:59 +0200

    pallet kp changes

--- configs/body_2d_keypoint/topdown_heatmap/coco/td-hm_res50_8xb64-210e_coco-256x192.py
+++ configs/body_2d_keypoint/topdown_heatmap/coco/td-hm_res50_8xb64-210e_coco-256x192.py
@@ -71,21 +71,8 @@ model = dict(
 # base dataset settings
 dataset_type = 'CocoDataset'
 data_mode = 'topdown'
-# data_root = 'data/exported_ls_data/'
-# data_root = 'data/1704_all_exported_data_project_id_422/'
-# data_root = 'data/1704_split_exported_data_project_id_422/'
-# data_root = 'data/demodesk_iter_2/'
-# data_root = 'data/idk_what_i_do_1335_stef_17_10_24/'
-# data_root = '/data/new_mmpose/mmpose/data/4045_stef_png_18_10_24/'
-# data_root = '/data/new_mmpose/mmpose/data/new_kp_model/'
-# data_root = '/data/brugen_keypoints/'
-# data_root = 'data/stef_kp_04_11_24/'
-#data_root = 'data/pallet_#data_root = 'data/kp_id322_6_12_24/'
-#data_root = 'data/kp_id322_6_12_24/'
-# data_root = 'data/bruggen_9_12_kp/'
-# data_root = 'data/16_12/'
-data_root = 'data/17_pallet_kp/'
-
+data_root = '/data/now/brug_mts_pallet/'
+labels = ["top_left", "top_right", "bottom_left", "bottom_right"]
 # pipelines
 train_pipeline = [
     dict(type='LoadImage'),
@@ -135,7 +122,7 @@ train_dataloader = dict(
     sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
         type=dataset_type,
-	    num_keypoints=num_keypoints,
+	    labels=labels,
         data_root=data_root,
         data_mode=data_mode,
         ann_file='annotations/forklift_keypoints_train2017.json',
@@ -150,7 +137,7 @@ val_dataloader = dict(
     sampler=dict(type='DefaultSampler', shuffle=False, round_up=False),
     dataset=dict(
         type=dataset_type,
-	    num_keypoints=num_keypoints,
+	    labels=labels,
         data_root=data_root,
         data_mode=data_mode,
         ann_file='annotations/forklift_keypoints_train2017.json',
