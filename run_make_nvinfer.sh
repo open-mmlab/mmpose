ef7075b31874923cebecfb9e3a2301d684973f81
VolodymyrRatushnyi <volodymyr.ratushnyi@logivations.com>
2025-04-18 21:52:59 +0300

    RTDT-3453 Label Studio: fix configs for models

--- run_make_nvinfer.sh
+++ run_make_nvinfer.sh
@@ -1,27 +1,60 @@
 #!/bin/bash
 
-MODEL_DIR=$1
+MODEL_DIR=""
+OPERATE_ON_CLASS_NAMES=()
+CLASSES=()
+RES=()
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --model-dir)
+            MODEL_DIR="$2"
+            shift 2
+            ;;
+        --operate-on-class-names)
+            shift
+            while [[ $# -gt 0 ]] && [[ ! "$1" =~ ^-- ]]; do
+                OPERATE_ON_CLASS_NAMES+=("$1")
+                shift
+            done
+            ;;
+        --classes)
+            shift
+            while [[ $# -gt 0 ]] && [[ ! "$1" =~ ^-- ]]; do
+                CLASSES+=("$1")
+                shift
+            done
+            ;;
+        --res)
+            shift
+            while [[ $# -gt 0 ]] && [[ ! "$1" =~ ^-- ]]; do
+                RES+=("$1")
+                shift
+            done
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            exit 1
+            ;;
+    esac
+done
+
 MODEL_DIR=${MODEL_DIR%%/}
-shift
-
-OPERATED_ON_CLASS_NAME=$1
-shift
-
-CLASSES=( "$@" )
+OPERATE_ON_CLASS_NAMES=$(IFS=';' ; echo "${OPERATE_ON_CLASS_NAMES[*]}")
 CLASSES=$(IFS=';' ; echo "${CLASSES[*]}")
+RES=$(IFS=';' ; echo "${RES[*]}")
 
 echo "[property]
 
 # model loading.
 onnx-file=keypoint_detector.onnx
-model-engine-file=keypoint_detector.onnx_b8_gpu0_fp16.engine
 
 # model config
 infer-dims=3;256;192
-gie-unique-id=2
-operate-on-class-ids=0
-operated-on-class-name=$OPERATED_ON_CLASS_NAME
 
 [custom]
+min-kp-score=0.0
+res=$RES
+operate-on-class-names=$OPERATE_ON_CLASS_NAMES
 kp-names=$CLASSES
 " > "$MODEL_DIR/keypoints-config.txt"
